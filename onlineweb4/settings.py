0a004e3d642cdf173b600edfb6fe6cc0cdaaf046
Torkil Gustavsen <torkil@gmail.com>
2012-06-11 10:39:51 +0200

    Added South to the project

--- onlineweb4/settings.py
+++ onlineweb4/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_nose',
+    'south',
     'apps.events',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
