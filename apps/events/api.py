d91b8f96290498f1e36d64bd797fcea5e43d3df1
Torkil Gustavsen <torkil@gmail.com>
2012-07-12 23:51:02 +0200

    Rename data objects to 'events'

--- apps/events/api.py
+++ apps/events/api.py
@@ -1,7 +1,16 @@
+from copy import copy
 from tastypie.resources import ModelResource
 from models import Event
 
 class EventResource(ModelResource):
+
+    def alter_list_data_to_serialize(self, request, data):
+        # Rename list data object to 'events'.
+        if isinstance(data, dict):
+            data['events'] = copy(data['objects'])
+            del(data['objects'])
+        return data
+
     class Meta:
         queryset = Event.objects.all()
         resource_name = 'events'
