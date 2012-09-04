d769a6017d71d2de8a27f5e17faf99b9dd2e1b3f
Sigurd Fosseng <sigurd@fosseng.net>
2012-09-04 02:04:54 +0200

    local settings

---
+++ onlineweb4/settings/local.py
@@ -0,0 +1,47 @@
+import os
+from base import PROJECT_ROOT_DIRECTORY
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+    },
+    'sqlite': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'sqlite3/db.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
+
+
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # real
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # prints
+
+#MEDIA_ROOT = '/var/websites/prod/onlineweb_uploads'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT_DIRECTORY, "uploaded_media/")
+
+#MEDIA_URL = '//media.online.ntnu.no/'
+MEDIA_URL = '/media/'
+
+
+#MEDIA_ROOT = '/var/websites/prod/static'
+STATIC_ROOT = os.path.join(PROJECT_ROOT_DIRECTORY, 'collected_static')
+#STATIC_URL = '//static.online.ntnu.no'
+STATIC_URL = '/static/'
+
+# If you use django extensions that should not be used in production
+# add them here.
+# INSTALLED_APPS += (
+#   'apps.example',
+#   'debug_toolbar', # https://github.com/dcramer/django-debug-toolbar
+#   'django_extensions', # http://packages.python.org/django-extensions/
+# )
