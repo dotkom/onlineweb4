ccde8ac28282dd615b9b59679e0adee5351b0690
Tor Håkon Bonsaksen <torrib90@gmail.com>
2012-09-26 20:02:25 +0200

    Added models.py under companyprofiles

---
+++ apps/companyprofile/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Company(models.Model):
+    
+    name = models.CharField(_("Bedriftsnavn"), max_length=100)
+# TODO Bilde
+    short_description = models.TextField(_("Kort beskrivelse"), max_length=200)
+    long_description = models.TextField(_("Utdypende beskrivelse"), blank=True, null=True)
+    site = models.URLField(_("Hjemmeside"))
+    email_address = models.EmailField(_("E-mail addresse"), max_length=75, blank=True, null=True)
+    phone_number = models.IntegerField(_"Telefon nummer"), max_length=8, blank=True, null=True)
+#   public_profile = models.BooleanField(_("Offentlig profil"), default=False)
+
