a3161ddea5241f8d8e350fc9b185304b5d085b9e
Jonas Svarvaa <jonassvarvaa@gmail.com>
2012-10-03 23:25:16 +0200

    Added admin to urls and settings. Added admin.py for events. Started on views.py.

---
+++ apps/events/admin.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from onlineweb4.apps.events import Event
+from django.contrib import admin
+
+admin.site.register(Event)
--- onlineweb4/settings.py
+++ onlineweb4/settings.py
@@ -125,7 +125,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
