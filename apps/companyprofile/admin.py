7795ea62af832a5c1470f9e75cc86a05f601ed4c
Tor Håkon Bonsaksen <torrib90@gmail.com>
2012-10-03 21:40:07 +0200

    Removed long and short description from the list display

--- apps/companyprofile/admin.py
+++ apps/companyprofile/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from models import Company
 
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'short_description','long_description', 'site', 'email_address', 'phone_number',)
+    list_display = ('name', 'site', 'email_address', 'phone_number',)
 
 admin.site.register(Company, CompanyAdmin)
     
