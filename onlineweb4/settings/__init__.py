cd799a6568dba2f02d4f9a3f428014d76f5afa83
melwil <slettvold@gmail.com>
2012-10-08 19:52:36 +0200

    Added some more error handling in the file that imports the base settings file.

--- onlineweb4/settings/__init__.py
+++ onlineweb4/settings/__init__.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
+if not os.path.exists(os.path.join(os.path.dirname(globals()['__file__']), 'base.py')):
+    sys.stderr.write("Failed to locate the base settings file. Quitting.\n")
+    sys.exit(1)
 try:
     from onlineweb4.settings.base import *
 except ImportError:
-    pass
+    sys.stderr.write("Failed to import from the base settings file. Quitting.\n")
+    sys.exit(1)
