1068a946e6b9e6142b9ad0c5af82b8399b2c6350
Espen Jacobsson <espenjacobsson@gmail.com>
2012-10-10 14:23:41 -0700

    encoding derped, manytomanyfield on attendee

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -5,6 +5,10 @@ from apps.events.models import Attendee
 from apps.events.models import AttendanceEvent
 from django.contrib import admin
 
+
+class AdminAttendanceEvent(admin.ModelAdmin):
+    list_display = ('title',) 
+
 admin.site.register(Event)
-admin.site.register(AttendanceEvent)
+admin.site.register(AttendanceEvent, AdminAttendanceEvent)
 admin.site.register(Attendee)
