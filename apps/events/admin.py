8dd090990759ae80c17b2aa4a88d287c8e46c341
Espen Jacobsson <espenjacobsson@gmail.com>
2012-10-10 12:25:54 -0700

    attendees for attendanceevents

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from onlineweb4.apps.events import Event
+from apps.events.models import Event
+from apps.events.models import Attendee
+from apps.events.models import AttendanceEvent
 from django.contrib import admin
 
 admin.site.register(Event)
+admin.site.register(AttendanceEvent)
+admin.site.register(Attendee)
--- apps/events/models.py
+++ apps/events/models.py
@@ -8,28 +8,43 @@ class Event(models.Model):
     """
     Base class for Event-objects
     """
-    author = models.ForeignKey(User, related_name='author')
-    title = models.CharField(_('title'), max_length=100)
-    event_start = models.DateTimeField(_('start_date'))
-    event_end = models.DateTimeField(_('end_date'))
-    location = models.CharField(_('location'), max_length=100)
-    description = models.TextField(_('description'))
+    author = models.ForeignKey(User, related_name='oppretter')
+    title = models.CharField(_('tittel'), max_length=100)
+    event_start = models.DateTimeField(_('start-dato'))
+    event_end = models.DateTimeField(_('slutt-dato'))
+    location = models.CharField(_('lokasjon'), max_length=100)
+    description = models.TextField(_('beskrivelse'))
 
     def __unicode__(self):
         return self.title
 
     class Meta:
-        verbose_name = _('event')
-        verbose_name_plural = _('events')
+        verbose_name = _('arrangement')
+        verbose_name_plural = _('arrangement')
 
 class AttendanceEvent(Event):
     """
     Events that require special considerations regarding attendance.
     """
-    max_capacity = models.PositiveIntegerField(_('max_capacity'))
-    registration_start = models.DateTimeField(_('registration_start'))
-    registration_end = models.DateTimeField(_('registration_end'))
+    max_capacity = models.PositiveIntegerField(_('maks-kapasitet'))
+    registration_start = models.DateTimeField(_('registrerings-start'))
+    registration_end = models.DateTimeField(_('registrerings-slutt'))
 
     class Meta:
-        verbose_name = _('AttendanceEvent')
-        verbose_name_plural = _('AttendanceEvents')
+        verbose_name = _('påmeldingsarrangement')
+        verbose_name_plural = _('påmeldingsarrangement')
+
+
+class Attendee(models.Model):
+    user = models.ForeignKey(User)
+    event = models.ForeignKey(AttendanceEvent)
+    timestamp = models.DateTimeField(auto_now_add=True, editable=False)
+    paid = models.BooleanField(_('betalt'))
+    attended = models.BooleanField(_('var tilstede'))
+
+    def __unicode__(self):
+        return self.user.get_full_name()
+
+    class Meta:
+        unique_together = ("event", "user")
+        ordering = ['timestamp']
