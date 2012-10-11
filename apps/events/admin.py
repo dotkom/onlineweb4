8d2e1687e933242e3c3a8a3be78ac68833f952d9
Jonas Svarvaa <jonassvarvaa@gmail.com>
2012-10-11 22:54:49 +0200

    Added inline AttendanceEvent admin-thingy.

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -3,7 +3,22 @@
 from apps.events.models import Event, AttendanceEvent, Attendee
 from django.contrib import admin
 
+class AttendanceEventInline(admin.StackedInline):
+    model = AttendanceEvent
+    max_num = 1
+    extra = 0
 
-admin.site.register(Event)
-admin.site.register(AttendanceEvent)
-admin.site.register(Attendee)
+# class AttendeeInline(admin.StackedInline):
+#     model = Attendee
+#     extra = 0
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = ( AttendanceEventInline,
+                # AttendeeInline)
+                )
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for instance in instances:
+            instance.save()
+
+admin.site.register(Event, EventAdmin)
