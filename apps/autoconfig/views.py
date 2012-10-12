94772d2f71004ebedc45d17f2d76954ed8558221
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2012-10-12 14:51:34 +0200

    (autoconfig) swapped out render_to_response to render
    As we're not using RequestContext, might as well just use render.

--- apps/autoconfig/views.py
+++ apps/autoconfig/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.http import HttpResponseBadRequest
-from django.template import RequestContext
 
 def autoconfig(request):
   if 'emailaddress' in request.GET:
     email = request.GET['emailaddress']
     if '@online.ntnu.no' in email:
-      response = render_to_response('autoconfig/client_config.xml', {}, RequestContext(request))
+      response = render(request, 'autoconfig/client_config.xml')
       response['Content-Type'] = 'application/xml; charset=utf-8'
       return response
   return HttpResponseBadRequest()
