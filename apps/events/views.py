5eea85a6f5d8d1db2a2bcc7fda4256d8060beef9
Jonas Svarvaa <jonassvarvaa@gmail.com>
2012-10-24 19:15:26 +0200

    Lint :)

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,9 +1,10 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.shortcuts import get_object_or_404
-from django.template  import RequestContext
-from apps.events.models    import Event, Attendee
+from django.template import RequestContext
+from apps.events.models import Event, Attendee
 import datetime
+
 
 def index(request):
     events = Event.objects.filter(event_start__gte=datetime.date.today())
@@ -11,10 +12,12 @@ def index(request):
         return details(request, events[0].event_id)
     return render_to_response('events/index.html', {'events': events}, context_instance=RequestContext(request))
 
+
 def details(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
-    
+
     return render_to_response('events/details.html', {'event': event}, context_instance=RequestContext(request))
+
 
 def get_attendee(attendee_id):
     return get_object_or_404(Attendee, pk=attendee_id)
