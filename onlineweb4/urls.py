57f37bbb747b55ed2e728508124c6f54d4e04f14
melwil <slettvold@gmail.com>
2012-11-06 17:40:25 +0100

    Brushing up on urls.py

--- onlineweb4/urls.py
+++ onlineweb4/urls.py
@@ -1,25 +1,28 @@
 from django.conf.urls import patterns, include, url
-from tastypie.api import Api
-from apps.events.api import EventResource, UserResource
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
 
-# 
+from filebrowser.sites import site
+from tastypie.api import Api
+
+# Tastypie 
+from apps.events.api import EventResource, UserResource
+
 v0_api = Api(api_name='v0')
 v0_api.register(EventResource())
 v0_api.register(UserResource())
 
+# URL config 
 urlpatterns = patterns('',
-    # Examples:
+    # Filebrowser must be above all admin-urls
+    url(r'^admin/filebrowser/',     include(site.urls)),
+    url(r'^grappelli/',             include('grappelli.urls')),
+
+    # Admin urls
+    url(r'^admin/',             include(admin.site.urls)),
+    url(r'^admin/doc/',         include('django.contrib.admindocs.urls')),
+
+    # Onlineweb app urls
     # url(r'^$', 'onlineweb4.views.home', name='home'),
-    # url(r'^onlineweb4/', include('onlineweb4.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    (r'^api/', include(v0_api.urls)),
-    (r'^mail/', include('apps.autoconfig.urls'))
+    (r'^api/',      include(v0_api.urls)),
+    (r'^mail/',     include('apps.autoconfig.urls'))
 )
