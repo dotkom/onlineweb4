a72db2f5fe2c69fed48778dbf3d58623d6d76cce
melwil <slettvold@gmail.com>
2012-12-06 13:15:31 +0100

    Replaced the user resource in article api by the global resource in userprofile.

--- apps/article/api.py
+++ apps/article/api.py
@@ -1,24 +1,15 @@
 #-*- coding: utf-8 -*-
-from apps.article.models import Article
 from django.contrib.auth.models import User
-from tastypie.resources import ModelResource
-from copy import copy
+
 from tastypie import fields
+from tastypie.resources import ModelResource
 
-
-class UserResource(ModelResource):
-    class Meta:
-        queryset = User.objects.all()
-        resource_name = 'article/user'
-        # List of fields we do NOT want to make available
-        excludes = ['password',
-                    'email',
-                    'date_joined'
-                    'id',
-                    'last_login']
+from apps.article.models import Article
+from apps.userprofile.api import UserResource
 
 class ArticleResource(ModelResource):
-    author = fields.ToOneField(UserResource, 'created_by', full=True)
+    author = fields.ToOneField(UserResource, 'created_by')
+
     class Meta:
         queryset = Article.objects.all()
         resource_name = 'article/all'
