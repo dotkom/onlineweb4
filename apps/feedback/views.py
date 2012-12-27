136bf1b3d58f62ae5ba0da629b2d4a1e3a21a14f
Sigurd Fosseng <sigurd@fosseng.net>
2012-12-27 17:49:47 +0100

    A skeleton for displaying feedback schemas.
    example url: /feedback/auth/user/1/1

--- apps/feedback/views.py
+++ apps/feedback/views.py
@@ -1,7 +1,23 @@
 #-*- coding: utf-8 -*-
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
 from apps.feedback.models import Feedback
+from apps.feedback.models import FeedbackRelation
+
+
+def feedback(request, applabel, appmodel, object_id, feedback_id):
+    try:
+        ct = ContentType.objects.get(app_label=applabel, model=appmodel)
+        fbr = FeedbackRelation.objects.get(content_type=ct,
+                                           object_id=object_id,
+                                           feedback_id=feedback_id)
+    except ObjectDoesNotExist:
+        return HttpResponse("fant ikke skjemaet")
+
+    return HttpResponse(fbr.feedback.description)
 
 
 def index(request):
