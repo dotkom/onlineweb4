9c6f599b7682abd7a683caee3021afc88623e25c
Espen Jacobsson <espenjacobsson@gmail.com>
2013-01-23 12:08:15 -0800

    added custom field for dynamic fixtures in unicode test

--- apps/article/tests.py
+++ apps/article/tests.py
@@ -1,5 +1,6 @@
 from apps.article.models import Article
 from nose.tools import assert_equal
+from nose.tools import assert_not_equal
 from django_dynamic_fixture import G
 import logging
 
@@ -8,6 +9,6 @@ def testArticleUnicodeIsCorrect():
     logger = logging.getLogger(__name__)
     logger.debug("Testing testing on Article with dynamic fixtures")
 
-    article = G(Article)
-    assert_equal(article.__unicode__(), "1")
+    article = G(Article, heading="test_heading")
+    assert_equal(article.__unicode__(), "test_heading")
     
