4db31e7117795fa08641e6297dc1e0079e90a49d
Jonas Svarvaa <jonassvarvaa@gmail.com>
2013-01-28 21:12:05 +0100

    Matched id's of menu-items and corresponding sections of the frontpage. JS not currently working.

--- static/js/jonas.js
+++ static/js/jonas.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
+
     function scrolltothis(from, to) {
+
         $(from).click(function() {
-            var topPosition = jQuery(to).offset().top - 57; //See margin-top on body element
+            var topPosition = jQuery(to).offset().top - 57; // See body margin
             jQuery('html, body').animate({scrollTop:topPosition}, 'slow');
         });
+
     }
-    scrolltothis('#home', 'body');
-    scrolltothis('#arrangementer', '#arrangementer-heading');
-    scrolltothis('#artikler', '#artikler-heading');
+
+    scrolltothis('#events', '#events-heading');
+    scrolltothis('#articles', '#articles-heading');
+    scrolltothis('#about', '#about-heading');
+    scrolltothis('#business', '#business-heading');
+    scrolltothis('#offline', '#offline-heading');
+
 });
