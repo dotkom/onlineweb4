c3b14297c34c8302a3bd18660ac3db8adaadd9b3
Espen Jacobsson <espenjacobsson@gmail.com>
2013-01-30 13:44:57 -0800

    cleaned up article tests, converted events test to django testing

--- apps/article/tests.py
+++ apps/article/tests.py
@@ -1,16 +1,16 @@
 from apps.article.models import Article
 from django_dynamic_fixture import G
+from django.test import TestCase
 
 import logging
-import unittest
 
+class ArticleTests(TestCase):
+    
+    def setUp(self):
+        self.logger = logging.getLogger(__name__)
+        self.article = G(Article, heading="test_heading")
 
-class ArticleTests(unittest.TestCase):
 
     def testArticleUnicodeIsCorrect(self):
-
-        logger = logging.getLogger(__name__)
-        logger.debug("Testing testing on Article with dynamic fixtures")
-
-        article = G(Article, heading="test_heading")
-        self.assertEqual(article.__unicode__(), "test_heading")
+        self.logger.debug("Testing testing on Article with dynamic fixtures")
+        self.assertEqual(self.article.__unicode__(), "test_heading")
