a30dda6bfd1474cd4333f7497962a0f938a68f9a
Espen Jacobsson <espenjacobsson@gmail.com>
2013-01-30 13:44:57 -0800

    cleaned up article tests, converted events test to django testing

--- apps/article/tests.py
+++ apps/article/tests.py
@@ -3,15 +3,15 @@ from apps.article.admin import ArticleAdmin
 from nose.tools import assert_equal
 from nose.tools import assert_not_equal
 from django_dynamic_fixture import G
+from django.test import TestCase
 import logging
-import unittest
 
-class ArticleTests(unittest.TestCase):
+class ArticleTests(TestCase):
+    
+    def setUp(self):
+        self.logger = logging.getLogger(__name__)
+        self.article = G(Article, heading="test_heading")
 
     def testArticleUnicodeIsCorrect(self):
-
-        logger = logging.getLogger(__name__)
-        logger.debug("Testing testing on Article with dynamic fixtures")
-
-        article = G(Article, heading="test_heading")
-        assert_equal(article.__unicode__(), "test_heading")
+        self.logger.debug("Testing testing on Article with dynamic fixtures")
+        self.assertEqual(self.article.__unicode__(), "test_heading")
--- apps/events/tests.py
+++ apps/events/tests.py
@@ -1,27 +1,27 @@
 from apps.events.models import Event, AttendanceEvent, Attendee
 from django.contrib.auth.models import User
-from nose.tools import assert_equal, assert_not_equal
 from django_dynamic_fixture import G
+from django.test import TestCase
 import logging
 
-event = G(Event, title='Sjakkturnering')
-attendance_event = G(AttendanceEvent, event=event)
-user = G(User)
-attendee = G(Attendee, event=attendance_event, user=user)
-logger = logging.getLogger(__name__)
+class EventTest(TestCase):
 
+    def setUp(self):
+        self.event = G(Event, title='Sjakkturnering')
+        self.attendance_event = G(AttendanceEvent, event=self.event)
+        self.user = G(User)
+        self.attendee = G(Attendee, event=self.attendance_event, user=self.user)
+        self.logger = logging.getLogger(__name__)
 
-def testEventUnicodeIsCorrect():
-    logger.debug("Testing testing on Event with dynamic fixtures")
-    assert_equal(event.__unicode__(), u'Sjakkturnering')
+    def testEventUnicodeIsCorrect(self):
+        self.logger.debug("Testing testing on Event with dynamic fixtures")
+        self.assertEqual(self.event.__unicode__(), u'Sjakkturnering')
 
+    def testAttendanceEventUnicodeIsCorrect(self):
+        self.logger.debug("Testing testing on AttendanceEvent with dynamic fixtures")
+        self.assertEqual(self.attendance_event.__unicode__(), u'Sjakkturnering')
 
-def testAttendanceEventUnicodeIsCorrect():
-    logger.debug("Testing testing on AttendanceEvent with dynamic fixtures")
-    assert_equal(attendance_event.__unicode__(), u'Sjakkturnering')
-
-
-def testAttendeeUnicodeIsCorrect():
-    logger.debug("Testing testing on Attendee with dynamic fixtures")
-    assert_equal(attendee.__unicode__(), user.get_full_name())
-    assert_not_equal(attendee.__unicode__(), 'Ola Normann')
+    def testAttendeeUnicodeIsCorrect(self):
+        self.logger.debug("Testing testing on Attendee with dynamic fixtures")
+        self.assertEqual(self.attendee.__unicode__(), self.user.get_full_name())
+        self.assertNotEqual(self.attendee.__unicode__(), 'Ola Normann')
