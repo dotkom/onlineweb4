334701a290126ec3e1687124f6e2dacdf725bf6e
Espen Jacobsson <espenjacobsson@gmail.com>
2013-01-30 12:35:23 -0800

    removed nose, now running with standard django unit tests. this works fine.

--- apps/article/tests.py
+++ apps/article/tests.py
@@ -1,15 +1,17 @@
 from apps.article.models import Article
+from apps.article.admin import ArticleAdmin
 from nose.tools import assert_equal
 from nose.tools import assert_not_equal
 from django_dynamic_fixture import G
 import logging
+import unittest
 
+class ArticleTests(unittest.TestCase):
 
-def testArticleUnicodeIsCorrect():
+    def testArticleUnicodeIsCorrect(self):
 
-    logger = logging.getLogger(__name__)
-    logger.debug("Testing testing on Article with dynamic fixtures")
+        logger = logging.getLogger(__name__)
+        logger.debug("Testing testing on Article with dynamic fixtures")
 
-    article = G(Article, heading="test_heading")
-    assert_equal(article.__unicode__(), "test_heading")
-    
+        article = G(Article, heading="test_heading")
+        assert_equal(article.__unicode__(), "test_heading")
