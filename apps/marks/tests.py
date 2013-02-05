896e30ec0c340f69cc1a148e98622d2abee86efb
krekle <kristian.ekle@gmail.com>
2013-02-06 00:18:52 +0100

    Expanded the MarksTest and converted test to django testing

--- apps/marks/tests.py
+++ apps/marks/tests.py
@@ -1,24 +1,30 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from apps.marks.models import Mark
-from nose.tools import assert_equal, assert_not_equal
+from apps.marks.models import Mark, UserEntry
+from apps.marks.models import get_threshhold as duration
+from datetime import datetime, timedelta
+from django.contrib.auth.models import User
 from django_dynamic_fixture import G
+from django.test import TestCase
 import logging
 
-def testMarks():
-    logger = logging.getLogger(__name__)
-    logger.debug("Testing Mark")
 
-    mark = G(Mark)
-    assert_not_equal(mark.__unicode__(), "1")
-    assert_equal(mark.__unicode__(), "Prikk for 1")
-   
-    
-    #user = G(UserEntry)
-    #asser_equal(user.__unicode__(), "1")
-    
+class MarksTest(TestCase):
+
+    def setUp(self):
+        self.user=G(User)
+        #Mark added 59 (working) days ago.
+        markdate = duration()-timedelta(days=1)
+        self.mark = G(Mark, title="1",  mark_added_date=markdate)
+        self.userentry = G(UserEntry, user=self.user)
+        self.logger = logging.getLogger(__name__)
+
+    def testMarksActive(self):
+        self.logger.debug("Testing if Mark is active today with dynamic fixtures")
+        self.assertTrue(self.mark.is_active)
+        
+    def testMarkUnicode(self):    
+        self.logger.debug("Testing Mark unicode with dynamic fixtures")
+        self.assertEqual(self.mark.__unicode__(), "Prikk for 1")
+        
+    def testUserEntry(self):
+        self.logger.debug("Testing UserEntry unicode with dynamic fixtures")
+        self.assertEqual(self.userentry.__unicode__(), ("UserEntry for %s") % self.user.get_full_name())
