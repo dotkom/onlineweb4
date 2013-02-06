c5fc1dda7eb7cca843f631f0ae04d188625a4cb7
Jonas Svarvaa <jonassvarvaa@gmail.com>
2013-02-06 21:48:22 +0100

    Changed to render() instead of render_to_response()

--- apps/offline/views.py
+++ apps/offline/views.py
@@ -2,14 +2,13 @@
 
 from onlineweb.apps.offline.models import Offline
 from onlineweb.apps.offline.models import Issue
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template.context import RequestContext
 
 from onlineweb.decorators.view_decorators import html_view
 
-@html_view('offline/offline.html')
 def main(request):
 	issues = Issue.objects.all()
 	offline = Offline.objects.all()[0]
-	return render_to_response("offline/offline.html", {"offline":offline, "issues":issues}, context_instance=RequestContext(request))
+	return render("offline/offline.html", {"offline":offline, "issues":issues})
 
