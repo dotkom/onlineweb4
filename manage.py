590656eb17cf4b814a94a0b0af501b891c07daef
Jonas Svarvaa <jonassvarvaa@gmail.com>
2013-02-13 22:46:22 +0100

    Added a way to insert/bootstrap chunks.

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+from bootstrap import Chunks
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onlineweb4.settings")
 
     from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+    if(sys.argv[1] == 'bootstrap'):
+        Chunks.bootstrap()
+    else:
+        execute_from_command_line(sys.argv)
