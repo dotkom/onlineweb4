d3db8528a221308dfce78dc6c0b609f1be2f2058
Nils Herde <hernil@laptop.(none)>
2013-02-13 22:38:54 +0100

    Changed to use the offline property

--- apps/offline/views.py
+++ apps/offline/views.py
@@ -3,7 +3,6 @@
 from apps.offline.models import Issue
 from django.shortcuts import render
 
-
 def main(request):
     issues = Issue.objects.all()
     return render(request, "offline/offline.html", {"issues": issues})
