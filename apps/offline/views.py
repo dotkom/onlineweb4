2aa49455be21e41f3cd499994217c4d4d2acc6dd
Nils Herde <mail@hernil.com>
2013-02-15 18:57:49 +0100

    Fixed render of offline issues by using issues = Issue.objects.all() in views.py and added changes to template

--- apps/offline/views.py
+++ apps/offline/views.py
@@ -1,10 +1,10 @@
 # -*- encoding: utf-8 -*-
 
-from apps.offline.models import Offline
+from apps.offline.models import Issue
 from django.shortcuts import render
 from django.template.context import RequestContext
 
 def main(request):
-	issues = Offline.issues
+	issues = Issue.objects.all()
 	return render(request, "offline/offline.html", { "issues":issues })
 
