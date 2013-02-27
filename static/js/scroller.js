0854be9b6a967fd55a0ea3cb57b062fdd7baa26e
Jonas Svarvaa <jonassvarvaa@gmail.com>
2013-02-28 00:08:45 +0100

    Initial scroller commit

--- static/js/scroller.js
+++ static/js/scroller.js
@@ -1,18 +1,49 @@
 $(document).ready(function() {
 
+    function setnavs() {
+        navs = {
+            '#events': jQuery('#events').offset().top - 50,
+            '#articles': jQuery('#articles').offset().top - 50,
+            '#about': jQuery('#about').offset().top - 50,
+            '#business': jQuery('#business').offset().top - 50,
+            '#offline': jQuery('#offline').offset().top - 50
+        };
+        return navs;
+    }
+
+    var navs = setnavs();
+
     function scrolltothis(from, to) {
         $("a[href='"+from+"']").click(function(event) {
             event.preventDefault();
-            var topPosition = jQuery(to).offset().top - 70; // See body margin
-            jQuery('html, body').animate({scrollTop:topPosition}, 'slow');
-        });
+            var topPosition = jQuery(to).offset().top - 50; // See body margin
+            jQuery('html, body').animate({scrollTop:topPosition}, 250);
 
+            // Set classes
+            $(".top-menu-link a").removeClass('active');
+            $("a[href='"+from+"']").addClass("active");
+        });
     }
 
-    scrolltothis('#events', '#events-heading');
-    scrolltothis('#articles', '#articles-heading');
-    scrolltothis('#about', '#about-heading');
-    scrolltothis('#business', '#business-heading');
-    scrolltothis('#offline', '#offline-heading');
+    for (nav in navs) {
+        scrolltothis(nav, nav)
+    };
+
+    function scrollspy() {
+        var current = $(window).scrollTop();
+        for (nav in navs) {
+            var diff = current - navs[nav];
+            if (diff > -20) {
+                $(".top-menu-link a").removeClass('active');
+                $("a[href='"+nav+"']").addClass("active");
+            }
+        }
+    }
+
+    // TODO: heavy shit? Find a reliable way to setnavs instead of doing it fucking all the time.
+    $(window).scroll(function() {
+        navs = setnavs();
+        scrollspy();
+    });
 
 });
