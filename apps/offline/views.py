d4066a7557256676e582e7090b1bc321a103453d
Nils Herde <mail@hernil.com>
2013-02-27 22:38:32 +0100

    Forgot to add all files on last commit

--- apps/offline/views.py
+++ apps/offline/views.py
@@ -5,4 +5,15 @@ from django.shortcuts import render
 
 def main(request):
     issues = Issue.objects.all()
-    return render(request, "offline/offline.html", {"issues": issues})
+    years = set()
+    for issue in issues:
+        years.add(issue.release_date.year)
+
+    years = list(reversed(sorted(years)))
+
+    ctx = {
+        "issues": issues,
+        "years": years,
+
+    }
+    return render(request, "offline/offline.html", ctx)
