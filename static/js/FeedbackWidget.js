b51a11f6e3303d6fd9701abf75e4582c4361c8a8
Edgar Vedvik <edgar.vedvik@gmail.com>
2013-03-21 18:04:58 +0100

    added javascript lib. to draw charts. made chart for Field of study questionC

--- static/js/FeedbackWidget.js
+++ static/js/FeedbackWidget.js
@@ -1,42 +1,41 @@
 function createchart(chartdata) {
 
-    var width = 300,
-        height = 300,
-        radius = 100
+    var width = 960,
+        height = 500,
+        radius = Math.min(width, height) / 2;
 
     var color = d3.scale.category20c();
 
-    var data = chartdata
-
-    var vis = d3.select("body")
-        .append("svg:svg")
-        .data([data])
-            .attr("width", width)
-            .attr("height", height)
-        .append("svg:g")
-            .attr("transform", "translate(" + radius + "," + radius + ")")
-    
     var arc = d3.svg.arc()
-        .outerRadius(radius);
+        .outerRadius(radius - 10)
+        .innerRadius(0);
 
     var pie = d3.layout.pie()
+        .sort(null)
         .value(function(d) { return d.value; });
 
-    var arcs = vis.selectAll("g.slice")
-        .data(pie)
-        .enter()
-            .append("svg:g")
-                .attr("class", "slice");
-        arcs.append("svg:path")
-                .attr("fill", function(d, i) { return color(i); })
-                .attr("d", arc);
+    var svg = d3.select("div#charts")
+        .html("lololololololololo")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+      .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-        arcs.append("svg:text")
-                .attr("transform", function(d) {
-                    d.innerRadius = 0;
-                    d.outerRadius = radius;
-                    return "translate(" + arc.centroid(d) + ")";
-                })
-                .attr("text-anchor", "middle")
-                .text(function(d, i) { return data[i].label; });
+    var data = chartdata
+
+    var g = svg.selectAll(".arc")
+        .data(pie(data))
+      .enter().append("g")
+        .attr("class", "arc");
+
+    g.append("path")
+        .attr("d", arc)
+        .attr("fill", function(d, i) { return color(i); })
+
+    g.append("text")
+        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d.data.label; });
 }
