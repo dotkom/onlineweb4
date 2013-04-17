694cef35e0430ae6613ce69e69b25066ec0d6642
Jonas Svarvaa <jonassvarvaa@gmail.com>
2013-04-17 23:10:39 +0200

    Made tagcloud sizes relative to max frequency of a tag.

--- static/js/TagCloud.js
+++ static/js/TagCloud.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
     $('div.tagcloud span').each(function () {
+        var max = $('div.tagcloud').data('max_frequency');
+        console.log(max);
         frequency = $(this).data('frequency');
-        rel = (frequency / 10 < 1) ? frequency / 10 + 1: (frequency / 10 > 2) ? "2" : frequency / 10;
+        rel = ((frequency / (max/3)) + 1);
         $(this).css("font-size", rel + 'em');
-        if(rel < 1.2) {
+        if(rel < 2) {
             rel = 400;
-        } else if(rel < 1.5) {
+        } else if(rel < 2.5) {
             rel = 600;
-        } else if(rel < 1.7) {
+        } else if(rel < 3.5) {
             rel = 700;
         } else {
             rel = 800;
