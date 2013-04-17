8a31f6ce3089323bb36d3b01a7aa62c9fcb58b1d
thomasgautvedt <thomasgautv@hotmail.com>
2013-04-17 22:33:07 +0200

    Now working with filtering of years & paginering

--- static/js/offline-filter.js
+++ static/js/offline-filter.js
@@ -1,8 +1,8 @@
-var busy = false;
-var offline_num_in_row = 1;
-var offline_total_rows = 1;
-var num_issues_to_display = 12;
-var offline_num_in_row_previous = 1337;
+var offline_num_in_row = 1; // Number of issues in a single row (may change on resize)
+var offline_total_rows = 1; // Number of total rows (calculated based on the number of total issues to display)
+var num_issues_to_display = 4; // Number of total issues
+var num_issues_to_display_max = num_issues_to_display;
+var offline_num_in_row_previous = 1337; // The previous number of total issues
 
 $(function() {
     $('.filter-year').on('click', function(e) {
@@ -10,17 +10,13 @@ $(function() {
             e.preventDefault();
         else
             e.stop();
-        
-        if (!busy) {
-            busy = true;
+
+        // Swap classes
+        $("#filter-menu .active").removeClass("active");
+        $(this).parent().addClass("active");
             
-            // Swap classes
-            $("#filter-menu .active").removeClass("active");
-            $(this).parent().addClass("active");
-            
-            // The sort
-            filter($(this).html());
-        }
+        // The sort
+        filter($(this).html());
     });
     
     $('#filter-reset').on('click', function(e) {
@@ -29,20 +25,16 @@ $(function() {
         else
             e.stop();
         
-        if (!busy) {
-            busy = true;
-            if ($('#filter-menu .active').length > 0){
-                $('#filter-menu .active').removeClass('active');
-                $('.offline_issue:visible').fadeOut(400,function () {
-                    if ($(".offline_issue:animated").length === 0) {
-                        $('.offline_issue').fadeIn(400,function () {
-                            if ($(".offline_issue:animated").length === 0)
-                                busy = false;
-                        });
-                    }
-                });
-            }
-        }
+        num_issues_to_display = num_issues_to_display_max;
+        
+        $('.offline_issue').each(function() {
+            $obj = $(this);
+            if (!$obj.hasClass('displayable'))
+                $obj.addClass('displayable');
+        });
+        
+        init_offline(true);
+        
     });
     
     
@@ -84,14 +76,16 @@ $(function() {
                     clicked_index = $('.offline-nav').length-1;
             }
             
+            var num = 0;
             // Hide all the visible issues and fade in the new ones
             $('.offline_issue:visible').fadeOut(400,function () {
                 if ($(".offline_issue:animated").length === 0) {
 
-                    $('.offline_issue').each(function(i) {
-                        if (i >= (num_issues_to_display*parseInt(clicked_index)) && i < (num_issues_to_display*(parseInt(clicked_index)+1))) {
+                    $('.offline_issue.displayable').each(function() {
+                        if (num >= (num_issues_to_display*parseInt(clicked_index)) && num < (num_issues_to_display*(parseInt(clicked_index)+1))) {
                             $(this).fadeIn(400);
                         }
+                        num++;
                     });
                 }
             });
@@ -126,32 +120,35 @@ $(function() {
     });
     
     // Resize container 'n shit
-    init_offline();
+    init_offline(false);
     
     // Fikser paginator
     init_pageinator();
     
     // On resize
-    $(window).on("debouncedresize", init_offline);
+    $(window).on("debouncedresize", init_offline(false));
 });
 
 function filter(year) {
-    $('.offline_issue:visible').stop().fadeOut(350);
     $('.offline_issue').each(function () {
-        if (parseInt(year) == parseInt($(this).data('year'))) {
-            $(this).delay(400).fadeIn(400,function () {
-                if ($('.offline_issue:animated').length === 0) {
-                    busy = false;
-                }
-            });
+        $obj = $(this);
+        if (parseInt(year) == parseInt($obj.data('year'))) {
+            if (!$obj.hasClass('displayable'))
+                $obj.addClass('displayable');
+        }
+        else {
+            if ($obj.hasClass('displayable'))
+                $obj.removeClass('displayable');
         }
     });
+    
+    init_offline(true);
 }
 
-function init_offline() {
+function init_offline(state) {
     // Checking to see if offline_rows_minimum is greater than the number of issues
-    if ($('.offline_issue').length < num_issues_to_display)
-        num_issues_to_display = $('.offline_issue').length;
+    if ($('.offline_issue.displayable').length < num_issues_to_display)
+        num_issues_to_display = $('.offline_issue.displayable').length;
     
     console.log(num_issues_to_display);
     // Number of issues in one row (this can change based on the width of the page)
@@ -166,13 +163,13 @@ function init_offline() {
     $('#offline-wrapper').css({height: (offline_rows_minimum*230)},400);
     
     // Storing total number of rows with issues
-    offline_total_rows = parseInt(Math.ceil($('.offline_issue').length/(offline_num_in_row*offline_rows_minimum)));
+    offline_total_rows = parseInt(Math.ceil($('.offline_issue.displayable').length/(offline_num_in_row*offline_rows_minimum)));
     
     // Checking if we need to trigger a click and repaint the menu
     if (offline_num_in_row_previous == 1337)
         offline_num_in_row_previous = offline_total_rows;
     else {
-        if (offline_num_in_row_previous != offline_total_rows) {
+        if (offline_num_in_row_previous != offline_total_rows || state) {
             offline_num_in_row_previous = offline_total_rows;
             init_pageinator();
         }
