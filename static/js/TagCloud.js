237ff44fbea83ff4d01d9a8bdd57b8bb5fbf8f17
Jonas Svarvaa <jonassvarvaa@gmail.com>
2013-04-17 20:06:07 +0200

    Added font-weight changes to TagCloud.

--- static/js/TagCloud.js
+++ static/js/TagCloud.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
     $('div.tagcloud span').each(function () {
         frequency = $(this).data('frequency');
-        $(this).css("fontSize", (frequency / 10 < 1) ? frequency / 10 + 1 + "em": (frequency / 10 > 2) ? "2em" : frequency / 10 + "em");
+        rel = (frequency / 10 < 1) ? frequency / 10 + 1: (frequency / 10 > 2) ? "2" : frequency / 10;
+        $(this).css("font-size", rel + 'em');
+        if(rel < 1.2) {
+            rel = 400;
+        } else if(rel < 1.5) {
+            rel = 600;
+        } else if(rel < 1.7) {
+            rel = 700;
+        } else {
+            rel = 800;
+        }
+        $(this).css("font-weight", rel);
     });
 });
