12acf648b412e963b6b1d3e79b6e0f7b7c3bc033
Jonas Svarvaa <jonassvarvaa@gmail.com>
2013-04-17 22:35:33 +0200

    Added links and stuff for years and months. No filter functionality implemented yet though.

--- apps/article/views.py
+++ apps/article/views.py
@@ -17,11 +17,10 @@ def index(request):
         else:
             latest.i = 1
         i += 1
-    
-    
+
     return render_to_response('article/index.html', {'featured' : featured[0], 'latest': latestNews}, context_instance=RequestContext(request))
 
-def archive(request, name=None, slug=None):
+def archive(request, name=None, slug=None, year=None, month=None):
     """
     Parameters
     ----------
@@ -29,9 +28,40 @@ def archive(request, name=None, slug=None):
         Tag name
     slug:
         Tag slug
+    year:
+        Article year (published_date)
+    month:
+        Article month (published_date)
     """
 
     articles = Article.objects.all()
+
+    month_strings = {
+        '1': u'Januar',
+        '2': u'Februar',
+        '3': u'Mars',
+        '4': u'April',
+        '5': u'Mai',
+        '6': u'Juni',
+        '7': u'Juli',
+        '8': u'August',
+        '9': u'September',
+        '10': u'Oktober',
+        '11': u'November',
+        '12': u'Desember',
+    }
+
+    dates = {}
+    for article in articles:
+        year = str(article.published_date.year)
+        month = str(article.published_date.month)
+        if year not in dates:
+            dates[year] = []
+        for y in dates:
+            if year == y:
+                if month_strings[month] not in dates[year]:
+                    dates[year].append(month_strings[month])
+
     if name:
         filtered = []
         for article in articles:
@@ -41,7 +71,11 @@ def archive(request, name=None, slug=None):
         articles = filtered
 
     tags = Tag.objects.all().order_by('?')
-    return render_to_response('article/archive.html', {'articles' : articles, 'tags' : tags } ,context_instance=RequestContext(request))
+
+    return render_to_response('article/archive.html', {'articles': articles, 'tags': tags, 'dates': dates } ,context_instance=RequestContext(request))
+
+def archive_tag(request, name, slug):
+    return archive(request, name=name, slug=slug)
 
 def details(request, article_id):
 	article = get_object_or_404(Article, pk=article_id)
