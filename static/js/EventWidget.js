7b4ad6c63f872676470baeb9d716364f86641f23
Espen Jacobsson <espenjacobsson@gmail.com>
2013-05-09 15:38:41 +0000

    updated holder.js and fixed frontpage event holder.js injection

--- static/js/EventWidget.js
+++ static/js/EventWidget.js
@@ -52,12 +52,16 @@ function EventWidget (Utils){
      * @return string
      */
     function createEventItem(item) {
+
+        var holderImage = $("<img>").attr({ "data-src": "holder.js/120x65" })
+
         html = '<div class="span6">';
         html +=     '<div class="span1 event-type-' + item.event_type + '">';
         html +=         '<div class="row-fluid"><span class="event-calendar-date">' + moment(item.event_start).format('DD') + '</span></div>';
         html +=         '<div class="row-fluid"><span class="event-calendar-month">' + moment(item.event_start).format('MMM') + '</span></div>';
         html +=     '</div>';
-        html +=     '<div class="span3"><img src="/static/js/holder.js/120x65" alt="" /></div>';
+    /*    html +=     '<div class="span3"><img src="/static/js/holder.js/120x65" alt="" /></div>';*/
+        html += '<div class="span3">' + holderImage[0].outerHTML + "</div>";
         html +=     '<div class="span8">';
         html +=         '<div class="event-title">' + item.title + '</div>';
         html +=         '<div class="event-ingress">' + item.ingress + '</div>';
