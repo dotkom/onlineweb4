d2ae831a8626e0f636090fd6534d72c3ad304619
thomasgautvedt <thomasgautv@hotmail.com>
2013-05-10 11:53:13 +0200

    Added field for Vimeo-id in article

--- apps/article/models.py
+++ apps/article/models.py
@@ -18,6 +18,7 @@ class Article(models.Model):
     image = FileBrowseField(_(u"bilde"), 
         max_length=200, directory=IMAGE_FOLDER, blank=True,
         extensions=IMAGE_EXTENSIONS, null=True)
+    video_id = models.CharField(_("vimeo id"), max_length=200, blank=True)
     created_date = models.DateTimeField(_(u"opprettet-dato"), auto_now_add=True, editable=False)
     changed_date = models.DateTimeField(_(u"sist endret"), editable=False, auto_now=True)
     published_date = models.DateTimeField(_(u"publisert"))
