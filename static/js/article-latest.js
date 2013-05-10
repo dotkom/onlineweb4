6ef106b0dbb7405ba143c4b6cd7083ff9e0dda3b
thomasgautvedt <thomasgautv@hotmail.com>
2013-05-10 13:08:34 +0200

    Fixed stupid mistake causing errors

--- static/js/article-latest.js
+++ static/js/article-latest.js
@@ -1,4 +1,5 @@
 $(function() {
+    var utils = new Utils(); // Class for the Widget
     var articleWidget = new ArticleWidget(utils);
     
     // Loading the lastest articles from the api to display on the right
