3d015e314e819b5eb5d05e88d2224c5ad3da11b9
Espen Jacobsson <espenjacobsson@gmail.com>
2013-05-11 18:48:06 +0200

    added actual libs to bootstrap

--- vagrantbootstrap.sh
+++ vagrantbootstrap.sh
@@ -1,2 +1,2 @@
 apt-get update
-apt-get install -y python-dev python-setuptools python-virtualenv vim virtualenvwrapper git screen libjpeg zlib libtiff libfreetype libwebp
+apt-get install -y python-dev python-setuptools python-virtualenv vim virtualenvwrapper git screen libjpeg8 zlib-bin libtiff4 libfreetype6 libwebp2
