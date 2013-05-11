e578a528caba867cb69b27a3d2cf98663277e210
Espen Jacobsson <espenjacobsson@gmail.com>
2013-05-11 03:47:33 +0200

    adding vagrant configuration files

---
+++ vagrantbootstrap.sh
@@ -0,0 +1,8 @@
+apt-get update
+apt-get install -y python-dev
+apt-get install -y python-setuptools
+apt-get install -y python-virtualenv
+apt-get install -y vim
+apt-get install -y virtualenvwrapper
+apt-get install -y git
+apt-get install -y screen
