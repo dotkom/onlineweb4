faf66b9c61ef786063b2a8b2007b882ddba859ac
Espen Jacobsson <espenjacobsson@gmail.com>
2013-05-11 05:50:59 +0200

    faster installing of packages

--- vagrantbootstrap.sh
+++ vagrantbootstrap.sh
@@ -1,8 +1,2 @@
 apt-get update
-apt-get install -y python-dev
-apt-get install -y python-setuptools
-apt-get install -y python-virtualenv
-apt-get install -y vim
-apt-get install -y virtualenvwrapper
-apt-get install -y git
-apt-get install -y screen
+apt-get install -y python-dev python-setuptools python-virtualenv vim virtualenvwrapper git screen
