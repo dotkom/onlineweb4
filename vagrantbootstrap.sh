9b22b5a8c81321bb952ce321cfda8ededb4a5620
Espen Jacobsson <espenjacobsson@gmail.com>
2013-05-11 18:38:40 +0200

    adding dependencies to vagrant bootstrap script

--- vagrantbootstrap.sh
+++ vagrantbootstrap.sh
@@ -1,2 +1,2 @@
 apt-get update
-apt-get install -y python-dev python-setuptools python-virtualenv vim virtualenvwrapper git screen
+apt-get install -y python-dev python-setuptools python-virtualenv vim virtualenvwrapper git screen libjpeg zlib libtiff libfreetype libwebp
