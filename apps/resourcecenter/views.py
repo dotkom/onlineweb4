eb3d23771157c7bffee285914b2fb0cc9c8f2fb2
Michael Johansen <michael@informatikk.org>
2013-05-12 17:38:55 +0200

    Added index to views.py for Resourcecenter

--- apps/resourcecenter/views.py
+++ apps/resourcecenter/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+#-*- coding: utf-8 -*-
+from django.http import HttpResponse
+# from django.shortcuts import render_to_response
+# from django.shortcuts import get_object_or_404
+# from django.template import RequestContext
+# from apps.resourcecenter.models import Event, AttendanceEvent, Attendee
+# import datetime
+
+
+def index(request):
+	return HttpResponse('HEI! :D:D:D::D:D:D:D')
+	# return render_to_response('resourcecenter/index.html', {'resourcecenter': resourcecenter}, context_instance=RequestContext(request))
