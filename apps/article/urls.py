bdcd3ee2aa44f95564031dc794228173bb4c7270
Edgar Vedvik <edgar.vedvik@gmail.com>
2013-05-16 21:49:46 +0200

    added OAuth authorization with vimeo

--- apps/article/urls.py
+++ apps/article/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('apps.article.views',
     url(r'^$', 'index', name='article_index'),
     url(r'^(?P<article_id>\d+)/$', 'details', name='article_details'),
+    url(r'^vimeo/', 'vimeoUpload', name='article_vimeo'),
 )
--- apps/article/views.py
+++ apps/article/views.py
@@ -2,6 +2,7 @@ from django.template import Template, Context, loader, RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
 from models import Article
+import vimeo
 
 def index(request):
     # Featured
@@ -21,6 +22,26 @@ def index(request):
     
     return render_to_response('article/index.html', {'featured' : featured[0], 'latest': latestNews}, context_instance=RequestContext(request))
 
+def vimeoUpload(request):
+    if request.session.get('has_request_token', True) and request.session.get('has_secret_token', True):
+        request.session['has_request_token'] = False 
+        client = vimeo.Client(key='c60b0e891d1712b6e86534bcdab319f257a814dd', secret='b2e65f399539d43c9e124afd93476ac95f263b2b', callback='http://127.0.0.1:8000/article/vimeo/', username='user16310918', token = False)
+        oauth_verif = request.GET.get('oauth_verifier','')
+        token = client.exchange_token(oauth_verif)
+        client = vimeo.Client(key='c60b0e891d1712b6e86534bcdab319f257a814dd', secret='b2e65f399539d43c9e124afd93476ac95f263b2b', callback='http://127.0.0.1:8000/article/vimeo/', username='user16310918', token = True)
+        videos = client.get('vimeo.videos.upload.getQuota')
+        print(videos)
+        return render_to_response('article/vimeo.html', context_instance=RequestContext(request))
+
+    else:
+
+        client = vimeo.Client(key='c60b0e891d1712b6e86534bcdab319f257a814dd', secret='b2e65f399539d43c9e124afd93476ac95f263b2b', callback='http://127.0.0.1:8000/article/vimeo/', username='user16310918')
+        request.session['has_request_token'] = True
+        request.session['has_secret_token'] = True
+
+        return render_to_response('article/vimeo.html', {'authorize_url' : client.authorize_url() }, context_instance=RequestContext(request))
+
+    
 def details(request, article_id):
 	article = get_object_or_404(Article, pk=article_id)
 	
