96992a8d46697c2ef74185710c2428f69e952ad6
Michael Johansen <michael@informatikk.org>
2013-05-21 20:06:22 +0200

    Added view methods and urls for all pages

--- apps/resourcecenter/urls.py
+++ apps/resourcecenter/urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls import patterns, include, url 
 
 urlpatterns = patterns('apps.resourcecenter.views',
+	# Index page
     url(r'^$', 'index', name='resourcecenter_index'),
+    # Subpages
+    url(r'^$', 'notifier', name='resourcecenter_notifier'),
+    url(r'^$', 'mailinglists', name='resourcecenter_mailinglists'),
+    url(r'^$', 'infopages', name='resourcecenter_infopages'),
+    url(r'^$', 'gameservers', name='resourcecenter_gameservers'),
+    url(r'^$', 'githubrepos', name='resourcecenter_githubrepos'),
+    url(r'^$', 'irc', name='resourcecenter_irc'),
 )
