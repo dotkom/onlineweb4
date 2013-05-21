ae481f4cd932a33ffc8915d6b75baef6ca3fb7e0
Michael Johansen <michael@informatikk.org>
2013-05-21 20:48:35 +0200

    Added proper paths to urls.py

--- apps/resourcecenter/urls.py
+++ apps/resourcecenter/urls.py
@@ -4,10 +4,10 @@ urlpatterns = patterns('apps.resourcecenter.views',
 	# Index page
     url(r'^$', 'index', name='resourcecenter_index'),
     # Subpages
-    url(r'^$', 'notifier', name='resourcecenter_notifier'),
-    url(r'^$', 'mailinglists', name='resourcecenter_mailinglists'),
-    url(r'^$', 'infopages', name='resourcecenter_infopages'),
-    url(r'^$', 'gameservers', name='resourcecenter_gameservers'),
-    url(r'^$', 'githubrepos', name='resourcecenter_githubrepos'),
-    url(r'^$', 'irc', name='resourcecenter_irc'),
+    url(r'^notifier/$', 'notifier', name='resourcecenter_notifier'),
+    url(r'^mailinglists/$', 'mailinglists', name='resourcecenter_mailinglists'),
+    url(r'^infopages/$', 'infopages', name='resourcecenter_infopages'),
+    url(r'^gameservers/$', 'gameservers', name='resourcecenter_gameservers'),
+    url(r'^githubrepos/$', 'githubrepos', name='resourcecenter_githubrepos'),
+    url(r'^irc/$', 'irc', name='resourcecenter_irc'),
 )
