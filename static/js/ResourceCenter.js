aef0f73dbba02de602a1789f80158e288cf1a2ef
Michael Johansen <michael@informatikk.org>
2013-06-12 21:59:53 +0200

    Got proper card switching working! - Basically, when you click a resource (the facade div) it slides away to expose details about the resource (the details div).

--- static/js/ResourceCenter.js
+++ static/js/ResourceCenter.js
@@ -1,24 +1,38 @@
+var RS_DEBUG = 1;
+
+function rslog(msg) {
+	if (RS_DEBUG) {
+		console.log('ResourceCenter:', msg);
+	}
+}
+
 // Wait till the document is ready
 $(function() {
 
 	var toggleDetails = function() {
 		var isToggled = $(this).find('.facade').css('top') == '0px';
 		if (isToggled) {
+			rslog('Show details for', $(this.id));
 			// Slide the facade up
 			$(this).find('.facade').animate({
 				top:'-200pt',
+				opacity:'0',
 			}, 150);
 			$(this).find('.details').animate({
 				top:'-200pt',
+				opacity:'1',
 			}, 150);
 		}
 		else {
+			rslog('Show facade', $(this.id));
 			// Slide the facade down
 			$(this).find('.facade').animate({
 				top:'0pt',
+				opacity:'1',
 			}, 150);
 			$(this).find('.details').animate({
 				top:'0pt',
+				opacity:'0',
 			}, 150);
 		}
 	};
