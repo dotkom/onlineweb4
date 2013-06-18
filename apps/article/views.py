9dda8b46e0b30f0f351722ed61137cf455af893a
Edgar Vedvik <edgar.vedvik@gmail.com>
2013-06-18 08:07:46 +0200

    files are now uploading like they should

--- apps/article/views.py
+++ apps/article/views.py
@@ -8,6 +8,7 @@ from apps.article.forms import UploadForm
 from apps.article.forms import createUploadForm
 import vimeo
 import simplejson as json
+import urllib2
 
 def index(request):
     # Featured
@@ -28,7 +29,7 @@ def index(request):
     return render_to_response('article/index.html', {'featured' : featured[0], 'latest': latestNews}, context_instance=RequestContext(request))
 
 def vimeoUpload(request):
-    if request.method == 'POST' and request.session.get('has_authorized', True):
+    if request.method == 'POST' and request.session.get('has_authorized', True) and 1 == 2:
         request.session['has_authorized'] = False
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
@@ -37,12 +38,21 @@ def vimeoUpload(request):
             else:
                 client = request.session['client']
                 ticket = json.loads(client.get('vimeo.videos.upload.getTicket', upload_method='streaming'))
+                opener = urllib2.build_opener(urllib2.HTTPHandler)
+                put_request = urllib2.Request(ticket['ticket']['endpoint'], data=request.FILES['docfile'])
+                put_request.add_header('Content-type', 'video/avi')
+                print(request.session.get('size_availiable'))
+                print(request.session['size_availiable'])
+                put_request.add_header('Content-Length', request.session.get('size_availiable'))
+                put_request.get_method = lambda: 'PUT'
+                url = opener.open(put_request)
                 print(ticket)
                 
         else:
             messages.error(request, _(u'Det oppstod en feil.'))
         return render_to_response('article/vimeo.html', {'message' : "upload is good"}, context_instance=RequestContext(request))
     elif request.session.get('has_authorized', True):
+        request.session['has_authorized'] = False
         oauth_verif = request.GET.get('oauth_verifier', '')
         client = vimeo.Client(key='c60b0e891d1712b6e86534bcdab319f257a814dd', secret='b2e65f399539d43c9e124afd93476ac95f263b2b', callback='http://127.0.0.1:8000/article/vimeo/', username='user16310918', token = False)
         token = client.exchange_token(oauth_verif)
@@ -50,8 +60,10 @@ def vimeoUpload(request):
         request.session['client'] = client
         size_availiable = json.loads(client.get('vimeo.videos.upload.getQuota'))['user']['upload_space']['free']
         request.session['size_availiable'] = size_availiable
-        form = createUploadForm(size_availiable)
-        return render_to_response('article/vimeo.html', {'form' : form, 'size_availiable' : size_availiable }, context_instance=RequestContext(request))
+        response = json.loads(client.get('vimeo.videos.upload.getTicket'))
+        ticket = response['ticket']['endpoint']
+        ticket_id = response['ticket']['id']
+        return render_to_response('article/vimeo.html', {'size_availiable' : size_availiable, 'upload_ticket' : ticket, 'ticket_id' : ticket_id  }, context_instance=RequestContext(request))
     else:
         print("start")
         client = vimeo.Client(key='c60b0e891d1712b6e86534bcdab319f257a814dd', secret='b2e65f399539d43c9e124afd93476ac95f263b2b', callback='http://127.0.0.1:8000/article/vimeo/', username='user16310918')
