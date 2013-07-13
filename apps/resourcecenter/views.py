3fc909b1f44e391e4c3e1debe616137f3336ba22
Michael Johansen <michael@informatikk.org>
2013-07-13 04:27:23 +0200

    views: removing unused url paths

--- apps/resourcecenter/views.py
+++ apps/resourcecenter/views.py
@@ -7,15 +7,5 @@ from django.template import RequestContext
 def index(request):
 	return render_to_response('resourcecenter/index.html', context_instance=RequestContext(request))
 # Subpages
-def notifier(request):
-	return render_to_response('resourcecenter/notifier.html', context_instance=RequestContext(request))
 def mailinglists(request):
 	return render_to_response('resourcecenter/mailinglists.html', context_instance=RequestContext(request))
-def infopages(request):
-	return render_to_response('resourcecenter/infopages.html', context_instance=RequestContext(request))
-def gameservers(request):
-	return render_to_response('resourcecenter/gameservers.html', context_instance=RequestContext(request))
-# def githubrepos(request):
-# 	return render_to_response('resourcecenter/githubrepos.html', context_instance=RequestContext(request))
-def irc(request):
-	return render_to_response('resourcecenter/irc.html', context_instance=RequestContext(request))
