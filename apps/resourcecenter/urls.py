8677f52f5ba5876c1781b17986e1f1e29e81bd69
Michael Johansen <michael@informatikk.org>
2013-07-13 04:27:56 +0200

    urls: removing unused url paths

--- apps/resourcecenter/urls.py
+++ apps/resourcecenter/urls.py
@@ -4,10 +4,5 @@ urlpatterns = patterns('apps.resourcecenter.views',
 	# Index page
     url(r'^$', 'index', name='resourcecenter_index'),
     # Subpages
-    url(r'^notifier/$', 'notifier', name='resourcecenter_notifier'),
     url(r'^mailinglists/$', 'mailinglists', name='resourcecenter_mailinglists'),
-    url(r'^infopages/$', 'infopages', name='resourcecenter_infopages'),
-    url(r'^gameservers/$', 'gameservers', name='resourcecenter_gameservers'),
-    # url(r'^githubrepos/$', 'githubrepos', name='resourcecenter_githubrepos'),
-    url(r'^irc/$', 'irc', name='resourcecenter_irc'),
 )
