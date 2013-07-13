da4f0af21a444d60fce8ff1e98b1d6c0e0e10d70
Michael Johansen <michael@informatikk.org>
2013-07-13 14:08:04 +0200

    template, js and css for /resourcecenter/gameservers

---
+++ static/js/Gameservers.js
@@ -0,0 +1,12 @@
+var GS_DEBUG = 1;
+
+function gslog(msg) {
+	if (ML_DEBUG) {
+		console.log('Resourcecenter/Gameservers:', msg);
+	}
+}
+
+// Wait till the document is ready
+$(function() {
+	// nothing here yet
+});
