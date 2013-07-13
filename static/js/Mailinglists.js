9c9c46a9225addc3c3e496d922a34e40ba064467
Michael Johansen <michael@informatikk.org>
2013-07-13 13:29:37 +0200

    js: new file Mailinglists.js, is an empty shell so far

---
+++ static/js/Mailinglists.js
@@ -0,0 +1,12 @@
+var ML_DEBUG = 1;
+
+function mllog(msg) {
+	if (ML_DEBUG) {
+		console.log('Mailinglists:', msg);
+	}
+}
+
+// Wait till the document is ready
+$(function() {
+	// nothing here yet
+});
