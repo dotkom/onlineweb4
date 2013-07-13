e6c1eeb6ad7b30f1dd46f48946cd941ece26d8d7
Michael Johansen <michael@informatikk.org>
2013-07-13 22:08:26 +0200

    removed mailinglists from views.py in resourcecenter #81 - now that mailinglists is a free elf and all...

--- apps/resourcecenter/views.py
+++ apps/resourcecenter/views.py
@@ -7,7 +7,5 @@ from django.template import RequestContext
 def index(request):
 	return render_to_response('resourcecenter/index.html', context_instance=RequestContext(request))
 # Subpages
-def mailinglists(request):
-    return render_to_response('resourcecenter/mailinglists.html', context_instance=RequestContext(request))
 def gameservers(request):
 	return render_to_response('resourcecenter/gameservers.html', context_instance=RequestContext(request))
