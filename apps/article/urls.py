bcced6fab18f13aef660f31070b32fb57c1b5be5
lizter <christian@strandyoung.com>
2013-07-29 22:52:42 +0000

    Removing unused index url in article

--- apps/article/urls.py
+++ apps/article/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('apps.article.views',
-    url(r'^$', 'index', name='article_index'),
+    #url(r'^$', 'index', name='article_index'),
     url(r'^archive$', 'archive', name='article_archive'),
     url(r'^(?P<article_id>\d+)/$', 'details', name='article_details'),
     url(r'^tag/(?P<name>[^\.]+)/(?P<slug>[^\.]+)', 'archive_tag', name='view_article_tag'),
