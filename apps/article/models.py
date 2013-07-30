f41069f2ab2b9a8570bc01375deffdfaf8837deb
Nils Herde <mail@hernil.com>
2013-07-30 18:10:20 +0000

    Removed a field limitation which had no purpose No longer needed. We now have short and long ingress so the limitation was not needed (as well as not supported for TextField datatype)

--- apps/article/models.py
+++ apps/article/models.py
@@ -15,7 +15,7 @@ class Article(models.Model):
 
     heading = models.CharField(_(u"tittel"), max_length=200)
     ingress_short = models.CharField(_(u"kort ingress"), max_length=100)
-    ingress = models.TextField(_(u"ingress"), max_length=225)
+    ingress = models.TextField(_(u"ingress"))
     content = models.TextField(_(u"content"))
     image = FileBrowseField(_(u"bilde"), 
         max_length=200, directory=IMAGE_FOLDER,
