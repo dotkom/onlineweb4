0a491cb5b9b2a5706fc2f3e43ddcd2ec6d253d09
Torkil Rein Gustavsen <torkil@gmail.com>
2013-08-06 00:27:33 +0200

    Adds friendlier event_type identifiers, pagination and filters
    Uses a proper Backbone router for filtering and pagination
    instead of queryString. This creates a dependency to
    Backbone's router abstraction, but allows for more
    extendable and elegant filtering and pagination.

--- static/js/event_widget/scripts/controller.js
+++ static/js/event_widget/scripts/controller.js
@@ -26,33 +26,5 @@ define(function(require) {
         this[name] = method;
     };
 
-    /*
-     * Converts a queryString such as '?event_type=1&author=foo' to
-     * object {event_type: 1, author: foo}
-     */
-
-    Controller.prototype.extractParams = function(queryString) {
-        var params = {};
-        if (queryString) {
-            _.each(
-                _.map(decodeURI(queryString).split(/&/g), function(el){
-                    var aux = el.split('='), o = {};
-                    if (aux.length >= 1){
-                        var val;
-                        if (aux.length === 2) {
-                            val = aux[1];
-                        }
-                        o[aux[0]] = val;
-                    }
-                    return o;
-                }),
-                function(o){
-                    _.extend(params,o);
-                }
-            );
-        }
-        return params;
-    };
-
     return Controller;
 });
--- static/js/event_widget/scripts/router.js
+++ static/js/event_widget/scripts/router.js
@@ -8,11 +8,10 @@ define(function(require) {
     var Controller = require('controller');
 
     var WidgetController = new Controller({
-        index: function(queryString) {
-            var params = this.extractParams(queryString);
 
+        index: function() {
             var events = new EventCollection();
-            var xhr = events.fetch({ data: params });
+            var xhr = events.fetch();
             
             xhr.done(function() {
                 var widget = new Widget({collection: events});
@@ -23,13 +22,45 @@ define(function(require) {
                 // XXX: TODO: Handle failure
             });
 
+        },
+
+        filtered: function(eventType, limit, offset) {
+            // XXX: This kind of coupling is pretty, pretty
+            // awful. The client have no business knowing the
+            // internals of our data model. An identifier
+            // should suffice, yes?
+            var eventTypes = {
+                'sosialt': 1,
+                'bedpres': 2,
+                'kurs': 3,
+                'utflukt': 4,
+                'internt': 5,
+                'annet': 6
+            };
+
+            // This makes the available options explicit for
+            // clarity's sake. The Collection itself should have
+            // sane defaults if instantiated withhout parameters.
+            var events = new EventCollection({
+                eventType: eventTypes[eventType],
+                limit: limit,
+                offset: offset
+            });
+            var xhr = events.fetch();
+
+            xhr.done(function() {
+                var widget = new Widget({collection: events});
+                RegionManager.main.show(widget);
+            });
         }
     });
 
     var WidgetRouter = Marionette.AppRouter.extend({
         controller: WidgetController,
+
         appRoutes: {
-            '(/)?:queryString': 'index'
+            '': 'index',
+            ':eventType(/)(:limit)(/)(:offset)': 'filtered'
         }
     });
 
