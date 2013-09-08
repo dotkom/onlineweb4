428f2e393ffb3fb6fe1610fab9577552b8a63f19
Håvard Slettvold <slettvold@gmail.com>
2013-09-08 03:34:19 +0200

    Added middleware to settings.

--- onlineweb4/settings/base.py
+++ onlineweb4/settings/base.py
@@ -100,6 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'middleware.http.Http403Middleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
