32eea3ab7fbb83371d6c364bc3c576bc689c08aa
Håvard Slettvold <slettvold@gmail.com>
2013-09-08 03:38:39 +0200

    Added admin entry for allowed usernames.

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -9,14 +9,14 @@ class OnlineUserAdmin(admin.ModelAdmin):
     model = OnlineUser
     list_display = ['username', 'first_name', 'last_name', 'field_of_study', 'is_member',]
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    fieldsets = ((None, { 'fields': ('username', 'password',) }),
-        (_('Personal info'), { 'fields': ('first_name', 'last_name', 'email', 'phone_number',) }),
-        (_('Studieinformasjon'), { 'fields': ('ntnu_username', 'field_of_study', 'started_date', 'compiled',) }),
-        (_('Address'), { 'fields': ('address', 'zip_code',) }),
-        (_('Important dates'), { 'fields': ('last_login','date_joined',)}),
-        (_('Allergies'), { 'fields': ('allergies',) }),
-        (_('Other info'), { 'fields': ('infomail', 'mark_rules', 'rfid', 'nickname', 'website', 'image',) }),
-        (_('Permissions'), { 'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',) }),
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_(u'Personal info'), {'fields': ('first_name', 'last_name', 'email', 'phone_number', )}),
+        (_(u'Studieinformasjon'), {'fields': ('ntnu_username', 'field_of_study', 'started_date', 'compiled',)}),
+        (_(u'Address'), {'fields': ('address', 'zip_code',)}), 
+        (_(u'Important dates'), {'fields': ('last_login', 'date_joined',)}),
+        (_(u'Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
     )
     filter_horizontal = ('groups', 'user_permissions',)
 
@@ -24,5 +24,11 @@ admin.site.register(OnlineUser, OnlineUserAdmin)
 
 class AllowedUsernameAdmin(admin.ModelAdmin):
     model = AllowedUsername
+    list_display = ('username', 'registered', 'expiration_date', 'note')
+    fieldsets = (
+        (None, {'fields': ('username', 'registered', 'expiration_date')}),
+        (_(u'Notater'), {'fields': ('note', 'description')}),
+
+    )
 
 admin.site.register(AllowedUsername, AllowedUsernameAdmin)
