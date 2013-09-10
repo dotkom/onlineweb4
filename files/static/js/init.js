ec28c8620768dcd4ca29b9b96e116333eefe8fb8
thomasgautvedt <thomasgautv@hotmail.com>
2013-09-10 22:12:51 +0200

    Nice try for a white guy at making a usable map with styling

--- files/static/js/init.js
+++ files/static/js/init.js
@@ -1,6 +1,94 @@
-
-$(function()
-{
+$(function() {
     $('.dropdown-toggle').dropdown();
+    
+    var map = new google.maps.Map(document.getElementById("footer-map"),{
+    center: new google.maps.LatLng(63.41819751959266, 10.40592152481463),
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [{
+        featureType: "all",
+        stylers: [{
+            saturation: -100
+        }]
+    }, {
+        stylers: [{
+            hue: "#545454"
+        }, {
+            lightness: -30
+        }]
+    }, {
+        "featureType": "poi.school",
+        "stylers": [{
+            "color": "#555555"
+        }]
+    }, {
+        "featureType": "poi.business",
+        "stylers": [{
+            "color": "#3366aa"
+        }, {
+            "weight": 0.5
+        }]
+    }, {
+        "featureType": "poi.park",
+        "stylers": [{
+            "color": "#bbcfbb"
+        }]
+    }, {
+        "featureType": "poi.sports_complex",
+        "stylers": [{
+            "saturation": 4
+        }, {
+            "weight": 0.5
+        }, {
+            "color": "#bbcfbb"
+        }]
+    }]
+});
 
+/*
+[
+  {
+    "featureType": "poi.school",
+    "stylers": [
+      { "color": "#555555" }
+    ]
+  },{
+    "featureType": "poi.sports_complex",
+    "stylers": [
+      { "saturation": 4 },
+      { "weight": 0.5 },
+      { "color": "#bbcfbb" }
+    ]
+  },{
+    "featureType": "poi.business",
+    "elementType": "labels.text",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#3366aa" },
+      { "weight": 1.6 },
+      { "saturation": 100 },
+      { "lightness": 11 }
+    ]
+  },{
+    "featureType": "poi.park",
+    "stylers": [
+      { "color": "#bbcfbb" }
+    ]
+  },{
+    "featureType": "poi.business",
+    "stylers": [
+      { "color": "#3366aa" },
+      { "weight": 0.5 }
+    ]
+  }
+]
+*/
+
+// Henter alle markere til kartet
+var tempMarker0 = new google.maps.Marker({
+    map: map,
+    position: new google.maps.LatLng(63.41819751959266, 10.40592152481463),
+    //icon: 'assets/css/gfx/kartikoner/overnatting.png',
+    visible: true
+});
 });
