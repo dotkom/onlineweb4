14de982d9b7a1ce3c74eca780c65acf743589371
thomasgautvedt <thomasgautv@hotmail.com>
2013-09-17 19:22:25 +0200

    Added new url-pattern

--- apps/article/urls.py
+++ apps/article/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('apps.article.views',
     #url(r'^$', 'index', name='article_index'),
     url(r'^archive$', 'archive', name='article_archive'),
-    url(r'^(?P<article_id>\d+)/$', 'details', name='article_details'),
+    url(r'^(?P<article_id>\d+)/(.*)$', 'details', name='article_details'),
     url(r'^tag/(?P<name>[^\.]+)/(?P<slug>[^\.]+)', 'archive_tag', name='view_article_tag'),
     url(r'^year/(?P<year>\d+)/$', 'archive_year', name='article_archive_year'),
     url(r'^year/(?P<year>\d+)/month/(?P<month>[^\.]+)/$', 'archive_month', name='article_archive_month'),
