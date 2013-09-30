ff0ef5254b065598c4c2f54a5bd054ca75fd1466
Espen Jacobsson <espenjacobsson@gmail.com>
2013-09-30 15:19:56 +0200

    remove unused import

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from filebrowser.fields import FileBrowseField
 
 FIELD_OF_STUDY_CHOICES = (
     (0, '--'),
