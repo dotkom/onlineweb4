766561d6a5c41c399f8c4d59f6f2f6d26fdd7f3a
Espen Jacobsson <espenjacobsson@gmail.com>
2013-09-30 15:18:46 +0200

    refactor admin.py

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -9,65 +9,14 @@ class OnlineUserAdmin(admin.ModelAdmin):
     model = OnlineUser
     list_display = ['username', 'first_name', 'last_name', 'field_of_study', 'is_online',]
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    fieldsets = (
-        (None, {
-            'fields': (
-                'username',
-                'password'
-            )
-        }),
-        (_('Personal info'), {
-            'fields': (
-                'first_name',
-                'last_name',
-                'email',
-                'phone_number',
-            )
-        }),
-        (_('Studieinformasjon'), {
-            'fields': (
-                'ntnu_username',
-                'field_of_study',
-                'started_date',
-                'compiled',
-            )
-        }),
-        (_('Address'), {
-            'fields': (
-                'address',
-                'zip_code',
-            )
-        }),
-        (_('Important dates'), {
-            'fields': (
-                'last_login',
-                'date_joined',
-            )
-        }),
-        (_('Allergies'), {
-           'fields': (
-               'allergies',
-           )
-        }),
-        (_('Other info'), {
-           'fields': (
-               'infomail',
-               'mark_rules',
-               'rfid',
-               'nickname',
-               'website',
-               'image',
-           )
-        }),
-        (_('Permissions'), {
-            'fields': (
-                'is_active',
-                'is_staff',
-                'is_superuser',
-                'groups',
-                'user_permissions',
-            )
-        }),
+    fieldsets = ((None, { 'fields': ('username', 'password',) }),
+        (_('Personal info'), { 'fields': ('first_name', 'last_name', 'email', 'phone_number',) }),
+        (_('Studieinformasjon'), { 'fields': ('ntnu_username', 'field_of_study', 'started_date', 'compiled',) }),
+        (_('Address'), { 'fields': ('address', 'zip_code',) }),
+        (_('Important dates'), { 'fields': ('last_login','date_joined',)}),
+        (_('Allergies'), { 'fields': ('allergies',) }),
+        (_('Other info'), { 'fields': ('infomail', 'mark_rules', 'rfid', 'nickname', 'website', 'image',) }),
+        (_('Permissions'), { 'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',) }),
     )
     filter_horizontal = ('groups', 'user_permissions',)
 
