e007cb17163f3c9e03c2f4596aab70a4b381144e
Håkon <torrib90@gmail.com>
2013-10-01 22:53:32 +0200

    Started creating tests

--- apps/feedback/tests.py
+++ apps/feedback/tests.py
@@ -7,10 +7,27 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from django.utils.translation import ugettext_lazy as _
-
+from apps.feedback.feedback_mail import FeedbackMail, Message
+from apps.feedback.models import Feedback, FeedbackRelation, Attendee
+from apps.events.models import Event, AttendanceEvent
+from apps.authentication.models import OnlineUser as User
+from datetime import datetime
 
 class SimpleTest(TestCase):
     fixtures = ["test_feedback_fixture.json"]
+
+    def setUp(self):
+        user1 = User.objects.create(username="user1", email="user1@mail.com")
+        user2 = User.objects.create(username="user2", email="user2@mail.com")
+        event1 = Event.objects.create(title="Bedpress", event_start = datetime.today(), event_end= datetime.today(), event_type = 2, author = user1)
+        event2 = Event.objects.create(title="Kurs", event_start = datetime.today(), event_end= datetime.today(), event_type = 3, author = user1)
+        event3 = Event.objects.create(title="Sosial Happening", event_start = datetime.today(), event_end= datetime.today(), event_type = 1, author = user1)
+        attendanceEvent1 = AttendanceEvent.objects.create(registration_start = datetime.today(), registration_end = datetime.today(), event = event1)
+        attendanceEvent2 = AttendanceEvent.objects.create(registration_start = datetime.today(), registration_end = datetime.today(), event = event2)
+        attendanceEvent3 = AttendanceEvent.objects.create(registration_start = datetime.today(), registration_end = datetime.today(), event = event3)
+        feedback = Feedback.objects.create(author = user1)
+        atendee1 = Attendee.objects.create(event = event1, user = user1)
+        atendee2 = Attendee.objects.create(event = event1, user = user2)
 
     def test_post_correct(self):
         #TODO: do eeet! test posted against db (Sigurd) 2013-02-08
