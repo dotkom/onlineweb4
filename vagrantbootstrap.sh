3ae5856f16f6c02d70f05772ab48ffa0281b26a9
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2013-10-03 23:29:56 +0200

    silent mode
    silent mode, toggle on/off in script
    don't try to reinstall if commands exist

--- vagrantbootstrap.sh
+++ vagrantbootstrap.sh
@@ -1,11 +1,53 @@
+#!/bin/bash
+
+
+LJUST_COLS=50
+RJUST_COLS=60
+VERBOSE=false
+
+
+function init() {
+    printf "\n\nVERBOSE = $VERBOSE, toggle in vagrantbootstrap.sh\n\n"
+    sleep 1
+
+    echo "killing running dev servers"
+    pkill -9 -f "python manage.py"
+}
+
+function progress() {
+    if $VERBOSE
+    then
+        $@
+    else
+        cmd="$@"
+        $@ &> /dev/null &
+        pid=$!
+        spinner='-\|/'
+
+        i=0
+        while kill -0 $pid &> /dev/null
+        do
+            i=$(( (i+1) %4 ))
+            printf "\r\t%-${LJUST_COLS}.${LJUST_COLS}s %${RJUST_COLS}s" "${cmd}" "[ ${spinner:$i:1}${spinner:$i:1} ]"
+            sleep .1
+        done
+
+        if [ $? -ne 0 ]
+        then
+            echo "return code $?";
+        fi
+        printf "\r\t%-${LJUST_COLS}.${LJUST_COLS}s %${RJUST_COLS}s\n" "${cmd}" "[ OK ]"
+    fi
+}
+
 function update_packages() {
     echo "updating packages"
-    sudo apt-get update
+    progress sudo apt-get update
 }
 
 function install_packages() {
     echo "installing packages"
-    sudo apt-get install -y \
+    progress sudo apt-get install -y \
         python-dev python-setuptools python-virtualenv vim \
         tmux screen git-core curl build-essential openssl \
         libjpeg8 libjpeg8-dev zlib-bin libtiff4 libtiff4-dev libfreetype6 libfreetype6-dev libwebp2 libpq-dev libssl-dev\
@@ -15,7 +57,7 @@ function install_packages() {
 function setup_virtualenv() {
     echo "installing virtualenvwrapper"
     # use pip to install globally, installing with apt doesn't create the shellscript for sourcing
-    sudo pip install virtualenvwrapper
+    progress sudo pip install virtualenvwrapper
     source /usr/local/bin/virtualenvwrapper.sh
 
     if ! grep -q "source /usr/local/bin/virtualenvwrapper.sh" .bashrc; then
@@ -24,14 +66,14 @@ function setup_virtualenv() {
     fi
 
     echo "creating virtualenv"
-    mkvirtualenv onlineweb
+    progress mkvirtualenv onlineweb
 }
 
 function install_onlineweb_requirements() {
     echo "installing onlineweb requirements"
     workon onlineweb
     cd /vagrant
-    pip install -r requirements.txt
+    progress pip install -r requirements.txt
 }
 
 
@@ -39,25 +81,31 @@ function install_lessc() {
     cd /home/vagrant
     workon onlineweb
     echo "installing node"
-    echo "downloading node v0.10.20"
-    wget http://nodejs.org/dist/v0.10.20/node-v0.10.20.tar.gz
-    tar zxf node-v0.10.20.tar.gz
+    if [ ! -f "node-v0.10.20.tar.gz" ]
+    then
+        if $VERBOSE
+        then
+            progress wget http://nodejs.org/dist/v0.10.20/node-v0.10.20.tar.gz
+        else
+            progress wget --quiet http://nodejs.org/dist/v0.10.20/node-v0.10.20.tar.gz
+        fi
+    fi
+    progress tar zxf node-v0.10.20.tar.gz
     cd node-v0.10.20/
     # set install prefix to virtualenv directory, maybe this will solve the problems with lessc not being available in the virtualenv
-    ./configure --prefix /home/vagrant/.virtualenvs/onlineweb/
+    progress ./configure --prefix /home/vagrant/.virtualenvs/onlineweb/
+    echo "compiling node (go get coffee)"
+    progress make
+    progress make install
 
-    echo "compiling in 3s (go get coffee)"
-    sleep 3
-    make
-
-    echo "installing"
-    make install
-
-    echo "installing npm"
-    curl https://npmjs.org/install.sh | sh
+    workon onlineweb
+    if ! type "npm" > /dev/null; then
+        echo "installing npm"
+        progress `curl https://npmjs.org/install.sh | sh`
+    fi
 
     echo "installing less compiler"
-    npm install less -g
+    progress npm install less -g
 }
 
 function prepare_and_run_onlineweb() {
@@ -65,11 +113,13 @@ function prepare_and_run_onlineweb() {
     cd /vagrant
     cp onlineweb4/settings/example-local.py onlineweb4/settings/local.py
     echo "creating tables"
-    python manage.py syncdb
+    progress python manage.py syncdb
     echo "starting dev server"
     python manage.py runserver 0.0.0.0:8000 &
     echo "done, check http://localhost:8001 on host"
 }
+
+init
 
 update_packages
 install_packages
