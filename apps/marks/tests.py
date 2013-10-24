916cffd9bbefd37b5dd7afdbb14fcfe69344741e
Thomas Gautvedt <thomasgautv@hotmail.com>
2013-10-24 23:58:54 +0200

    Added mark-test again

--- apps/marks/tests.py
+++ apps/marks/tests.py
@@ -15,10 +15,10 @@ class MarksTest(TestCase):
         self.userentry = G(UserEntry, user=self.user)
         self.logger = logging.getLogger(__name__)
 
-    #def testMarksActive(self):
-    #    self.logger.debug("Testing if Mark is active today with dynamic fixtures")
-    #    self.assertTrue(self.mark.is_active)
-    #    
+    def testMarksActive(self):
+        self.logger.debug("Testing if Mark is active today with dynamic fixtures")
+        self.assertTrue(self.mark.is_active)
+        
     def testMarkUnicode(self):    
         self.logger.debug("Testing Mark unicode with dynamic fixtures")
         self.assertEqual(self.mark.__unicode__(), "Prikk for 1")
