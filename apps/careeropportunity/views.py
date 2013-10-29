c79ee18e0dc11bf13652e04c3160b9ae98c585d3
Christian Strand Young <christian@strandyoung.com>
2013-10-29 20:14:45 +0100

    Now sorting by featured flag in carreropportunity

--- apps/careeropportunity/views.py
+++ apps/careeropportunity/views.py
@@ -1,17 +1,21 @@
 #-*- coding: utf-8 -*-
 from datetime import datetime
+
 from django.shortcuts import render_to_response
 from django.shortcuts import get_object_or_404
 from django.template import RequestContext
+
 from apps.careeropportunity.models import CareerOpportunity
 
 
 def index(request):
-    opportunities = CareerOpportunity.objects.filter(start__lte=datetime.now(), end__gte=datetime.now())
+    opportunities = CareerOpportunity.objects.filter(
+    	start__lte=datetime.now(), end__gte=datetime.now()).orderBy('featured')
     
     return render_to_response('careeropportunity/index.html', \
-            {'opportunities': opportunities}, \
+            {'featured_opportunities': featured_opportunities, 'opportunities': opportunities}, \
             context_instance=RequestContext(request))
+
 
 def details(request, opportunity_id):
     opportunity = get_object_or_404(CareerOpportunity, pk=opportunity_id)
