2a67ad8fd9ffd732b189a497d37d124c22d1cd39
mythern <aleks@mythserv.com>
2013-11-01 00:18:19 +0000

    Added noop authorization to enable patching of user objects

--- apps/api/rfid/user.py
+++ apps/api/rfid/user.py
@@ -3,6 +3,8 @@
 from tastypie import fields
 from tastypie.resources import ModelResource, ALL
 from apps.authentication.models import OnlineUser as User
+from tastypie.authentication import ApiKeyAuthentication
+from tastypie.authorization import Authorization
 
 class UserResource(ModelResource):
 
@@ -10,6 +12,8 @@ class UserResource(ModelResource):
         queryset = User.objects.all()
         resource_name = 'user'
         fields = ['username', 'first_name', 'last_name', 'rfid', ]
+	allowed_methods = ['get', 'patch']
+        authorization = Authorization()
         filtering = {
             "username": ALL,
             "rfid": ALL,
