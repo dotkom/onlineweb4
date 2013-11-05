ece6bbdd02f1395dcf6688fbc21231e4fa5a9eac
Espen Jacobsson <espenjacobsson@gmail.com>
2013-11-05 19:40:54 +0100

    Remove dependency left over from wiki

--- onlineweb4/urls.py
+++ onlineweb4/urls.py
@@ -3,10 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.http import HttpResponse
-from django_notify.urls import get_pattern as get_notify_pattern
 from django.views.generic import TemplateView
-
-from wiki.urls import get_pattern as get_wiki_pattern
 
 from apps import mommy
 
