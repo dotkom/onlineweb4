d281fa8f31bb5f5e18c10ca39a4791287990e18c
Espen Jacobsson <espenjacobsson@gmail.com>
2013-11-23 06:17:25 +0100

    Remove marks from profile form

--- apps/profiles/forms.py
+++ apps/profiles/forms.py
@@ -11,7 +11,7 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = OnlineUser
 
-        fields = ['nickname', 'website', 'phone_number', 'address', 'zip_code', 'allergies', 'gender', 'mark_rules', ]
+        fields = ['nickname', 'website', 'phone_number', 'address', 'zip_code', 'allergies', 'gender', ]
         widgets = {
             'allergies' : forms.Textarea(attrs={'id' : 'allergies'}),
         }
