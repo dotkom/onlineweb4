2903e6cd5cc2daa0b115aea02cb059d12094152b
Espen Jacobsson <espenjacobsson@gmail.com>
2013-11-26 22:38:43 +0100

    Fixed error when fetching default profile image

--- apps/authentication/templatetags/gravatar_url_resolver.py
+++ apps/authentication/templatetags/gravatar_url_resolver.py
@@ -6,8 +6,11 @@ register = template.Library()
 
 @register.assignment_tag(takes_context=True)
 def gravatar_url(context, user, size):
-    default = "%s%s_%s.png" % (context['request'].META['HTTP_HOST'],
+
+    prefix = "https://" if context['request'].is_secure() else "http://"
+    default = "%s%s%s_%s.png" % (prefix, context['request'].META['HTTP_HOST'],
                                settings.DEFAULT_PROFILE_PICTURE_PREFIX, user.gender)
+
     gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(user.get_email().email).hexdigest() + "?"
     gravatar_url += urllib.urlencode({'d': default, 's':str(size)})
 
