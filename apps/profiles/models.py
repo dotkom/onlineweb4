2a6e9faa920cc5eeb7171aed0458594baf27df95
Christian Strand Young <christian@strandyoung.com>
2014-01-28 21:06:00 +0100

    Applied privacy settings to profiles

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -8,7 +8,7 @@ from apps.authentication.models import OnlineUser as User
 
 class Privacy(models.Model):
     visible_for_other_users = models.BooleanField(_(u"profil synlig for andre brukere"), default=True)
-    expose_username = models.BooleanField(_(u"vis brukernavn"), default=True)
+    expose_nickname = models.BooleanField(_(u"vis kallenavn"), default=True)
     expose_email = models.BooleanField(_(u"vis epost"), default=True)
     expose_phone_number = models.BooleanField(_(u"vis telefonnummer"), default=True)
     expose_address = models.BooleanField(_(u"vis addresse"), default=True)
