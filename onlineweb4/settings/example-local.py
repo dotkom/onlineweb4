fb54762d3322b165dfa4c7c68dcaf4d54fd68106
melwil <slettvold@gmail.com>
2014-02-05 19:55:24 +0000

    Override for LOGGING variable in develop to prevent a metric fuckton of debug statements every time we run manage.py.

--- onlineweb4/settings/example-local.py
+++ onlineweb4/settings/example-local.py
@@ -73,3 +73,37 @@ FILEBROWSER_MEDIA_ROOT = MEDIA_ROOT
 #   'debug_toolbar', # https://github.com/dcramer/django-debug-toolbar
 #   'django_extensions', # http://packages.python.org/django-extensions/
 # )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
