beec537b4c713536c52da1cfa17fa192e889c739
Christian Duvholt <cxhristian@gmail.com>
2014-02-19 20:40:45 +0100

    Added empty admin view

--- apps/genfors/urls.py
+++ apps/genfors/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('apps.genfors.views',
 	url(r'^$', 'genfors', name='genfors_index'),
+	url(r'^admin$', 'admin', name='genfors_admin'),
 )
