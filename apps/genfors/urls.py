7ceebdc8eaae25b6b0cf2ce4e79da4e0466d1fb8
Christian Duvholt <cxhristian@gmail.com>
2014-02-19 23:15:05 +0100

    Tabs to spaces

--- apps/genfors/urls.py
+++ apps/genfors/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('apps.genfors.views',
-	url(r'^$', 'genfors', name='genfors_index'),
-	url(r'^admin$', 'admin', name='genfors_admin'),
-	url(r'^admin/logout$', 'admin_logout', name='genfors_admin_logout'),
+    url(r'^$', 'genfors', name='genfors_index'),
+    url(r'^admin$', 'admin', name='genfors_admin'),
+    url(r'^admin/logout$', 'admin_logout', name='genfors_admin_logout'),
 )
--- apps/genfors/views.py
+++ apps/genfors/views.py
@@ -11,38 +11,38 @@ import datetime
 
 @login_required
 def genfors(request):
-	context = {}
-	today = datetime.date.today()
+    context = {}
+    today = datetime.date.today()
 
-	meetings = Meeting.objects.filter(start_date__range=[today, today + datetime.timedelta(days=1)]).order_by('-start_date')
-	if meetings:
-		meeting = meetings[0]
-		context['meeting'] = meeting
-	return render(request, "genfors/index.html", context)
+    meetings = Meeting.objects.filter(start_date__range=[today, today + datetime.timedelta(days=1)]).order_by('-start_date')
+    if meetings:
+        meeting = meetings[0]
+        context['meeting'] = meeting
+    return render(request, "genfors/index.html", context)
 
 
 @login_required
 def admin(request):
-	context = {}
-	if request.session.get('genfors_admin') == True:
-		meetings = Meeting.objects.filter(registration_locked=False).order_by('-start_date')
-		if meetings:
-			meeting = meetings[0]
-			context['meeting'] = meeting
-			return render(request, "genfors/admin.html", context)
-	else:
-		if request.method == 'POST':
-			form = LoginForm(request.POST)
-			context['form'] = form
-			if form.is_valid():
-				request.session['genfors_admin'] = True
-				return redirect('genfors_admin')
-		else:
-			context['form'] = LoginForm()
-	return render(request, "genfors/admin_login.html", context)
+    context = {}
+    if request.session.get('genfors_admin') == True:
+        meetings = Meeting.objects.filter(registration_locked=False).order_by('-start_date')
+        if meetings:
+            meeting = meetings[0]
+            context['meeting'] = meeting
+            return render(request, "genfors/admin.html", context)
+    else:
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            context['form'] = form
+            if form.is_valid():
+                request.session['genfors_admin'] = True
+                return redirect('genfors_admin')
+        else:
+            context['form'] = LoginForm()
+    return render(request, "genfors/admin_login.html", context)
 
 
 def admin_logout(request):
-	if 'genfors_admin' in request.session:
-		del request.session['genfors_admin']
-	return redirect('genfors_index')
+    if 'genfors_admin' in request.session:
+        del request.session['genfors_admin']
+    return redirect('genfors_index')
