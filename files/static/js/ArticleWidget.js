62f5e168ca326018057588fb896ad45a28483fff
Tri M. Nguyen <mail@trimn.net>
2014-04-19 16:27:07 +0200

    Remove unused code from articleWidget.js

--- files/static/js/ArticleWidget.js
+++ files/static/js/ArticleWidget.js
@@ -1,7 +1,7 @@
 function ArticleWidget (Utils){
     ArticleWidget.prototype.render = function(callback) {
         callback = callback || function () {};
-        
+
          // Loading featured
         Utils.makeApiRequest({
             'url': '/api/v0/article/all/?format=json&limit=8',
@@ -19,7 +19,7 @@ function ArticleWidget (Utils){
                         else
                             output_normal += '<div class="col-xs-6 col-md-2"><a href="/article/'+data.articles[i].id+'/'+data.articles[i].slug+'"><img src="'+data.articles[i].image_article_front_small+'" alt="'+data.articles[i].heading+'"><br /><h4>'+data.articles[i].heading+'</h4></a></div>';
                     }
-                    
+
                     // Appending
                     $('#article-frontpage-featured').html(output_featured);
                     $('#article-frontpage-normal').html(output_normal);
@@ -27,48 +27,11 @@ function ArticleWidget (Utils){
                 }else {
                     $('#article-frontpage-featured').html('<div class="col-md-12"><p class="ingress">Ingen artikler funnet</p></div>');
                 }
-                
+
                 // Calling the callback
                 callback();
             }
         });
-        
-        /*
-        
-        // Loading featured
-        Utils.makeApiRequest({
-            'url': '/api/v0/article/all/?format=json&limit=2&featured=True',
-            'method': 'GET',
-            'data': {},
-            success: function(data) {
-                var output = '';
 
-                // The loop
-                for (var i = 0; i < data.articles.length; i++) {
-                    output += '<div class="span6"><a href="/article/'+data.articles[i].id+'"><img src="'+data.articles[i].image_article_front_featured+'" alt="'+data.articles[i].heading+'"></a><h3><a href="/article/'+data.articles[i].id+'">'+data.articles[i].heading+'</a></h3><p>'+data.articles[i].ingress+'</p></div>';
-                }
-
-                $('#article-frontpage-featured').html(output);
-            }
-        });
-
-        // Loading "normal" articles
-        Utils.makeApiRequest({
-            'url': '/api/v0/article/all/?format=json&limit=6&featured=False',
-            'method': 'GET',
-            'data': {},
-            success: function(data) {
-                var output = '';
-
-                // The loop
-                for (var i = 0; i < data.articles.length; i++) {
-                    output += '<div class="span2"><a href="/article/'+data.articles[i].id+'"><img src="'+data.articles[i].image_article_front_small+'" alt="'+data.articles[i].heading+'"></a><br /><h4><a href="/article/'+data.articles[i].id+'">'+data.articles[i].heading+'</a></h4></div>';
-                }
-
-                $('#article-frontpage-normal').html(output);
-            }
-        });
-        
-        */
-    }
+    };
 }
