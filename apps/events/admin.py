1908656acafedaa3730ff8c99b3e33a4d9141b9f
melwil <slettvold@gmail.com>
2014-08-04 22:23:32 +0000

    Changed this hook to check against the correct waiting list.

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -118,13 +118,15 @@ class EventAdmin(admin.ModelAdmin):
         else:
             # If attendance max capacity changed we will notify users that they are now on the attend list
             old_event = Event.objects.get(id=obj.id)
-            if old_event.is_attendance_event() and old_event.wait_list:
-                diff_capacity = obj.attendance_event.max_capacity - old_event.attendance_event.max_capacity
-                if diff_capacity > 0:
-                    if diff_capacity > len(old_event.wait_list):
-                        diff_capacity = len(old_event.wait_list)
-                    # Using old_event because max_capacity has already been changed in obj
-                    old_event.notify_waiting_list(host=request.META['HTTP_HOST'], extra_capacity=diff_capacity)
+            if old_event.is_attendance_event():
+                old_waitlist_size = old_event.attendance_event.waitlist_qs.count()
+                if old_waitlist_size > 0:
+                    diff_capacity = obj.attendance_event.max_capacity - old_event.attendance_event.max_capacity
+                    if diff_capacity > 0:
+                        if diff_capacity > old_waitlist_size:
+                            diff_capacity = old_waitlist_size
+                        # Using old_event because max_capacity has already been changed in obj
+                        old_event.notify_waiting_list(host=request.META['HTTP_HOST'], extra_capacity=diff_capacity)
         obj.save()
 
     def save_formset(self, request, form, formset, change):
