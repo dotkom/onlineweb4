b33ff5d4c57a37d1b6342434b1d5db650c544d37
Christian Strand Young <christian@strandyoung.com>
2014-08-27 22:15:00 +0200

    URL pattern for the user search site

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -16,7 +16,8 @@ urlpatterns = patterns('apps.profiles.views',
     url(r'^verify_email/$', 'verify_email', name='profile_verify_email'),
     url(r'^save_membership_details/$', 'save_membership_details', name='profile_save_membership_details'),
     url(r'^update_mark_rules/$', 'update_mark_rules', name='update_mark_rules'),
+    
     url(r'^api_user_search/$', 'api_user_search', name='profiles_api_user_search'),
+    url(r'^user_search/$', 'user_search', name='profiles_user_search'),
     url(r'^(?P<username>[a-zA-Z0-9_-]+)/$', 'view_profile', name='profiles_view'),
-
 )
