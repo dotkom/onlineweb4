01a487e89bc234a89bf15d0df47e14a70780f778
melwil <slettvold@gmail.com>
2014-09-24 19:04:39 +0000

    Added the url to create membership applications.

--- apps/approval/urls.py
+++ apps/approval/urls.py
@@ -3,6 +3,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('apps.approval.views',
-    url(r'^$', 'index', name='applications'),
     url(r'^send_fos_application/$', 'create_fos_application', name='approval_send_fos_application'),
+    url(r'^send_membership_application/$', 'create_membership_application', name='approval_send_membership_application'),
 )
