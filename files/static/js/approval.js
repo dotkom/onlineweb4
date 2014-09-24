4187952777e01e5b41f6fcb8983ca64c7d1d4cfc
melwil <slettvold@gmail.com>
2014-09-24 22:12:52 +0000

    Finished js for ajax calls on approval list.

--- files/static/js/approval.js
+++ files/static/js/approval.js
@@ -1,4 +1,17 @@
-
+/* AJAX SETUP FOR CSRF */
+$.ajaxSetup({
+    crossDomain: false, // obviates need for sameOrigin test
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type)) {
+            xhr.setRequestHeader("X-CSRFToken", $.cookie('csrftoken'));
+        }
+    }
+});
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+/* END AJAX SETUP */
 
 $(document).ready(function() {
     $("div.application").each(function(i, row) {
@@ -6,7 +19,23 @@ $(document).ready(function() {
             approveApplication($(this).val(), row);
         });
         $(row).find("button.decline").click(function() {
-            declineApplication($(this).val(), row);
+            that = $(this);
+            // hide the first button
+            $(this).prop('disabled', true);
+            // show the row with message and confirmation
+            confirmrow = $("#confirm" + $(this).val());
+            $(confirmrow).show();
+            // find the button that will actually send the decline and set some stuff on it
+            $("button.sendDecline").click(function() {
+                $(confirmrow).hide();
+                message = $(confirmrow).find("textarea").val();
+                declineApplication($(this).val(), message, row);
+            });
+            // cancel should hide the row and enable the first button again
+            $("button.cancel").click(function() {
+                $(that).prop('disabled', false);
+                $(confirmrow).hide();
+            });
         });
     });
     
@@ -16,9 +45,7 @@ $(document).ready(function() {
             method: 'POST',
             url: 'approve_application/',
             data: {'application_id': application_id, },
-            success: function(response) {
-                response = JSON.parse(response['responseText']);
-                utils.setStatusMessage(response, 'alert-success');
+            success: function() {
                 // TODO Make animation
                 $(row).hide();
             },
@@ -35,8 +62,27 @@ $(document).ready(function() {
         });
     }
 
-    var declineApplication = function(application_id, row) {
-        
+    var declineApplication = function(application_id, message, row) {
+        var utils = new Utils();
+        $.ajax({
+            method: 'POST',
+            url: 'decline_application/',
+            data: {'application_id': application_id, 'message': message, },
+            success: function() {
+                // TODO Make animation
+                $(row).hide();
+            },
+            error: function(response) {
+                if (response['status'] === 412) {
+                    response = JSON.parse(response['responseText']);
+                    utils.setStatusMessage(response['message'], 'alert-danger');
+                }
+                else {
+                    utils.setStatusMessage('En uventet error ble oppdaget. Kontakt dotkom@online.ntnu.no for assistanse.', 'alert-danger');
+                }
+            },
+            crossDomain: false
+        });
     }
 
 });
