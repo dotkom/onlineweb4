910744b1d8546e8099d88741bd17866747cfc908
Tri M. Nguyen <mail@trimn.net>
2014-09-28 11:16:27 +0200

    :lipstick:

--- files/static/js/init.js
+++ files/static/js/init.js
@@ -16,61 +16,53 @@ $(function() {
         disableDefaultUI: true,
         streetViewControl: false,
 
-        styles: [{
-            featureType: "all",
-            stylers: [{
-                saturation: -100
-            }]
+        styles: [
+            {
+                "featureType": "all",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    }
+                ]
+            },
+            {
+                "stylers": [
+                    { "hue": "#666666" },
+                    { "lightness": -30 }
+                ]
 
 
+            },
+            {
+                "featureType": "poi.school",
+                "stylers": [
+                    { "color": "#b3b3b3" }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "stylers": [
+                    { "color": "#d2e4c4" }
+                ]
 
-        }, {
-            stylers: [{
-                hue: "#666666"
-            }, {
-                lightness: -30
-            }]
-
-
-        }, {
-            "featureType": "poi.school",
-            "stylers": [{
-                "color": "#b3b3b3"
-            }]
-       /* }, {
-            "featureType": "poi.business",
-            "visibility": 0,
-            "stylers": [{
-                "color": "#00396e"
-            }, {
-                "weight": 0
-           }] */
-        }, {
-            "featureType": "poi.park",
-            "stylers": [{
-                "color": "#d2e4c4"
-            }]
-
-},{
-    "featureType": "road.local",
-    elementType: 'all',
-    "stylers": [
-        { hue: '#f4f4f4' },
-
-        { lightness: 52 }
-    ]
-
-
-        }, {
-            "featureType": "poi.sports_complex",
-            "stylers": [{
-                "saturation": 4
-            }, {
-                "weight": 0.5
-            }, {
-                "color": "#bad5aa"
-            }]
-        }]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": 'all',
+                "stylers": [
+                    { "hue": '#f4f4f4' },
+                    { "lightness": 52 }
+                ]
+            },
+            {
+                "featureType": "poi.sports_complex",
+                "stylers": [
+                    { "saturation": 4 },
+                    { "weight": 0.5 },
+                    { "color": "#bad5aa" }
+                ]
+            }
+        ]
     });
 
     // Applying the marker to the map
