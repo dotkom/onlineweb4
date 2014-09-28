4145afc994709341cb966b24c3c86e318d998cc8
Tri M. Nguyen <mail@trimn.net>
2014-09-28 10:26:22 +0200

    Add navigation bar toggle functionality

--- files/static/js/init.js
+++ files/static/js/init.js
@@ -6,13 +6,13 @@ $(function() {
     setTimeout(function () {
         $('.alert').fadeOut();
     }, 5000);
-    
+
     // Init the footer-map
     var map = new google.maps.Map(document.getElementById("footer-map"),{
         center: new google.maps.LatLng(63.41819751959266, 10.40592152481463),
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-       
+
         disableDefaultUI: true,
         streetViewControl: false,
 
@@ -21,7 +21,7 @@ $(function() {
             stylers: [{
                 saturation: -100
             }]
-            
+
 
 
         }, {
@@ -31,7 +31,7 @@ $(function() {
                 lightness: -30
             }]
 
-            
+
         }, {
             "featureType": "poi.school",
             "stylers": [{
@@ -50,17 +50,17 @@ $(function() {
             "stylers": [{
                 "color": "#d2e4c4"
             }]
-            
-},{  
-    "featureType": "road.local",  
-    elementType: 'all',  
-    "stylers": [  
-        { hue: '#f4f4f4' },  
-        
-        { lightness: 52 }
-    ]  
 
-          
+},{
+    "featureType": "road.local",
+    elementType: 'all',
+    "stylers": [
+        { hue: '#f4f4f4' },
+
+        { lightness: 52 }
+    ]
+
+
         }, {
             "featureType": "poi.sports_complex",
             "stylers": [{
@@ -72,7 +72,7 @@ $(function() {
             }]
         }]
     });
-    
+
     // Applying the marker to the map
     var online_marker = new google.maps.Marker({
         map: map,
@@ -80,7 +80,7 @@ $(function() {
         icon: '/static/img/map-marker.png',
         visible: true
     });
-    
+
     // Reset center of the map on window-resize (using jquery-plugin to only fire the event when resizing is finished)
     // Also swap event image sources for proper resolution on mobile view
     $(window).on("debouncedresize",function(e) {
@@ -108,16 +108,56 @@ $(function() {
     });
 
 
+    /* nav bar toggle
+    ---------------------------------------------------------------------------*/
+    var mainnav_button = document.getElementById('mainnav-button')
+    mainnav_button.addEventListener('click', function (e) {
+        if (/(^|\s)rotate-button(\s|$)/.test(document.querySelectorAll('.mn-svg')[0].getAttribute('class'))) {
+            removeAnimation()
+            $('.mn-nav').fadeOut(300)
+        }
+        else {
+            addAnimation()
+            $('.mn-nav').fadeIn(300)
+        }
+    })
+
+    function addAnimation() {
+        var svg = document.querySelectorAll('.mn-svg')
+
+        svg[0].setAttribute('class', 'mn-svg rotate-button')
+        svg[1].setAttribute('class', 'mn-svg rotate-button-ccw')
+        document.querySelector('rect.mn-svg-rect-top').setAttribute('class', 'mn-svg-rect-top transform-button')
+        document.querySelector('.mn-svg-rect-bottom').setAttribute('class', 'mn-svg-rect-bottom transform-button')
+    }
+
+    function removeAnimation() {
+        var svg = document.querySelectorAll('.mn-svg')
+
+        svg[0].setAttribute('class', 'mn-svg')
+        svg[1].setAttribute('class', 'mn-svg')
+        document.querySelector('rect.mn-svg-rect-top').setAttribute('class', 'mn-svg-rect-top')
+        document.querySelector('.mn-svg-rect-bottom').setAttribute('class', 'mn-svg-rect-bottom')
+    }
 
 
     /* Menu element change
     --------------------------------------------------------------------------------- */
-    $(".nav.navbar-nav a[href='"+window.location.pathname+"']").parent().addClass('active');
+    $(".subnavbar a[href='"+window.location.pathname+"']").parent().addClass('active');
 
     switch (window.location.pathname) {
         case "/article/archive":
         case "/offline/":
             $(".navbar .nav.navbar-nav a[href='/events/']").parent().addClass('active');
+    }
+
+
+    $(".mn-nav a[href='"+window.location.pathname+"']").parent().addClass('active');
+
+    switch (window.location.pathname) {
+        case "/article/archive":
+        case "/offline/":
+            $(".mn-nav a[href='/events/']").parent().addClass('active');
     }
 
 
@@ -134,19 +174,19 @@ $(function() {
 
     function change_subnavbar_behaviour() {
         var active_link = $('.navbar .nav.navbar-nav .active');
-
-        if ($('.subnavbar').length) {
-            if ($(window).innerWidth() < 768) {
-                if (!active_link.hasClass('appended')) {
-                    active_link.append($('.subnavbar div').html());
-                    active_link.addClass('appended');
-                }
-            }
-            else {
-                active_link.html(active_link_clone.html());
-                active_link.removeClass('appended');
-            }
-        }
+        //
+        // if ($('.subnavbar').length) {
+        //     if ($(window).innerWidth() < 768) {
+        //         if (!active_link.hasClass('appended')) {
+        //             active_link.append($('.subnavbar div').html());
+        //             active_link.addClass('appended');
+        //         }
+        //     }
+        //     else {
+        //         active_link.html(active_link_clone.html());
+        //         active_link.removeClass('appended');
+        //     }
+        // }
     }
 
     $('.dropdown-menu input, .dropdown-menu button, .dropdown-menu label').click(function(e) {
