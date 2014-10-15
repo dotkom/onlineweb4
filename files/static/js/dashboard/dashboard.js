528cb31f6a15e480d113d7f4a0debd2c9d4a186c
mythern <myth@overflow.no>
2014-10-15 23:41:08 +0200

    Forgot to add js file

---
+++ files/static/js/dashboard/dashboard.js
@@ -0,0 +1,13 @@
+var Dashboard = (function ($) {
+    
+    return {
+        init: function () {
+            console.log('Dashboard initiated.')
+        }
+    }
+
+})(jQuery)
+
+$(document).ready(function () {
+    Dashboard.init()
+})
