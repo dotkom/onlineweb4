ba81cf269e20a7c06aac43e90831a94f554530dc
mythern <myth@overflow.no>
2014-10-16 02:35:59 +0200

    Add font-awesome, accordion toggling, sidebar toggling and some prettifying

--- files/static/js/dashboard/dashboard.js
+++ files/static/js/dashboard/dashboard.js
@@ -2,7 +2,38 @@ var Dashboard = (function ($) {
     
     return {
         init: function () {
+            Dashboard.bind_sidebar()
             console.log('Dashboard initiated.')
+        },
+
+        bind_sidebar: function () {
+
+            Dashboard.bind_sidebar_toggle()
+
+            $('.treeview-menu').each(function () {
+                var nav_menu = $(this)
+                var nav_item = $(this).parent()
+                nav_item.on('click', 'a', function (e) {
+                    // Toggle the submenu
+                    nav_menu.slideToggle(200)
+                })
+            })
+        },
+
+        bind_sidebar_toggle: function () {
+           $("[data-toggle='offcanvas']").click(function(e) {
+                // If window is small enough, enable sidebar push menu
+                if ($(window).width() <= 992) {
+                    $('.row-offcanvas').toggleClass('active')
+                    $('.left-side').removeClass("collapse-left")
+                    $(".right-side").removeClass("strech")
+                    $('.row-offcanvas').toggleClass("relative")
+                // Else stretch
+                } else {
+                    $('.left-side').toggleClass("collapse-left")
+                    $(".right-side").toggleClass("strech")   
+                }
+           })
         }
     }
 
