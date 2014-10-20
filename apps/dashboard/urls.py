2133c49e5314fbe85926a4e347619a99fa28d608
mythern <myth@overflow.no>
2014-10-20 17:45:13 +0200

    Add example groups dashboard module

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('apps.dashboard.views',
     url(r'^$', 'index', name='dashboard_index'),
     url(r'^groups/$', 'group_index', name='group_index'),
+    url(r'^groups/(?P<pk>\d+)/$', 'group_detail', name='group_detail'),
 )
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,14 +1,15 @@
 # -*- encoding: utf-8 -*-
 
-from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
+from django.shortcuts import render, get_object_or_404
 
 from guardian.decorators import permission_required
 
-from apps.dashboard.tools import has_access, get_base_context
 from apps.approval.models import MembershipApproval
+from apps.authentication.models import OnlineUser as User
+from apps.dashboard.tools import has_access, get_base_context
 
 @login_required
 def index(request):
@@ -39,3 +40,20 @@ def group_index(request):
     context['groups'] = Group.objects.all().order_by('name')
 
     return render(request, 'dashboard/groups.html', context)
+
+@login_required
+def group_detail(request, pk):
+    """
+    Group module in dashboard that lists groups.
+    """
+
+    if not has_access(request):
+        raise PermissionDenied
+
+    context = get_base_context(request)
+
+    context['group'] = get_object_or_404(Group, pk=pk)
+    context['group_users'] = list(context['group'].user_set.all())
+    context['group_users'].sort()
+
+    return render(request, 'dashboard/groups_detail.html', context)
