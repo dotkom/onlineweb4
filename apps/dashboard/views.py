6cafb3b583699754faaa82a6f8f4ac982fba9c5d
mythern <myth@overflow.no>
2014-10-20 02:14:04 +0200

    Add login required decorator to main dashboard view and fix firefox sidebar outlining on click

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -2,11 +2,13 @@
 
 from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import login_required
 
 from guardian.decorators import permission_required
 
 from apps.dashboard.tools import has_access
 
+@login_required
 def index(request):
     """
     This is the main dashboard view
