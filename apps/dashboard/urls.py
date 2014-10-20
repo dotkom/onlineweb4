35e7856cf5907bfcdfecec375fac680394ac99c1
mythern <myth@overflow.no>
2014-10-20 17:17:50 +0200

    Add groups dashboard module with links to django-admin

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('apps.dashboard.views',
     url(r'^$', 'index', name='dashboard_index'),
+    url(r'^groups/$', 'group_index', name='group_index'),
 )
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 
 from guardian.decorators import permission_required
 
@@ -20,4 +21,21 @@ def index(request):
 
     context = get_base_context(request)
 
-    return render(request, 'dashboard.html', context)
+    return render(request, 'dashboard/dashboard.html', context)
+
+# Group module views
+
+@login_required
+def group_index(request):
+    """
+    Group module in dashboard that lists groups.
+    """
+
+    if not has_access(request):
+        raise PermissionDenied
+
+    context = get_base_context(request)
+
+    context['groups'] = Group.objects.all().order_by('name')
+
+    return render(request, 'dashboard/groups.html', context)
