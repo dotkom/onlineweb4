d75f060a190467857085d15a7f131826d7d53852
mythern <myth@overflow.no>
2014-11-12 22:58:00 +0100

    Add some more commenting on dashboard tools functions

--- apps/dashboard/tools.py
+++ apps/dashboard/tools.py
@@ -10,11 +10,13 @@ def has_access(request):
     """
     This helper method does a basic check to see if the logged in user
     has access to the dashboard.
+
+    We might add additional checks here later.
     """
 
     if request.user.is_superuser:
         return True
-    
+
     try:
         committees = Group.objects.get(name='Komiteer')
     except ObjectDoesNotExist:
@@ -30,7 +32,11 @@ def get_base_context(request):
     """
     This function returns a dictionary with the proper context variables
     needed for given permission settings. Should be used as the initial
-    context for every dashboard view.
+    context for every dashboard view. For example, it is used for rendering
+    badges in the dashboard menu.
+
+    Add your own if req.user.has_perm statements adding the context objects
+    of that you need.
     """
 
     context = {}
