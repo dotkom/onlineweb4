fe292ddfb2b9a9aa0bd8289593fb39e60d63d3c3
vagrant <vagrant@precise32.(none)>
2015-02-04 20:38:48 +0000

    initial models.py commit

--- apps/payment/models.py
+++ apps/payment/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import ugettext as _
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+from apps.authentication.models import OnlineUser as User
+
+class Payment(models.Models):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+
+    price = models.IntegerField(_(u"pris"))
+    deadline = models.DateTimeField(_(u"frist"), editable=False)
+    instant_payment = models.Boolean(_(u"betaling for påmelding"))
+
+    added_date = models.DateTimeField(_(u"opprettet dato"))
+    changed_date = models.DateTimeField(auto_now=True, editable=False)
+    last_changed_by = models.ForeignKey(User, editable=False)
+
+class PaymentRelation(models.Models):
+    payment = models.ForeignKey(Payment)
+    user = models.ForeignKey(User)
+    datetime = models.DateTimeField(auto_now=True)
