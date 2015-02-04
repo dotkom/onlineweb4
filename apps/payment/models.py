a72d99968e399d61cae4fcdfacd538cca6f44cd7
Tor Håkon Bonsaksen <torrib90@gmail.com>
2015-02-04 21:42:12 +0100

    Created initial migrations

--- apps/payment/models.py
+++ apps/payment/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from django.utils.translation import ugettext as _
 from django.contrib.contenttypes import generic
@@ -5,20 +7,20 @@ from django.contrib.contenttypes.models import ContentType
 
 from apps.authentication.models import OnlineUser as User
 
-class Payment(models.Models):
+class Payment(models.Model):
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey('content_type', 'object_id')
 
     price = models.IntegerField(_(u"pris"))
     deadline = models.DateTimeField(_(u"frist"), editable=False)
-    instant_payment = models.Boolean(_(u"betaling for påmelding"))
+    instant_payment = models.BooleanField(_(u"betaling for påmelding"))
 
     added_date = models.DateTimeField(_(u"opprettet dato"))
     changed_date = models.DateTimeField(auto_now=True, editable=False)
     last_changed_by = models.ForeignKey(User, editable=False)
 
-class PaymentRelation(models.Models):
+class PaymentRelation(models.Model):
     payment = models.ForeignKey(Payment)
     user = models.ForeignKey(User)
     datetime = models.DateTimeField(auto_now=True)
