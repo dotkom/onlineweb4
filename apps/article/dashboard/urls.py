bedcca7ba778323373ae26cfb6da5fc62b76362c
Christian Strand Young <christian@strandyoung.com>
2015-02-18 23:08:55 +0100

    Renamed article dashboard urls

--- apps/article/dashboard/urls.py
+++ apps/article/dashboard/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('apps.article.dashboard.views',
     url(r'^$', 'article_index', name='dashboard_article_index'),
-    url(r'^create/$', 'article_create_or_edit', name='dashboard_article_create'),
-    url(r'^edit/(?P<article_id>\d+)/(?P<article_slug>[a-zA-Z0-9_-]+)$', 'article_create_or_edit', name='dashboard_article_edit'),
+    url(r'^create/$', 'article_create', name='dashboard_article_create'),
+    url(r'^change/(?P<article_id>\d+)/$', 'article_change', name='dashboard_article_change'),
 
     url(r'^tag/$', 'tag_index', name='dashboard_tag_index'),
-    url(r'^tag/create$', 'tag_create_or_edit', name='dashboard_tag_create'),
-    url(r'^tag/edit/(?P<name>[^\.]+)/(?P<slug>[^\.]+)$', 'tag_create_or_edit', name='dashboard_tag_edit'),
+    url(r'^tag/create/$', 'tag_create', name='dashboard_tag_create'),
+    url(r'^tag/change/(?P<tag_id>\d+)/$', 'tag_change', name='dashboard_tag_change'),
 )
