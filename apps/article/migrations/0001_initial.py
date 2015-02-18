2eb0da1a678abbdd1ff1231fc19f6c49874dafa4
Christian Duvholt <cxhristian@gmail.com>
2015-02-18 22:25:27 +0100

    Reset migrations

--- apps/article/migrations/0001_initial.py
+++ apps/article/migrations/0001_initial.py
@@ -1,145 +1,74 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import filebrowser.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    depends_on = (
-        ("authentication", "0001_initial"),
-    )
+    dependencies = [
+    ]
 
-    def forwards(self, orm):
-        # Adding model 'Article'
-        db.create_table(u'article_article', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('heading', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('ingress_short', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('ingress', self.gf('django.db.models.fields.TextField')()),
-            ('content', self.gf('django.db.models.fields.TextField')()),
-            ('image', self.gf('filebrowser.fields.FileBrowseField')(max_length=200, null=True)),
-            ('video', self.gf('django.db.models.fields.CharField')(max_length=200, blank=True)),
-            ('created_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('changed_date', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('published_date', self.gf('django.db.models.fields.DateTimeField')()),
-            ('created_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='created_by', to=orm['authentication.OnlineUser'])),
-            ('changed_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='changed_by', to=orm['authentication.OnlineUser'])),
-            ('featured', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal(u'article', ['Article'])
-
-        # Adding model 'Tag'
-        db.create_table(u'article_tag', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('slug', self.gf('django.db.models.fields.CharField')(max_length=30)),
-        ))
-        db.send_create_signal(u'article', ['Tag'])
-
-        # Adding model 'ArticleTag'
-        db.create_table(u'article_articletag', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('article', self.gf('django.db.models.fields.related.ForeignKey')(related_name='article_tags', to=orm['article.Article'])),
-            ('tag', self.gf('django.db.models.fields.related.ForeignKey')(related_name='article_tags', to=orm['article.Tag'])),
-        ))
-        db.send_create_signal(u'article', ['ArticleTag'])
-
-        # Adding unique constraint on 'ArticleTag', fields ['article', 'tag']
-        db.create_unique(u'article_articletag', ['article_id', 'tag_id'])
-
-
-    def backwards(self, orm):
-        # Removing unique constraint on 'ArticleTag', fields ['article', 'tag']
-        db.delete_unique(u'article_articletag', ['article_id', 'tag_id'])
-
-        # Deleting model 'Article'
-        db.delete_table(u'article_article')
-
-        # Deleting model 'Tag'
-        db.delete_table(u'article_tag')
-
-        # Deleting model 'ArticleTag'
-        db.delete_table(u'article_articletag')
-
-
-    models = {
-        u'article.article': {
-            'Meta': {'ordering': "['published_date']", 'object_name': 'Article'},
-            'changed_by': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'changed_by'", 'to': u"orm['authentication.OnlineUser']"}),
-            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'content': ('django.db.models.fields.TextField', [], {}),
-            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'created_by'", 'to': u"orm['authentication.OnlineUser']"}),
-            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'featured': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'heading': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True'}),
-            'ingress': ('django.db.models.fields.TextField', [], {}),
-            'ingress_short': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'published_date': ('django.db.models.fields.DateTimeField', [], {}),
-            'video': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'})
-        },
-        u'article.articletag': {
-            'Meta': {'unique_together': "(('article', 'tag'),)", 'object_name': 'ArticleTag'},
-            'article': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'article_tags'", 'to': u"orm['article.Article']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'article_tags'", 'to': u"orm['article.Tag']"})
-        },
-        u'article.tag': {
-            'Meta': {'object_name': 'Tag'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'slug': ('django.db.models.fields.CharField', [], {'max_length': '30'})
-        },
-        u'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        u'auth.permission': {
-            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        u'authentication.onlineuser': {
-            'Meta': {'object_name': 'OnlineUser'},
-            'address': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
-            'allergies': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'compiled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'field_of_study': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'infomail': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'mark_rules': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'ntnu_username': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
-            'rfid': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
-            'started_date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2013, 8, 22, 0, 0)'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'}),
-            'zip_code': ('django.db.models.fields.CharField', [], {'max_length': '4', 'null': 'True', 'blank': 'True'})
-        },
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        }
-    }
-
-    complete_apps = ['article']
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('heading', models.CharField(max_length=45, verbose_name='tittel')),
+                ('ingress_short', models.CharField(max_length=100, verbose_name='kort ingress')),
+                ('ingress', models.TextField(verbose_name='ingress')),
+                ('content', models.TextField(verbose_name='content')),
+                ('image', filebrowser.fields.FileBrowseField(max_length=200, null=True, verbose_name='bilde')),
+                ('video', models.CharField(max_length=200, verbose_name='vimeo id', blank=True)),
+                ('created_date', models.DateTimeField(auto_now_add=True, verbose_name='opprettet-dato')),
+                ('changed_date', models.DateTimeField(auto_now=True, verbose_name='sist endret')),
+                ('published_date', models.DateTimeField(verbose_name='publisert')),
+                ('additional_authors', models.CharField(max_length=200, verbose_name='andre forfattere', blank=True)),
+                ('photographers', models.CharField(max_length=200, verbose_name='fotograf(er)', blank=True)),
+                ('featured', models.BooleanField(default=False, verbose_name='featured artikkel')),
+            ],
+            options={
+                'ordering': ['published_date'],
+                'verbose_name': 'artikkel',
+                'verbose_name_plural': 'artikler',
+                'permissions': (('view_article', 'View Article'),),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ArticleTag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('article', models.ForeignKey(related_name='article_tags', verbose_name='artikkel', to='article.Article')),
+            ],
+            options={
+                'verbose_name': 'tag',
+                'verbose_name_plural': 'tags',
+                'permissions': (('view_articletag', 'View ArticleTag'),),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50, verbose_name='navn')),
+                ('slug', models.CharField(max_length=30, verbose_name='kort navn')),
+            ],
+            options={
+                'permissions': (('view_tag', 'View Tag'),),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='articletag',
+            name='tag',
+            field=models.ForeignKey(related_name='article_tags', verbose_name='tag', to='article.Tag'),
+            preserve_default=True,
+        ),
+        migrations.AlterUniqueTogether(
+            name='articletag',
+            unique_together=set([('article', 'tag')]),
+        ),
+    ]
--- apps/profiles/migrations/0001_initial.py
+++ apps/profiles/migrations/0001_initial.py
@@ -1,102 +1,33 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Privacy'
-        db.create_table(u'profiles_privacy', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('visible_for_other_users', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('expose_username', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('expose_email', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('expose_first_name', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('expose_last_name', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('expose_field_of_study', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('expose_started_date', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('expose_compiled', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('expose_phone_number', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('expose_address', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(related_name='privacy', unique=True, to=orm['authentication.OnlineUser'])),
-        ))
-        db.send_create_signal(u'profiles', ['Privacy'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Privacy'
-        db.delete_table(u'profiles_privacy')
-
-
-    models = {
-        u'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        u'auth.permission': {
-            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        u'authentication.onlineuser': {
-            'Meta': {'object_name': 'OnlineUser'},
-            'address': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
-            'allergies': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'compiled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'field_of_study': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('filebrowser.fields.FileBrowseField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
-            'infomail': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'mark_rules': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'nickname': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
-            'ntnu_username': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
-            'rfid': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
-            'started_date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2013, 9, 3, 0, 0)'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'}),
-            'website': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
-            'zip_code': ('django.db.models.fields.CharField', [], {'max_length': '4', 'null': 'True', 'blank': 'True'})
-        },
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        u'profiles.privacy': {
-            'Meta': {'object_name': 'Privacy'},
-            'expose_address': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'expose_compiled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'expose_email': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'expose_field_of_study': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'expose_first_name': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'expose_last_name': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'expose_phone_number': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'expose_started_date': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'expose_username': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'privacy'", 'unique': 'True', 'to': u"orm['authentication.OnlineUser']"}),
-            'visible_for_other_users': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
-        }
-    }
-
-    complete_apps = ['profiles']
+    operations = [
+        migrations.CreateModel(
+            name='Privacy',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('visible_for_other_users', models.BooleanField(default=True, verbose_name='profil synlig for andre brukere')),
+                ('expose_nickname', models.BooleanField(default=True, verbose_name='vis kallenavn')),
+                ('expose_email', models.BooleanField(default=True, verbose_name='vis epost')),
+                ('expose_phone_number', models.BooleanField(default=True, verbose_name='vis telefonnummer')),
+                ('expose_address', models.BooleanField(default=True, verbose_name='vis addresse')),
+                ('user', models.ForeignKey(related_name='privacy', to=settings.AUTH_USER_MODEL, unique=True)),
+            ],
+            options={
+                'verbose_name': 'personvern',
+                'verbose_name_plural': 'personvern',
+                'permissions': (('view_privacy', 'View Privacy'),),
+            },
+            bases=(models.Model,),
+        ),
+    ]
