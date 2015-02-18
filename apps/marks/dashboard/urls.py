f28404f7f789f306f8244bc65bccb7946110c131
OptimusCrime <thomasgautv@hotmail.com>
2015-02-18 22:07:10 +0100

    Added view for Marks details in dashboard

--- apps/marks/dashboard/urls.py
+++ apps/marks/dashboard/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('apps.marks.dashboard.views',
     url(r'^$', 'index', name='marks_index'),
+    url(r'^(?P<pk>\d+)/$', 'marks_details', name='marks_details'),
     url(r'^new/$', 'marks_new', name='marks_new'),
 )
--- apps/marks/dashboard/views.py
+++ apps/marks/dashboard/views.py
@@ -15,24 +15,48 @@ def index(request):
     Marks overview
     """
 
+    # Check access
     if not has_access(request):
         raise PermissionDenied
 
+    # Get context
     context = get_base_context(request)
 
-    marks_split = []
+    # Find all marks and do additional fixes
+    marks_collection = []
     marks = list(Mark.objects.all())
     for mark in marks:
         marks_temp = mark
         marks_temp.users_num = len(mark.given_to.all())
         marks_temp.category_clean = mark.get_category_display()
-        
-        marks_split.append(marks_temp)
 
-    context['marks'] = marks_split
+        marks_collection.append(marks_temp)
 
+    # Add collection to context
+    context['marks'] = marks_collection
+
+    # Render view
     return render(request, 'marks/dashboard/index.html', context)
 
+@login_required
+@permission_required('marks.can_add', return_403=True)
+def marks_details(request, pk):
+    """
+    Display details for a given Mark
+    """
+
+    # Check permission
+    if not has_access(request):
+        raise PermissionDenied
+
+    # Get context
+    context = get_base_context(request)
+
+    # Get object
+    mark = get_object_or_404(Mark, pk=pk)
+
+    # Render view
+    return render(request, 'marks/dashboard/marks_details.html', context)
 
 @login_required
 @permission_required('marks.can_add', return_403=True)
