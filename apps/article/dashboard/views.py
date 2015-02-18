ce6c7ccb7f78b323aee7f3758f675cbe79343e30
Christian Strand Young <christian@strandyoung.com>
2015-02-18 23:54:32 +0100

    Made create and change form view code

--- apps/article/dashboard/views.py
+++ apps/article/dashboard/views.py
@@ -1,9 +1,11 @@
 # -*- encoding: utf-8 -*-
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 
 from guardian.decorators import permission_required
 
 from apps.article.models import Article, Tag
+from apps.article.dashboard.forms import TagForm, ArticleForm
 from apps.dashboard.tools import check_access_or_403, get_base_context
 
 
@@ -31,9 +33,43 @@ def tag_index(request):
 
 @permission_required('article.tag_create')
 def tag_create(request):
-    yield()
+    check_access_or_403(request)
+
+    form = TagForm()
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.sucess(request, u'Tag ble opprettet.')
+            redirect('dashboard_tag_index')
+        else:
+            messages.error(request, u'Noen av de påkrevde feltene inneholder feil.')
+            form = TagForm(request.POST)
+
+    context = get_base_context(request)
+    context['form'] = form
+
+    return render(request, 'article/dashboard/tag_create.html', context)
 
 
 @permission_required('article.change')
 def tag_change(request, tag_id):
-    yield()
+    check_access_or_403(request)
+
+    tag = get_object_or_404(Tag, pk=tag_id)
+
+    form = TagForm(instance=tag)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.sucess(request, u'Tag ble opprettet.')
+            redirect('dashboard_tag_index')
+        else:
+            messages.error(request, u'Noen av de påkrevde feltene inneholder feil.')
+            form = TagForm(request.POST)
+
+    context = get_base_context(request)
+    context['form'] = form
+    
+    return render(request, 'article/dashboard/tag_create.html', context)
