d4ef5d004e08deb1b1f02b5e1b699154d149f308
Tor Håkon Bonsaksen <torrib90@gmail.com>
2015-03-04 21:34:56 +0100

    Update the tests

--- apps/payment/tests.py
+++ apps/payment/tests.py
@@ -23,20 +23,19 @@ class PaymentTest(TestCase):
         self.assertEqual(payment_relation.payment, self.event_payment)
 
     def testEventDescription(self):
-        self.assertEqual(self.event_payment.content_object.payment_description(), "Sjakkturnering")
+        self.assertEqual(self.event_payment.content_object_description(), "Sjakkturnering")
 
     def testEventPostPaymentCreateAttendee(self):
-        self.event_payment.content_object.payment_complete(self.user)
+        self.event_payment.content_object_handle_payment(self.user)
 
         attendee = Attendee.objects.all()[0]
 
         self.assertEqual(attendee.user, self.user)
         self.assertEqual(attendee.event, self.attendance_event)
 
-
     def testEventPaymentCompleteModifyAttendee(self):
         G(Attendee, event=self.attendance_event, user=self.user)
-        self.event_payment.content_object.payment_complete(self.user)
+        self.event_payment.content_object_handle_payment(self.user)
 
         attendee = Attendee.objects.all()[0]
 
