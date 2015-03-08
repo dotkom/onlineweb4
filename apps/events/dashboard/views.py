6c132dca33f6a6b703c43a927c04171b75b781d2
Håvard Slettvold <slettvold@gmail.com>
2015-03-08 17:13:32 +0000

    Basically just finished the attendee section.

--- apps/events/dashboard/views.py
+++ apps/events/dashboard/views.py
@@ -3,8 +3,9 @@
 import json
 
 from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
@@ -48,35 +49,107 @@ def details(request, event_id, active_tab='attendees'):
     if not has_access(request):
         raise PermissionDenied
 
-    context = get_base_context(request)
-
     event = get_object_or_404(Event, pk = event_id)
-    context['event'] = event
-    context['active_tab'] = active_tab
-
-    context['change_event_form'] = ChangeEventForm(instance=event)
 
     # AJAX
     if request.method == 'POST':
         if request.is_ajax and 'action' in request.POST:
-            resp = {'status': 200}
+            resp = {}
             if request.POST['action'] == 'attended':
-                attendee = get_object_or_404(Attendee, pk = request.POST['attendee_id'])
+                if not event.is_attendance_event:
+                    return HttpResponse(u'Dette er ikke et påmeldingsarrangement.', status=400)
+                attendee = Attendee.objects.filter(pk = request.POST['attendee_id'])
+                if attendee.count() != 1:
+                    return HttpResponse(u'Fant ingen påmeldte med oppgitt ID (%s).' % request.POST['attendee_id'], status=400)
+                attendee = attendee[0]
                 attendee.attended = not attendee.attended
                 attendee.save()
-                return HttpResponse(json.dumps(resp), status=200)
+                return JsonResponse(resp)
             if request.POST['action'] == 'paid':
-                attendee = get_object_or_404(Attendee, pk = request.POST['attendee_id'])
+                if not event.is_attendance_event:
+                    return HttpResponse(u'Dette er ikke et påmeldingsarrangement.', status=400)
+                attendee = Attendee.objects.filter(pk = request.POST['attendee_id'])
+                if attendee.count() != 1:
+                    return HttpResponse(u'Fant ingen påmeldte med oppgitt ID (%s).' % request.POST['attendee_id'], status=400)
+                attendee = attendee[0]
                 attendee.paid = not attendee.paid
                 attendee.save()
-                return HttpResponse(json.dumps(resp), status=200)
-            if request.POST['action'] == 'add_attende':
-                user = get_object_or_404(User, pk=int(request.POST['user_id']))
-                attendee = Attendee(user = user, event = event)
+                return JsonResponse(resp)
+            if request.POST['action'] == 'add_attendee':
+                if not event.is_attendance_event:
+                    return HttpResponse(u'Dette er ikke et påmeldingsarrangement.', status=400)
+                user = User.objects.filter(pk = request.POST['user_id'])
+                if user.count() != 1:
+                    return HttpResponse(u'Fant ingen bruker med oppgitt ID (%s).' % request.POST['user_id'], status=400)
+                user = user[0] 
+                if Attendee.objects.filter(user=user).count() != 0:
+                    return HttpResponse(u'%s er allerede påmeldt %s.' % (user.get_full_name(), event.title), status=400)
+                attendee = Attendee(user = user, event = event.attendance_event)
                 attendee.save()
-                resp['message'] = '%s ble lagt til i %s' % (resp['full_name'], event)
-                resp['attendees'] = [{}]
-                return HttpResponse(json.dumps(resp), status=200)
+                resp['message'] = u'%s ble meldt på %s' % (user.get_full_name(), event)
+                resp['attendees'] = []
+                for number, a in enumerate(attendee.event.attendees_qs):
+                    resp['attendees'].append({
+                        'number': number+1,
+                        'id': a.id,
+                        'first_name': a.user.first_name,
+                        'last_name': a.user.last_name,
+                        'paid': a.paid,
+                        'attended': a.attended,
+                        'link': reverse('dashboard_attendee_details', kwargs={'attendee_id': a.id})
+                    })
+                resp['waitlist'] = []
+                for number, a in enumerate(attendee.event.waitlist_qs):
+                    resp['waitlist'].append({
+                        'number': number+1,
+                        'id': a.id,
+                        'first_name': a.user.first_name,
+                        'last_name': a.user.last_name,
+                        'paid': a.paid,
+                        'attended': a.attended,
+                        'link': reverse('dashboard_attendee_details', kwargs={'attendee_id': a.id})
+                    })
+                return JsonResponse(resp, safe=False)
+            if request.POST['action'] == 'remove_attendee':
+                if not event.is_attendance_event:
+                    return HttpResponse(u'Dette er ikke et påmeldingsarrangement.', status=400)
+                attendee = Attendee.objects.filter(pk = request.POST['attendee_id'])
+                if attendee.count() != 1:
+                    return HttpResponse(u'Fant ingen påmeldte med oppgitt ID (%s).' % request.POST['attendee_id'], status=400)
+                attendee = attendee[0]
+                attendee.delete()
+                resp['message'] = u'%s ble fjernet fra %s' % (attendee.user.get_full_name(), attendee.event)
+                resp['attendees'] = []
+                for number, a in enumerate(attendee.event.attendees_qs):
+                    resp['attendees'].append({
+                        'number': number+1,
+                        'id': a.id,
+                        'first_name': a.user.first_name,
+                        'last_name': a.user.last_name,
+                        'paid': a.paid,
+                        'attended': a.attended,
+                        'link': reverse('dashboard_attendee_details', kwargs={'attendee_id': a.id})
+                    })
+                resp['waitlist'] = []
+                for number, a in enumerate(attendee.event.waitlist_qs):
+                    resp['waitlist'].append({
+                        'number': number+1,
+                        'id': a.id,
+                        'first_name': a.user.first_name,
+                        'last_name': a.user.last_name,
+                        'paid': a.paid,
+                        'attended': a.attended,
+                        'link': reverse('dashboard_attendee_details', kwargs={'attendee_id': a.id})
+                    })
+                return JsonResponse(resp)
+    
+    # NON AJAX
+    context = get_base_context(request)
+
+    context['event'] = event
+    context['active_tab'] = active_tab
+
+    context['change_event_form'] = ChangeEventForm(instance=event)
 
 
     return render(request, 'events/dashboard/details.html', context)
