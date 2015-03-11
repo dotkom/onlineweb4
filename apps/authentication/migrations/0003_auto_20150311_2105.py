72629fd5b05be9fce149a390e0a731e7464116a3
Christian Duvholt <cxhristian@gmail.com>
2015-03-11 21:12:22 +0100

    Use date.today as default for started_date
    Django can't serialize timezone().now().date so let's use date.today instead

---
+++ apps/authentication/migrations/0003_auto_20150311_2105.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authentication', '0002_auto_20150304_2211'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='onlineuser',
+            name='started_date',
+            field=models.DateField(default=datetime.date.today, verbose_name='startet studie'),
+            preserve_default=True,
+        ),
+    ]
