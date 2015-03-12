fae71f0e58ba6516e3f0a200e5687ad875a07cad
Håvard Slettvold <slettvold@gmail.com>
2015-03-12 20:35:57 +0000

    Completed attendance management.

--- apps/events/dashboard/forms.py
+++ apps/events/dashboard/forms.py
@@ -7,27 +7,24 @@ from filebrowser.widgets import FileInput
 from apps.events.models import Event, AttendanceEvent
 
 
+def _get_datetime_widget(field_name):
+    return DateTimeWidget(
+                          attrs = {'id': field_name + "_datepicker"}, 
+                          usel10n = True, 
+                          bootstrap_version=3,
+                          options = {
+                              'format': 'dd/mm/yyyy hh:ii',
+                          }
+                      )
+
+
 class ChangeEventForm(forms.ModelForm):
     
     class Meta:
         model = Event
         widgets = {
-            'event_start': DateTimeWidget(
-                attrs = {'id': "event_start_datepicker"}, 
-                usel10n = True, 
-                bootstrap_version=3,
-                options = {
-                    'format': 'dd/mm/yyyy hh',
-                }
-            ),
-            'event_end': DateTimeWidget(
-                attrs = {'id': "event_end_datepicker"}, 
-                usel10n = True, 
-                bootstrap_version=3,
-                options = {
-                    'format': 'dd/mm/yyyy hh'
-                }
-            ),
+            'event_start': _get_datetime_widget('event_start'),
+            'event_end': _get_datetime_widget('event_end'),
         }
         fields = ['title', 'event_type', 'event_start', 'event_end', 'location', 'ingress_short', 'ingress', 'description', 'image']
 
@@ -35,3 +32,9 @@ class ChangeEventForm(forms.ModelForm):
 class ChangeAttendanceEventForm(forms.ModelForm):
     class Meta:
         model = AttendanceEvent
+        widgets = {
+            'registration_start': _get_datetime_widget('registration_end'),
+            'unattend_deadline': _get_datetime_widget('unattend_deadline'),
+            'registration_end': _get_datetime_widget('registration_end'),
+        }
+        fields = ['max_capacity', 'waitlist', 'guest_attendance', 'automatically_set_marks', 'registration_start', 'unattend_deadline', 'registration_end']
