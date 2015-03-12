dd0ab3928fa9b929f1a80936a96b2f3cf6d28747
Håvard Slettvold <slettvold@gmail.com>
2015-03-12 17:25:41 +0000

    Added datepicker to forms. Added some fields.

--- apps/events/dashboard/forms.py
+++ apps/events/dashboard/forms.py
@@ -1,6 +1,9 @@
 
 from django import forms
 
+from datetimewidget.widgets import DateTimeWidget
+from filebrowser.widgets import FileInput
+
 from apps.events.models import Event, AttendanceEvent
 
 
@@ -8,7 +11,25 @@ class ChangeEventForm(forms.ModelForm):
 
     class Meta:
         model = Event
-        fields = ['title', 'event_start', 'event_end', 'location', 'ingress_short', 'ingress', 'description']
+        widgets = {
+            'event_start': DateTimeWidget(
+                attrs = {'id': "event_start_datepicker"}, 
+                usel10n = True, 
+                bootstrap_version=3,
+                options = {
+                    'format': 'dd/mm/yyyy hh',
+                }
+            ),
+            'event_end': DateTimeWidget(
+                attrs = {'id': "event_end_datepicker"}, 
+                usel10n = True, 
+                bootstrap_version=3,
+                options = {
+                    'format': 'dd/mm/yyyy hh'
+                }
+            ),
+        }
+        fields = ['title', 'event_type', 'event_start', 'event_end', 'location', 'ingress_short', 'ingress', 'description', 'image']
 
 
 class ChangeAttendanceEventForm(forms.ModelForm):
