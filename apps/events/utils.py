7910ee25e3324a381e32db11fa7ff3cdebc7e20f
Håvard Slettvold <slettvold@gmail.com>
2015-03-12 17:28:58 +0000

    Changed how utils fetch allowed events. Added possibility of just getting the categories.

--- apps/events/utils.py
+++ apps/events/utils.py
@@ -1,13 +1,21 @@
-from apps.events.models import Event
+from apps.events.models import Event, TYPE_CHOICES
 
-def get_group_restricted_events(user):
+def get_group_restricted_events(user, all_events=False):
     """ Returns a queryset of events with attendance_event that a user has access to """
+    types_allowed = get_types_allowed(user)
+
+    if all_events:
+        return Event.objects.filter(event_type__in=types_allowed)
+    else:
+        return Event.objects.filter(attendance_event__isnull=False, event_type__in=types_allowed)
+
+def get_types_allowed(user):
     types_allowed = []
 
     groups = user.groups.all()
 
     if reduce(lambda r, g: g.name in ['Hovedstyret', 'dotKom'] or r, groups, False):
-        return Event.objects.filter(attendance_event__isnull=False)
+        return [t[0] for t in TYPE_CHOICES]
 
     for group in groups:
         if group.name == 'arrKom':
@@ -19,5 +27,5 @@ def get_group_restricted_events(user):
 
         if group.name == 'fagKom':
             types_allowed.append(3) # kurs
-
-    return Event.objects.filter(attendance_event__isnull=False, event_type__in=types_allowed)
+    
+    return types_allowed
