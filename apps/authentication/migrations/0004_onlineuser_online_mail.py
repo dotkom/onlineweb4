540d6a1071fb91af927a00ea93fb1b95a43ba153
Håvard Slettvold <slettvold@gmail.com>
2015-03-18 22:05:19 +0000

    Added field for online mail alias.

---
+++ apps/authentication/migrations/0004_onlineuser_online_mail.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authentication', '0003_auto_20150311_2105'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='onlineuser',
+            name='online_mail',
+            field=models.CharField(max_length=50, null=True, verbose_name='Online-epost', blank=True),
+            preserve_default=True,
+        ),
+    ]
