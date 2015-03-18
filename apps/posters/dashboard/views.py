dd140bef44fb83b32f0db2026ece76f8d6396ed2
sklirg <hlsolbjorg@gmail.com>
2015-03-18 23:36:04 +0100

    Assign proKommer to order

--- apps/posters/dashboard/views.py
+++ apps/posters/dashboard/views.py
@@ -4,14 +4,16 @@ import json
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 from django.core.exceptions import PermissionDenied, ObjectDoesNotExist
-from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect, HttpResponse
 from django.utils import timezone
 from django.utils.translation import ugettext as _
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 from guardian.decorators import permission_required
 
+from apps.authentication.models import OnlineUser as User
 from apps.dashboard.tools import has_access, get_base_context
 from apps.posters.models import Poster
 from apps.posters.forms import AddPosterForm, EditPosterForm
@@ -26,8 +28,16 @@ def index(request):
     if request.is_ajax():
         do_ajax_shit=True
 
-    #posters = Poster.objects.filter()
+    # The group with members who should populate the dropdownlist
+    group = Group.objects.get(name='proKom')
+    users_to_populate = group.user_set.all()
+    print(users_to_populate)
+
+
     context = get_base_context(request)
+    context['new_orders'] = Poster.objects.filter(assigned_to=None).order_by('-id')
+    context['active_orders'] = Poster.objects.filter(finished=False).order_by('-id')
+    context['workers'] = User.objects.filter(groups=Group.objects.get(name='proKom'))
 
     return render(request, 'posters/dashboard/index.html', context)
 
@@ -91,10 +101,43 @@ def change(request):
 @ensure_csrf_cookie
 @login_required
 @permission_required('posters.view_poster_order', return_403=True)
-def details(request):
+def detail(request, order_id=None):
     if request.is_ajax():
         do_ajax_shit=True
 
     context = get_base_context(request)
 
     return render(request, 'posters/dashboard/details.html', context)
+
+
+# Ajax
+
+
+# @ensure_csrf_cookie
+@login_required
+@permission_required('posters.view_poster_order', return_403=True)
+def assign_person(request):
+    if request.is_ajax():
+        if request.method == 'POST':
+            order_id = request.POST.get('order_id')
+            orders = Poster.objects.filter(pk=order_id)
+            assign_to_id = request.POST.get('assign_to_id')
+            assign_to = User.objects.get(pk=assign_to_id)
+
+            if orders.count() == 0:
+                response_text = json.dumps({'message': _(
+                    u"""Kan ikke finne en ordre med denne IDen (%s).
+Om feilen vedvarer etter en refresh, kontakt dotkom@online.ntnu.no.""") % order_id})
+                return HttpResponse(status=412, content=response_text)
+
+            order = orders[0]
+
+            if order.finished or order.assigned_to is not None:
+                response_text = json.dumps({'message': _(u"Denne ordren er allerede behandlet.")})
+                return HttpResponse(status=412, content=response_text)
+
+            order.assigned_to = assign_to
+            order.save()
+
+            return HttpResponse(status=200)
+
--- files/static/js/dashboard/posters.js
+++ files/static/js/dashboard/posters.js
@@ -1,60 +1,20 @@
 $(document).ready(function() {
-    $("div.application").each(function(i, row) {
-        $(row).find("button.approve").click(function() {
-            approveApplication($(this).val(), row);
+    console.log("waowoaowoa");
+    $("div.order").each(function(i, row) {
+        $(row).find("button.assign").click(function() {
+            assignToJob($(this).val(), row);
         });
-        $(row).find("button.decline").click(function() {
-            that = $(this);
-            // hide the first button
-            $(this).prop('disabled', true);
-            // show the row with message and confirmation
-            confirmrow = $("#confirm" + $(this).val());
-            $(confirmrow).show();
-            // find the button that will actually send the decline and set some stuff on it
-            $("button.sendDecline").click(function() {
-                $(confirmrow).hide();
-                message = $(confirmrow).find("textarea").val();
-                declineApplication($(this).val(), message, row);
-            });
-            // cancel should hide the row and enable the first button again
-            $("button.cancel").click(function() {
-                $(that).prop('disabled', false);
-                $(confirmrow).hide();
-            });
-        }).prop('disabled', false);
     });
     
-    var approveApplication = function(application_id, row) {
+    var assignToJob = function(order_id, row) {
+        var assign_to_id = $(row).find('form').find(':selected').val();
         var utils = Dashboard.tools
         $.ajax({
             method: 'POST',
-            url: 'approve_application/',
-            data: {'application_id': application_id, },
+            url: 'assign_person/',
+            data: {'order_id': order_id, 'assign_to_id': assign_to_id},
             success: function() {
                 $(row).css('background-color', '#b0ffb0');
-                $(row).fadeOut(500);
-            },
-            error: function(response) {
-                if (response['status'] === 412) {
-                    response = JSON.parse(response['responseText']);
-                    utils.setStatusMessage(response['message'], 'alert-danger');
-                }
-                else {
-                    utils.setStatusMessage('En uventet error ble oppdaget. Kontakt dotkom@online.ntnu.no for assistanse.', 'alert-danger');
-                }
-            },
-            crossDomain: false
-        });
-    }
-
-    var declineApplication = function(application_id, message, row) {
-        var utils = Dashboard.tools
-        $.ajax({
-            method: 'POST',
-            url: 'decline_application/',
-            data: {'application_id': application_id, 'message': message, },
-            success: function() {
-                $(row).css('background-color', '#f0b0b0');
                 $(row).fadeOut(500);
             },
             error: function(response) {
