8d686598c00b8fff1e84ab324e8d3d4a43088bf7
Håvard Slettvold <slettvold@gmail.com>
2015-03-20 12:47:00 +0000

    Added reservatino form to event editing. Some work progress in formset for reservees.

--- apps/events/dashboard/forms.py
+++ apps/events/dashboard/forms.py
@@ -5,7 +5,7 @@ from django import forms
 from datetimewidget.widgets import DateTimeWidget
 from filebrowser.widgets import FileInput
 
-from apps.events.models import Event, AttendanceEvent, RuleBundle
+from apps.events.models import Event, AttendanceEvent, RuleBundle, Reservation
 
 
 def _get_datetime_widget(field_name):
@@ -39,3 +39,9 @@ class ChangeAttendanceEventForm(forms.ModelForm):
             'registration_end': _get_datetime_widget('registration_end'),
         }
         fields = ['max_capacity', 'waitlist', 'guest_attendance', 'automatically_set_marks', 'registration_start', 'unattend_deadline', 'registration_end', 'rule_bundles']
+
+
+class ChangeReservationForm(forms.ModelForm):
+    class Meta:
+        model = Reservation
+        exclude = ['attendance_event',]
