3b7ead43d02390123a829f9cefdf0676d2d09903
Christian Duvholt <cxhristian@gmail.com>
2015-03-25 22:35:20 +0100

    Inline product images in product admin

--- apps/webshop/admin.py
+++ apps/webshop/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 
-from apps.webshop.models import Category, Product
+from apps.webshop.models import Category, Product, ProductImage
 
 
 class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ProductImageInline]
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
