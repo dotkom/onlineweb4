8be22e0e4fcb068600af4d210572c6ba7da6e021
Christian Duvholt <cxhristian@gmail.com>
2015-03-25 22:49:28 +0100

    Super simple category display

--- apps/webshop/urls.py
+++ apps/webshop/urls.py
@@ -6,6 +6,6 @@ from apps.webshop.views import Home, CategoryDetail, ProductDetail, Checkout
 urlpatterns = patterns('apps.webshop.views',
     url(r'^$', Home.as_view(), name='webshop_home'),
     url(r'^category/(?P<pk>\d+)/$', CategoryDetail.as_view(), name='webshop_category'),
-    url(r'^product/(?P<id>\d+)/(?P<slug>\w*)/$', ProductDetail.as_view(), name='webshop_product'),
+    url(r'^product/(?P<pk>\d+)/$', ProductDetail.as_view(), name='webshop_product'),
     url(r'^checkout/$', Checkout.as_view(), name='webshop_checkout'),
 )
