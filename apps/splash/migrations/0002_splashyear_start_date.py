19a76342f7f3aed7668501471901fe4e0db3bf94
Christian Strand Young <christian@strandyoung.com>
2015-04-22 22:48:31 +0200

    Migrations for splash

---
+++ apps/splash/migrations/0002_splashyear_start_date.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('splash', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='splashyear',
+            name='start_date',
+            field=models.DateField(default=datetime.datetime(2015, 4, 22, 20, 14, 54, 144759, tzinfo=utc), verbose_name='start_date'),
+            preserve_default=False,
+        ),
+    ]
