0e712db82d6e7b38a8447fc2f8b4a2c57ae5e536
Christian Duvholt <cxhristian@gmail.com>
2015-04-22 22:40:51 +0200

    Show order line status in each webshop view

--- apps/webshop/views.py
+++ apps/webshop/views.py
@@ -1,8 +1,21 @@
 from django.views.generic import TemplateView, DetailView
-from apps.webshop.models import Category, Product
+from apps.webshop.models import Category, Product, Order, OrderLine
 
 
-class Home(TemplateView):
+class CartMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(CartMixin, self).get_context_data(**kwargs)
+        context['order_line'] = self.current_order_line()
+        return context
+
+    def current_order_line(self):
+        order_line = OrderLine.objects.filter(user=self.request.user).first()
+        if not order_line:
+            order_line = OrderLine.objects.create(user=self.request.user)
+        return order_line
+
+
+class Home(CartMixin, TemplateView):
     template_name = 'webshop/base.html'
 
     def get_context_data(self, **kwargs):
@@ -11,13 +24,13 @@ class Home(TemplateView):
         return context
 
 
-class CategoryDetail(DetailView):
+class CategoryDetail(CartMixin, DetailView):
     model = Category
     context_object_name = 'category'
     template_name = 'webshop/category.html'
 
 
-class ProductDetail(DetailView):
+class ProductDetail(CartMixin, DetailView):
     model = Product
     context_object_name = 'product'
     template_name = 'webshop/product.html'
