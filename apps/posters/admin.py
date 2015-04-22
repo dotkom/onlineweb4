9d0d8c8cb4f231b8b9892a1d301714487296ab19
Nicolas A. Tonne <nixatto@gmail.com>
2015-04-22 22:11:37 +0200

    Regroup fields

--- apps/posters/admin.py
+++ apps/posters/admin.py
@@ -9,8 +9,8 @@ class PosterAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'company', 'when', 'assigned_to', 'display_from', 'display_to',
                     'ordered_date', 'ordered_by', 'ordered_committee')
     fieldsets = (
-        (_(u'Event info'), {'fields': ('title', 'when', 'company')}),
-        (_(u'Order info'), {'fields': ('category', 'amount', 'price', 'description', 'comments')}),
+        (_(u'Event info'), {'fields': ('title', 'when', 'company','price', 'description')}),
+        (_(u'Order info'), {'fields': ('category', 'amount', 'comments')}),
         (_(u'proKom'), {'fields': ('display_from', 'display_to', 'assigned_to', 'ordered_by', 'ordered_committee', 'finished')}),
     )
     search_fields = ('title', 'category', 'company', 'when')
