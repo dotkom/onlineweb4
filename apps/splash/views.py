a1bf03f69b9cadddcc7e0015788f23f9bad0f862
Tri Nguyen <mail@trimn.net>
2015-07-26 22:43:08 +0200

    Append event merging on splash_events

--- apps/splash/views.py
+++ apps/splash/views.py
@@ -6,6 +6,9 @@ from apps.splash.models import SplashEvent, SplashYear
 def index(request):
     # I'm really sorry ...
     splash_year = SplashYear.objects.get(start_date__gt=str(datetime.date.today() - datetime.timedelta(180)))
+
+    splash_year.events = _merge_events(splash_year.splash_events.all())
+
     return render(request, 'splash/base.html', {'splash_year': splash_year })
 
 
