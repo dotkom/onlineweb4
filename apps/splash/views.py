3c4f7906f98e6dfb9afe6993bee993ed05b969f3
Tri Nguyen <mail@trimn.net>
2015-07-26 22:42:49 +0200

    Add method for merging duplicated events

--- apps/splash/views.py
+++ apps/splash/views.py
@@ -7,3 +7,16 @@ def index(request):
     # I'm really sorry ...
     splash_year = SplashYear.objects.get(start_date__gt=str(datetime.date.today() - datetime.timedelta(180)))
     return render(request, 'splash/base.html', {'splash_year': splash_year })
+
+
+# And I'm really sorry for this ...
+def _merge_events(splash_events):
+    events = []
+
+    for event in splash_events:
+        if len(events) > 0 and event.start_time.strftime('%d-%m') == events[-1][0].start_time.strftime('%d-%m'):
+            events[-1].append(event)
+        else:
+            events.append([event])
+
+    return events
