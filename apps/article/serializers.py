71ec798d6a85a2aa0e4b80e6095d4da67612db70
sklirg <hlsolbjorg@gmail.com>
2015-08-18 16:15:01 +0200

    Clean up article serializer a bit

--- apps/article/serializers.py
+++ apps/article/serializers.py
@@ -5,7 +5,7 @@ from apps.authentication.serializers import UserSerializer
 
 
 class ArticleSerializer(serializers.ModelSerializer):
-    author = UserSerializer(source='created_by')  # serializers.StringRelatedField(source='created_by')
+    author = UserSerializer(source='created_by')
     absolute_url = serializers.CharField(source='get_absolute_url', read_only=True)
 
     class Meta:
@@ -13,7 +13,6 @@ class ArticleSerializer(serializers.ModelSerializer):
         fields = (
                 'absolute_url', 'additional_authors', 'author', 'changed_date', 'content', 'created_date',
                 'featured', 'heading', 'id',
-                # 'image_article_front_featured', 'image_article_front_small', 'image_article_full', 'image_article_main', 'image_article_related',
                 'ingress', 'ingress_short', 'photographers', 'published_date', 'slug', 'video', 'images', 'article_tags',
             )
 
