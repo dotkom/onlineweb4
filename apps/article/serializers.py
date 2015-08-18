6ad2e780e1816113d82e8aa683a931956f922269
sklirg <hlsolbjorg@gmail.com>
2015-08-18 15:19:29 +0200

    Initial serializers for article app

---
+++ apps/article/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from apps.article.models import Article, Tag
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(source='created_by')
+    absolute_url = serializers.CharField(source='get_absolute_url', read_only=True)
+
+    class Meta:
+        model = Article
+        fields = (
+                'absolute_url', 'additional_authors', 'author', 'changed_date', 'content', 'created_date',
+                'featured', 'heading', 'id',
+                # 'image_article_front_featured', 'image_article_front_small', 'image_article_full', 'image_article_main', 'image_article_related',
+                'ingress', 'ingress_short', 'photographers', 'published_date', 'slug', 'video', 'images', 'article_tags',
+            )
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('name', 'short_name')
