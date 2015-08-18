0746b8d764a3c9989ebf2a0983dcbc389d06c1d5
sklirg <hlsolbjorg@gmail.com>
2015-08-18 15:17:56 +0200

    Set up a shared router for api v1

---
+++ apps/api/utils.py
@@ -0,0 +1,11 @@
+from rest_framework.response import Response
+from rest_framework.routers import DefaultRouter, SimpleRouter
+
+
+class SharedAPIRootRouter(SimpleRouter):
+    shared_router = DefaultRouter()
+
+    def register(self, *args, **kwargs):
+        self.shared_router.register(*args, **kwargs)
+        # super().register(*args, **kwargs)  # Python 3 way of calling super method
+        super(SimpleRouter, self).register(*args, **kwargs)  # Python 2 way of calling super method
