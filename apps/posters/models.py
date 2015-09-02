c6d7d48b2c153ddcf63bf93571b7919956db7bd0
sklirg <hlsolbjorg@gmail.com>
2015-09-02 21:10:24 +0200

    Absolute and dashboard urls are actually the same. Keep for eventual future separation

--- apps/posters/models.py
+++ apps/posters/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.core.urlresolvers import reverse
 from django.forms import ModelForm
 from django.utils.translation import ugettext as _
 
@@ -57,12 +58,11 @@ class Poster(EventMixin):
     def poster_up(self):
         return self.finished and self.display_from < datetime.now().date() < self.display_to
 
-    @models.permalink
     def get_absolute_url(self):
-        return ('posters_details', None, {'order_id': self.id})
+        return reverse('posters_detail', args=[str(self.id)])
 
     def get_dashboard_url(self):
-        return '/dashboard%s' % self.get_absolute_url()
+        return self.get_absolute_url()
 
 
 class Freestyle(OrderMixin):
