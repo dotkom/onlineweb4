429e1fbb0d205d0f0190ccdc3f6fc12412d8132e
sklirg <hlsolbjorg@gmail.com>
2015-09-16 19:53:35 +0200

    Some documentation

--- apps/posters/models.py
+++ apps/posters/models.py
@@ -14,6 +14,14 @@ from apps.events.models import Event
 
 
 class OrderMixin(models.Model):
+    """
+    A mixin for all orders.
+    Handles most fields, and brings some basic functionality.
+
+    ordered_by and ordered_committee will get set automatically based on submitting user.
+    amount and description is optional, but should exist in all orders.
+    assigned_to and finished will be set by ProKom.
+    """
     order_type = models.IntegerField( choices=(
         (1, 'Plakat'),
         (2, 'Bong'),
@@ -33,14 +41,23 @@ class OrderMixin(models.Model):
 
 
 class EventMixin(OrderMixin):
+    """
+    Mixin for all orders requring an event.
+    """
     event = models.ForeignKey(Event, related_name=u'Arrangement')
 
 
 class GeneralOrder(OrderMixin):
+    """
+    General order, basically an order that does not have a corresponding event.
+    """
     title = models.CharField(_(u'arrangementstittel'), max_length=60)
 
 
 class Poster(EventMixin):
+    """
+    Poster order
+    """
     price = models.DecimalField(_(u'pris'), max_digits=10, decimal_places=2, blank=True, null=True)
     display_from = models.DateField(_(u"vis fra"), blank=True, null=True)
     display_to = models.DateField(_(u"vis til"), blank=True, null=True)
