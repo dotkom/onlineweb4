a539696902eece3def384de175c7032d07770a3e
mythern <myth@overflow.no>
2015-09-20 04:35:51 +0200

    Temp store changes. I am le tired.

--- files/static/js/gallery.js
+++ files/static/js/gallery.js
@@ -5,284 +5,300 @@
  TODO: Make the gallery prettier by framing stuff and allowing previews of different sizes
  */
 
-$.ajaxSetup({
-    crossDomain: false, // obviates need for sameOrigin test
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type)) {
-            xhr.setRequestHeader("X-CSRFToken", $.cookie('csrftoken'));
-        }
-    }
-});
+var Gallery = (function ($, tools) {
 
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
+    // Binds all buttons and event listeners as well as
+    // lazyloading for images
+    var bindEventListeners = function () {
 
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-            $('#large-preview').attr('src', e.target.result);
-        };
-
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-
-$("#image-input").change(function(){
-    readURL(this);
-});
-
-$("#zoominbutton").click(function() {
-    var image = $('#editing-image');
-    image.cropper('zoom', 0.1);
-});
-
-$("#zoomoutbutton").click(function() {
-    var image = $('#editing-image');
-    image.cropper('zoom', -0.1);
-});
-
-$("#resetbutton").click(function() {
-    var image = $('#editing-image');
-    image.cropper('reset', true);
-});
-
-$("#originalimagebutton").click(function() {
-    var image = $("#editing-image");
-    window.open(image.attr("src"));
-});
-
-
-var clearMessage = function(id) {
-    setTimeout(function() {
-        var message = $('#' + id);
-        message.fadeOut(1000, function() {
-            $(this).remove();
-        })
-    }, 6000);
-}
-
-var createMessage = function(message) {
-    var id = new Date().getTime();
-    var messageElement = document.createElement("p");
-    messageElement.setAttribute('id', id);
-    messageElement.setAttribute('class', 'gallery-message');
-    messageElement.innerHTML = message;
-    clearMessage(id);
-    return messageElement;
-}
-
-var setErrorMessage = function(message) {
-    var messageElement = createMessage(message);
-    messageElement.className += " " + 'text-danger' + " " + 'bg-danger';
-    $('div#messages').append(messageElement);
-}
-
-var setSuccessMessage = function(message) {
-    var messageElement = createMessage(message);
-    messageElement.className += " " + 'text-success' + " " + 'bg-success';
-    $('div#messages').append(messageElement);
-}
-
-
-var updateUneditedFiles = function() {
-    $.ajax({
-        method: 'GET',
-        url: 'number_of_untreated',
-        success: function(res) {
-            var res = jQuery.parseJSON(res);
-            var text = "Behandle (" + res['untreated'] + ")";
-            $("#edit-button > div.caption").text(text);
-        },
-        crossDomain: false
-    });
-};
-
-$(".bttrlazyloading").each(function() {
-    $(this).bttrlazyloading(
-        {
-            container: '#gallery'
-        }
-    );
-});
-
-
-var fetchUnhandledImages = function() {
-    setUpdateSpin();
-
-    $.ajax({
-        method: 'GET',
-        url: 'get_all_untreated',
-        success: function(res) {
-            res = jQuery.parseJSON(res);
-            updateAllUnhandledImages(res['untreated']);
-            setUpdateRefresh();
-        },
-        error: function() {
-            setUpdateRefresh();
-        },
-        crossDomain: false
-    });
-};
-
-
-var setUpdateSpin = function() {
-    $('a#fetchallimages > i').removeClass("fa-refresh").addClass("fa-spinner fa-spin");
-};
-
-var setUpdateRefresh = function() {
-    $('a#fetchallimages > i').removeClass("fa-spinner").removeClass("fa-spin").addClass("fa-refresh");
-};
-
-var setCropSpin = function() {
-    $('button#accept-crop-button > i').removeClass("fa-check").addClass("fa-spinner fa-spin");
-};
-
-var setCropDefault = function() {
-    $('button#accept-crop-button > i').removeClass("fa-spinner").removeClass("fa-spin").addClass("fa-check");
-};
-
-var clearEditView = function() {
-    var editPane = $('#image-edit-content');
-    var imageEditPreview = $("#image-edit-preview");
-    editPane.empty();
-    imageEditPreview.empty();
-};
-
-
-var showEditView = function() {
-    clearEditView();
-
-    var editPane = $('#image-edit-content');
-    var imageContainer = $('<div class="image-container">');
-    imageContainer.hide();
-
-    var image = $('<img id="editing-image" class="edit-image">');
-    image.attr("src", $(this).attr("data-image-url"));
-    image.attr("data-image-id", $(this).attr("data-image-id"));
-
-    editPane.append(imageContainer);
-    imageContainer.append(image);
-
-
-    // For reasons most speculative we need to give cropper some time to bind to the image.
-    // Probably because the DOM hasn't been redrawn, and styles set
-    setTimeout(function() {
-        image.cropper({
-            aspectRatio: 16/9,
-            preview: "div > #image-edit-preview",
-            autoCrop: true,
-            dragCrop: true,
-            modal: true,
-            movable: true,
-            resizable: true,
-            zoomable: true,
-            rotatable: false,
-            multiple: false,
-            done: function(data) {
-                $('#cropperDataX').val(data.x);
-                $('#cropperDataY').val(data.y);
-                $('#cropperDataHeight').val(data.height);
-                $('#cropperDataWidth').val(data.width);
-            }
+        $("#image-input").change(function(){
+            readURL(this);
         });
-        imageContainer.show();
-    }, 400);
-};
 
-var updateAllUnhandledImages = function(unhandledImages) {
-    var thumbnailContainer = $("#thumbnail-view");
-    thumbnailContainer.empty();
+        $("#zoominbutton").click(function() {
+            var image = $('#editing-image');
+            image.cropper('zoom', 0.1);
+        });
 
-    $.each(unhandledImages, function(index, value) {
-        var imageThumbnail = $('<img class="thumbnail-image" data-toggle="tab" data-target="#edit-pane">');
-        imageThumbnail.attr("src", value['thumbnail']);
-        imageThumbnail.attr("data-image-id", value['id']);
-        imageThumbnail.attr("data-image-url", value['image']);
+        $("#zoomoutbutton").click(function() {
+            var image = $('#editing-image');
+            image.cropper('zoom', -0.1);
+        });
 
-        thumbnailContainer.append(imageThumbnail);
-        imageThumbnail.on("click", showEditView);
-    });
-};
+        $("#resetbutton").click(function() {
+            var image = $('#editing-image');
+            image.cropper('reset', true);
+        });
 
-$("a#fetchallimages").click(function() {
-    fetchUnhandledImages();
-});
+        $("#originalimagebutton").click(function() {
+            var image = $("#editing-image");
+            window.open(image.attr("src"));
+        });
 
+        $(".bttrlazyloading").each(function() {
+            $(this).bttrlazyloading(
+                {
+                    container: '#gallery'
+                }
+            );
+        });
 
-var getCurrentEditImageName = function() {
-    var imageSrc = $('#editing-image').attr('src');
-    return imageSrc.substring(imageSrc.lastIndexOf('/') + 1);
+        $("#accept-crop-button").click(function() {
+            crop_image();
+        });
 
-};
+        $("#fetchallimages").click(function() {
+            fetchUnhandledImages();
+        });
 
+        $('#add-responsive-image').on('click', function (e) {
+            e.preventDefault()
+            $('#image-selection-wrapper').slideToggle(200)
+        })
 
-var getCurrentEditImageId = function() {
-    return $('#editing-image').attr('data-image-id');
-};
-
-
-var imageEditingSuccessful = function() {
-
-    var imageName = getCurrentEditImageName();
-    var imageId = getCurrentEditImageId();
-    var message = "Image '" + imageName + "' with id " + imageId + " was edited successfully!";
-
-    setSuccessMessage(message);
-    clearEditView();
-
-    $('#showthumbnailpane').tab('show');
-    fetchUnhandledImages();
-};
-
-
-var crop_image = function() {
-    var image = $('#editing-image');
-    var cropData = image.cropper("getData");
-    var image_name = $('#image-name');
-    cropData.name = image_name.val();
-
-    if (name.length < 2) {
-        alert('Du må gi bildet et navn!');
-        image_name.focus();
-        return;
-    }
-    if (cropData.height < 768 || cropData.width < 1024) {
-        return alert('Utsnittet er for lite. Det må være minst 1024x768');
+        $('#upload-responsive-image').on('click', function (e) {
+            e.preventDefault()
+            $('#image-upload-wrapper').slideToggle(200)
+        })
     }
 
-    cropData.id = image.attr("data-image-id");
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
 
-    setCropSpin();
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
-    $.post("crop_image", cropData, function() {
-        imageEditingSuccessful();
-    }).fail(function($xhr) {
-        setErrorMessage($xhr.responseJSON);
-    }).always(function() {
-        setCropDefault();
-    });
-};
+            reader.onload = function (e) {
+                $('#large-preview').attr('src', e.target.result);
+            };
 
-$("#accept-crop-button").click(function() {
-    crop_image();
-});
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    var clearMessage = function(id) {
+        setTimeout(function() {
+            var message = $('#' + id);
+            message.fadeOut(1000, function() {
+                $(this).remove();
+            })
+        }, 6000);
+    }
+
+    var createMessage = function(message) {
+        var id = new Date().getTime();
+        var messageElement = document.createElement("p");
+        messageElement.setAttribute('id', id);
+        messageElement.setAttribute('class', 'gallery-message');
+        messageElement.innerHTML = message;
+        clearMessage(id);
+        return messageElement;
+    }
+
+    var setErrorMessage = function(message) {
+        var messageElement = createMessage(message);
+        messageElement.className += " " + 'text-danger' + " " + 'bg-danger';
+        $('div#messages').append(messageElement);
+    }
+
+    var setSuccessMessage = function(message) {
+        var messageElement = createMessage(message);
+        messageElement.className += " " + 'text-success' + " " + 'bg-success';
+        $('div#messages').append(messageElement);
+    }
 
 
-// Initial stuff
+    var updateUneditedFiles = function() {
+        $.ajax({
+            method: 'GET',
+            url: '/gallery/number_of_untreated',
+            success: function(res) {
+                var res = jQuery.parseJSON(res);
+                var text = "Behandle (" + res['untreated'] + ")";
+                $("#edit-button > div.caption").text(text);
+            },
+            crossDomain: false
+        });
+    };
 
-updateUneditedFiles();
-fetchUnhandledImages();
+    var fetchUnhandledImages = function() {
+        setUpdateSpin();
 
-setInterval(function() {
-    updateUneditedFiles();
-}, 3000);
+        $.ajax({
+            method: 'GET',
+            url: '/gallery/get_all_untreated',
+            success: function(res) {
+                res = jQuery.parseJSON(res);
+                updateAllUnhandledImages(res['untreated']);
+                setUpdateRefresh();
+            },
+            error: function() {
+                setUpdateRefresh();
+            },
+            crossDomain: false
+        });
+    };
 
-// No idea what this does, but it made things work earlier
-// Can probably be removed
-new Blazy({
-    container: '#edit-pane'
-});
+    var setUpdateSpin = function() {
+        $('#fetchallimages > i').removeClass("fa-refresh").addClass("fa-spinner fa-spin");
+    };
+
+    var setUpdateRefresh = function() {
+        $('#fetchallimages > i').removeClass("fa-spinner").removeClass("fa-spin").addClass("fa-refresh");
+    };
+
+    var setCropSpin = function() {
+        $('#accept-crop-button > i').removeClass("fa-check").addClass("fa-spinner fa-spin");
+    };
+
+    var setCropDefault = function() {
+        $('#accept-crop-button > i').removeClass("fa-spinner").removeClass("fa-spin").addClass("fa-check");
+    };
+
+    var clearEditView = function() {
+        var editPane = $('#image-edit-content');
+        var imageEditPreview = $("#image-edit-preview");
+        editPane.empty();
+        imageEditPreview.empty();
+    };
+
+    var showEditView = function() {
+        clearEditView();
+
+        var editPane = $('#image-edit-content');
+        var imageContainer = $('<div class="image-container">');
+        imageContainer.hide();
+
+        var image = $('<img id="editing-image" class="edit-image">');
+        image.attr("src", $(this).attr("data-image-url"));
+        image.attr("data-image-id", $(this).attr("data-image-id"));
+
+        editPane.append(imageContainer);
+        imageContainer.append(image);
+
+
+        // For reasons most speculative we need to give cropper some time to bind to the image.
+        // Probably because the DOM hasn't been redrawn, and styles set
+        setTimeout(function() {
+            image.cropper({
+                aspectRatio: 16/9,
+                preview: "div > #image-edit-preview",
+                autoCrop: true,
+                dragCrop: true,
+                modal: true,
+                movable: true,
+                resizable: true,
+                zoomable: true,
+                rotatable: false,
+                multiple: false,
+                done: function(data) {
+                    $('#cropperDataX').val(data.x);
+                    $('#cropperDataY').val(data.y);
+                    $('#cropperDataHeight').val(data.height);
+                    $('#cropperDataWidth').val(data.width);
+                }
+            });
+            imageContainer.show();
+        }, 400);
+    };
+
+    var updateAllUnhandledImages = function(unhandledImages) {
+        var thumbnailContainer = $("#thumbnail-view");
+        thumbnailContainer.empty();
+
+        $.each(unhandledImages, function(index, value) {
+            var imageThumbnail = $('<img class="thumbnail-image" data-toggle="tab" data-target="#edit-pane">');
+            imageThumbnail.attr("src", value['thumbnail']);
+            imageThumbnail.attr("data-image-id", value['id']);
+            imageThumbnail.attr("data-image-url", value['image']);
+
+            thumbnailContainer.append(imageThumbnail);
+            imageThumbnail.on("click", showEditView);
+        });
+    };
+
+    var getCurrentEditImageName = function() {
+        var imageSrc = $('#editing-image').attr('src');
+        return imageSrc.substring(imageSrc.lastIndexOf('/') + 1);
+
+    };
+
+    var getCurrentEditImageId = function() {
+        return $('#editing-image').attr('data-image-id');
+    };
+
+    var imageEditingSuccessful = function() {
+
+        var imageName = getCurrentEditImageName();
+        var imageId = getCurrentEditImageId();
+        var message = "Image '" + imageName + "' with id " + imageId + " was edited successfully!";
+
+        setSuccessMessage(message);
+        clearEditView();
+
+        $('#showthumbnailpane').tab('show');
+        fetchUnhandledImages();
+    };
+
+
+    var crop_image = function() {
+        var image = $('#editing-image');
+        var cropData = image.cropper("getData");
+        var image_name = $('#image-name');
+        cropData.name = image_name.val();
+
+        if (name.length < 2) {
+            alert('Du må gi bildet et navn!');
+            image_name.focus();
+            return;
+        }
+        if (cropData.height < 768 || cropData.width < 1024) {
+            return alert('Utsnittet er for lite. Det må være minst 1024x768');
+        }
+
+        cropData.id = image.attr("data-image-id");
+
+        setCropSpin();
+
+        $.post("crop_image", cropData, function() {
+            imageEditingSuccessful();
+        }).fail(function($xhr) {
+            setErrorMessage($xhr.responseJSON);
+        }).always(function() {
+            setCropDefault();
+        });
+    };
+
+    return {
+        init: function () {
+            if ($('#image-upload-wrapper').length || $('#image-selection-wrapper')) {
+
+                $.ajaxSetup({
+                    crossDomain: false, // obviates need for sameOrigin test
+                    beforeSend: function(xhr, settings) {
+                        if (!csrfSafeMethod(settings.type)) {
+                            xhr.setRequestHeader("X-CSRFToken", $.cookie('csrftoken'));
+                        }
+                    }
+                });
+
+                // Initial stuff
+                bindEventListeners();
+                updateUneditedFiles();
+                fetchUnhandledImages();
+
+                setInterval(function() {
+                    updateUneditedFiles();
+                }, 3000);
+            }
+        },
+        widget: {
+
+        }
+    }
+})(jQuery, Dashboard.tools)
+
+Gallery.init()
