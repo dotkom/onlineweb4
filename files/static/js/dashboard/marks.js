31c55eafaf75ea7ec2968aab9c58642182c17260
OptimusCrime <thomasgautv@hotmail.com>
2015-09-23 19:55:43 +0200

    Prettify js script, swap user search endpoint

--- files/static/js/dashboard/marks.js
+++ files/static/js/dashboard/marks.js
@@ -2,41 +2,41 @@ var Marks = (function ($, tools) {
 
     // Perform self check, display error if missing deps
     var performSelfCheck = function () {
-        var errors = false
-        if ($ == undefined) console.error('jQuery missing!')
-        if (tools == undefined) console.error('Dashboard tools missing!')
-        if (errors) return false
+        var errors = false;
+        if ($ == undefined) console.error('jQuery missing!');
+        if (tools == undefined) console.error('Dashboard tools missing!');
+        if (errors) return false;
         return true
-    }
+    };
 
     // Private generic ajax handler.
     // 
     // :param id: user ID in database
     // :param action: either 'remove_user' or 'add_user'
     var ajax_usermod = function (id, action) {
-        var url = window.location.href.toString()
+        var url = window.location.href.toString();
         var data = {
                 "user_id": id,
                 "action": action
-        }
+        };
         var success = function (data) {
             if (action === 'remove_user') {
-                data = JSON.parse(data)
-                draw_table('userlist', data)
+                data = JSON.parse(data);
+                draw_table('userlist', data);
             }
             else if (action === 'add_user') {
-                data = JSON.parse(data)
-                draw_table('userlist', data)
+                data = JSON.parse(data);
+                draw_table('userlist', data);
             }
-        }
+        };
         var error = function (xhr, txt, error) {
             tools.showStatusMessage(error, 'alert-danger')
-        }
+        };
 
         // Make an AJAX request using the Dashboard tools module
         tools.ajax('POST', url, data, success, error, null)
 
-    }
+    };
 
     return {
 
@@ -47,20 +47,20 @@ var Marks = (function ($, tools) {
 
             // Bind add users button
             $('#marks_details_users_button').on('click', function (e) {
-                e.preventDefault()
-                $('#marks_details_users').slideToggle(200)
-                $('#usersearch').focus()
-            })
+                e.preventDefault();
+                $('#marks_details_users').slideToggle(200);
+                $('#usersearch').focus();
+            });
             
             // Bind remove user buttons
             $('.remove-user').each(function (i) {
-                var cell = $(this).parent()
-                var user_id = $(this).attr('id')
+                var cell = $(this).parent();
+                var user_id = $(this).attr('id');
                 $(this).on('click', function (e) {
-                    e.preventDefault()
-                    Marks.user.remove(user_id)
+                    e.preventDefault();
+                    Marks.user.remove(user_id);
                 })
-            })
+            });
             
             /* Typeahead for user search */
 
@@ -69,16 +69,16 @@ var Marks = (function ($, tools) {
                 return this.each(function() {
                     this.disabled = !this.disabled
                 })
-            }
+            };
 
             // Typeahead template
             var user_search_template =  [
-                '<span data-id="{{ id }}" class="user-meta"><h4>{{ name }}</h4>'
-            ].join('')
+                '<span data-id="{{ id }}" class="user-meta"><h4>{{ value }}</h4>'
+            ].join('');
             
             // Bind the input field
             $('#usersearch').typeahead({
-                remote: "/profile/api_user_search/?query=%QUERY",
+                remote: "/profile/api_plain_user_search/?query=%QUERY",
                 updater: function (item) {
                     return item
                 },
@@ -86,7 +86,7 @@ var Marks = (function ($, tools) {
                 engine: Hogan
             }).on('typeahead:selected typeahead:autocompleted', function(e, datum) {
                 ($(function() {
-                    Group.user.add(datum.id)
+                    Marks.user.add(datum.id);
                     $('#usersearch').val('')
                 }))
             })
@@ -106,8 +106,8 @@ var Marks = (function ($, tools) {
         }
     }
 
-})(jQuery, Dashboard.tools)
+})(jQuery, Dashboard.tools);
 
 $(document).ready(function () {
-    Marks.init()
-})
+    Marks.init();
+});
