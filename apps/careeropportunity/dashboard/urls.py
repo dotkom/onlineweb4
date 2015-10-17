9cbe73665bfbef3b33253ed7200e7a8f05dbbfc0
sklirg <hlsolbjorg@gmail.com>
2015-10-17 20:05:23 +0200

    A nicer pep8

--- apps/careeropportunity/dashboard/urls.py
+++ apps/careeropportunity/dashboard/urls.py
@@ -2,9 +2,10 @@
 
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('apps.careeropportunity.dashboard.views',
-                       url(r'^$', 'index', name='careeropportunity_dashboard_index'),
-                       url(r'^add/$', 'detail', name='careeropportunity_dashboard_add'),
-                       url(r'^(?P<opportunity_id>\d+)/detail', 'detail', name='careeropportunity_dashboard_edit'),
-                       url(r'^(?P<opportunity_id>\d+)/delete', 'delete', name='careeropportunity_dashboard_delete'),
-                       )
+urlpatterns = patterns(
+    'apps.careeropportunity.dashboard.views',
+    url(r'^$', 'index', name='careeropportunity_dashboard_index'),
+    url(r'^add/$', 'detail', name='careeropportunity_dashboard_add'),
+    url(r'^(?P<opportunity_id>\d+)/detail', 'detail', name='careeropportunity_dashboard_edit'),
+    url(r'^(?P<opportunity_id>\d+)/delete', 'delete', name='careeropportunity_dashboard_delete'),
+)
