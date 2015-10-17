60fdfab7e3b557e46276c225ff159f5773930525
mythern <myth@overflow.no>
2015-10-17 19:26:32 +0200

    Change careeropportunity migration dep

--- apps/careeropportunity/migrations/0001_initial.py
+++ apps/careeropportunity/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('companyprofile', '0001_initial'),
+        ('companyprofile', '0001_squashed_0003_company_image'),
     ]
 
     operations = [
