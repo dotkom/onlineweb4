75a477c44e958ff447378f04bf6f0ef825a0ac28
mythern <myth@overflow.no>
2015-10-17 21:54:01 +0200

    Mergemigration

---
+++ apps/authentication/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authentication', '0009_auto_20151016_2033'),
+        ('authentication', '0010_auto_20151014_2058'),
+    ]
+
+    operations = [
+    ]
