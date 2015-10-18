f6de6e14a5b01295b270d92147e6ce097397a429
myth <myth@overflow.no>
2015-10-18 12:44:04 +0200

    Add pagination skeleton

---
+++ files/static/js/dashboard/article.js
@@ -0,0 +1,17 @@
+/**
+ * Created by myth on 10/18/15.
+ */
+
+var Article = (function ($, tools) {
+    // Private fields and methods
+
+    // Public fields
+    return {
+        init: {
+            // TODO: Bind buttons
+        },
+
+    }
+})(jQuery, Dashboard.tools)
+
+Article.init()
