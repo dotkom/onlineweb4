fe3ae13e258bb17398b27b83a07259ca0e7a6174
myth <myth@overflow.no>
2015-10-18 15:49:51 +0200

    Fix minor issues in urls

--- apps/marks/dashboard/urls.py
+++ apps/marks/dashboard/urls.py
@@ -2,10 +2,11 @@
 
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('apps.marks.dashboard.views',
+urlpatterns = patterns(
+    'apps.marks.dashboard.views',
     url(r'^$', 'index', name='marks_index'),
     url(r'^(?P<pk>\d+)/$', 'mark_details', name='mark_details'),
-    url(r'^(?P<pk>\d+)/delete$', 'mark_delete', name='mark_delete'),
-    url(r'^(?P<pk>\d+)/edit', 'mark_edit', name='mark_edit'),
+    url(r'^(?P<pk>\d+)/delete/$', 'mark_delete', name='mark_delete'),
+    url(r'^(?P<pk>\d+)/edit/$', 'mark_edit', name='mark_edit'),
     url(r'^new/$', 'marks_new', name='marks_new'),
 )
