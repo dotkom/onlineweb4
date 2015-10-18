8f4c3c43464d0a1a5a7ca88c555848f6b6fa45df
mythern <myth@overflow.no>
2015-10-18 10:09:55 +0200

    Add migration

---
+++ apps/article/migrations/0005_auto_20151018_1008.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('article', '0004_article_image'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='article',
+            name='image',
+            field=models.ForeignKey(default=None, blank=True, to='gallery.ResponsiveImage', null=True),
+            preserve_default=True,
+        ),
+    ]
