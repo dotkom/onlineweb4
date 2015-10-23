4fa020d08455651b54f858bd874cfad6ccdb11d7
myth <myth@overflow.no>
2015-10-24 01:41:58 +0200

    Add gallery migration

---
+++ apps/gallery/migrations/0007_auto_20151024_0141.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('gallery', '0006_auto_20151018_0707'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='unhandledimage',
+            options={'verbose_name': 'Ubehandlet bilde', 'verbose_name_plural': 'Ubehandlede bilder', 'permissions': (('view_unhandledimage', 'View UnhandledImage'),)},
+        ),
+    ]
