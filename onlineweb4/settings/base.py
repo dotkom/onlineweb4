6ff96acd8eec35fd7a7c10f69c101ae9af2fa864
Christian Duvholt <cxhristian@gmail.com>
2015-10-28 21:11:16 +0100

    Remove wiki notifications. Has never been used

--- onlineweb4/settings/base.py
+++ onlineweb4/settings/base.py
@@ -260,7 +260,6 @@ INSTALLED_APPS = (
     #Wiki
     'wiki',
     'wiki.plugins.attachments',
-    'wiki.plugins.notifications',
     'wiki.plugins.images',
     'wiki.plugins.macros',
     'wiki.plugins.help',
