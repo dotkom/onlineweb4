cfb5b575246346de9a7cae197a8931c2f4a8e0a1
Christian Duvholt <cxhristian@gmail.com>
2015-10-28 21:54:33 +0100

    Add fields for webshop model forms

--- apps/webshop/dashboard/forms.py
+++ apps/webshop/dashboard/forms.py
@@ -7,8 +7,10 @@ from apps.webshop.models import Category, Product
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
+        fields = ['name', 'slug']
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
+        fields = ['name', 'slug', 'short', 'description']
