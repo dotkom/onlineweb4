e501528b3cfc5ab39c1f877294558859938634c4
myth <myth@overflow.no>
2015-10-29 22:50:43 +0100

    Change field description for authors (this needs to be a 2 step process, otherwise django things its a delete and add migration, and not a rename + no-op. Replace references to additional_authors to authors in API serializer.

--- apps/article/serializers.py
+++ apps/article/serializers.py
@@ -6,14 +6,14 @@ from apps.gallery.serializers import ResponsiveImageSerializer
 
 
 class ArticleSerializer(serializers.ModelSerializer):
-    author = UserSerializer(source='created_by')
+    created_by = UserSerializer()
     image = ResponsiveImageSerializer()
     absolute_url = serializers.CharField(source='get_absolute_url', read_only=True)
 
     class Meta:
         model = Article
         fields = (
-                'absolute_url', 'additional_authors', 'author', 'changed_date', 'content', 'created_date',
+                'absolute_url', 'authors', 'created_by', 'changed_date', 'content', 'created_date',
                 'featured', 'heading', 'id',
                 'ingress', 'ingress_short', 'photographers', 'published_date', 'slug', 'video', 'image', 'article_tags',
         )
