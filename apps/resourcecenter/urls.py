48863e7b5cf6aa45a9d42e0f7c7e6b843379045f
myth <myth@overflow.no>
2015-10-31 15:50:49 +0100

    pep8 resourcecenter urls

--- apps/resourcecenter/urls.py
+++ apps/resourcecenter/urls.py
@@ -2,9 +2,8 @@
 
 from django.conf.urls import patterns, url 
 
-urlpatterns = patterns('apps.resourcecenter.views',
-	# Index page
+urlpatterns = patterns(
+    'apps.resourcecenter.views',
     url(r'^$', 'index', name='resourcecenter_index'),
-    # Subpages
     url(r'^gameservers/$', 'gameservers', name='resourcecenter_gameservers'),
 )
