6aef91a9c119cc50f64a8c5874d67cd58cd8003f
myth <myth@overflow.no>
2015-10-31 17:30:03 +0100

    pep8 mommy mgmt

--- apps/mommy/management/commands/mommy-task-run.py
+++ apps/mommy/management/commands/mommy-task-run.py
@@ -1,11 +1,13 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from apps import mommy
+
 
 class Command(BaseCommand):
     args = 'name of job'
     help = 'run a job'
 
-    def print_job_names(self):
+    @staticmethod
+    def print_job_names():
         possible_jobs = []
         for task, _ in mommy.schedule.tasks.iteritems():
             possible_jobs.append(task.__name__)
@@ -14,14 +16,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         mommy.autodiscover()
 
-        if(len(args) == 0):
+        if len(args) == 0:
             self.print_job_names()
             return
 
         # run shit
         do_name = args[0]
         for task, _ in mommy.schedule.tasks.iteritems():
-            if(task.__name__ == do_name):
+            if task.__name__ == do_name:
                 task.run()
                 return
         print "could not find job:" + do_name
