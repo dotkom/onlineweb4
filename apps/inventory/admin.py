9c9f769c0ff664f9f7434bbca39949821369b34e
myth <myth@overflow.no>
2015-10-31 17:12:59 +0100

    pep8 inventory admin

--- apps/inventory/admin.py
+++ apps/inventory/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from django.conf import settings
 from apps.inventory.models import Item, Batch
+
 
 class BatchInline(admin.TabularInline):
     model = Batch
     extra = 0
 
+
 class ItemAdmin(admin.ModelAdmin):
     inlines = [BatchInline]
     model = Item
-    list_display = ["name","total_amount","oldest_expiration_date","last_added"]
+    list_display = [
+        'name',
+        'total_amount',
+        'oldest_expiration_date',
+        'last_added'
+    ]
 
 admin.site.register(Item, ItemAdmin)
