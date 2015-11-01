a093f68221db1ab1985384fe58daca328c3f59b3
myth <myth@overflow.no>
2015-11-01 05:24:13 +0100

    Add migrations for modelchanges in article. Tag and ArticleTag have been deleted.

---
+++ apps/article/migrations/0010_auto_20151101_0522.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('article', '0009_article_tags'),
+    ]
+
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='articletag',
+            unique_together=set([]),
+        ),
+        migrations.RemoveField(
+            model_name='articletag',
+            name='article',
+        ),
+        migrations.RemoveField(
+            model_name='articletag',
+            name='tag',
+        ),
+        migrations.RemoveField(
+            model_name='article',
+            name='old_image',
+        ),
+        migrations.RemoveField(
+            model_name='article',
+            name='photographers',
+        ),
+        migrations.DeleteModel(
+            name='ArticleTag',
+        ),
+        migrations.DeleteModel(
+            name='Tag',
+        ),
+    ]
