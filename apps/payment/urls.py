f1c75930a92f50fe7269d463f68b0e968c80d153
Tor-Håkon Bonsaksen <torhaakb@stud.ntnu.no>
2015-11-04 22:11:54 +0100

    Payment implemented

--- apps/payment/urls.py
+++ apps/payment/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('apps.payment.views',
      url(r'^$', 'payment', name='payment'),
      url(r'^payment_info/$', 'payment_info', name='payment_info'),
+     url(r'^webshop_info/$', 'webshop_info', name='webshop_info'),
+     url(r'^webshop_pay/$', 'webshop_pay', name='webshop_pay'),
      url(r'^refund/(?P<payment_relation_id>\d+)$', 'payment_refund', name='payment_refund'),
 )
