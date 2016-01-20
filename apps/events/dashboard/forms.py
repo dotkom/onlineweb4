381d1e9a791e5a2fa87d80868065c1f3b69d0f60
sklirg <hlsolbjorg@gmail.com>
2016-01-20 21:02:37 +0100

    Hide non-functional menus.

--- apps/events/dashboard/forms.py
+++ apps/events/dashboard/forms.py
@@ -2,32 +2,24 @@
 
 from django import forms
 
-from datetimewidget.widgets import DateTimeWidget
-from filebrowser.widgets import FileInput
-
+from apps.dashboard.widgets import DatetimePickerInput, multiple_widget_generator
 from apps.events.models import Event, AttendanceEvent, RuleBundle, Reservation
-
-
-def _get_datetime_widget(field_name):
-    return DateTimeWidget(
-                          attrs = {'id': field_name + "_datepicker"}, 
-                          usel10n = True, 
-                          bootstrap_version=3,
-                          options = {
-                              'format': 'dd/mm/yyyy hh:ii',
-                          }
-                      )
 
 
 class ChangeEventForm(forms.ModelForm):
 
     class Meta:
         model = Event
-        widgets = {
-            'event_start': _get_datetime_widget('event_start'),
-            'event_end': _get_datetime_widget('event_end'),
-        }
         fields = ['title', 'event_type', 'event_start', 'event_end', 'location', 'ingress_short', 'ingress', 'description', 'image']
+
+        dtp_fields = [('event_start', {}), ('event_end', {})]
+
+        widgetlist = [
+            (DatetimePickerInput, dtp_fields)
+        ]
+
+        # Multiple widget generator merges results from regular widget_generator into a single widget dict
+        widgets = multiple_widget_generator(widgetlist)
 
 
 class ChangeAttendanceEventForm(forms.ModelForm):
@@ -38,11 +30,15 @@ class ChangeAttendanceEventForm(forms.ModelForm):
                 'registration_start', 'registration_end', 'unattend_deadline',
                 'automatically_set_marks', 'marks_has_been_set', 'rule_bundles',
                 )
-        widgets = {
-            'registration_start': _get_datetime_widget('registration_end'),
-            'unattend_deadline': _get_datetime_widget('unattend_deadline'),
-            'registration_end': _get_datetime_widget('registration_end'),
-        }
+
+        dtp_fields = [('registration_start', {}), ('registration_end', {}), ('unattend_deadline', {})]
+
+        widgetlist = [
+            (DatetimePickerInput, dtp_fields)
+        ]
+
+        # Multiple widget generator merges results from regular widget_generator into a single widget dict
+        widgets = multiple_widget_generator(widgetlist)
 
 
 class ChangeReservationForm(forms.ModelForm):
