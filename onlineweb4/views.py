6be65bc8d72998ad34c7868d0c84b1dbf7e14f46
nornes <andrehn@stud.ntnu.no>
2016-03-16 20:52:47 +0100

    fix styling and logging

--- onlineweb4/views.py
+++ onlineweb4/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from smtplib import SMTPException
 
 from onlineweb4.forms import ErrorForm
+from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import redirect, render
 from django.conf import settings
@@ -9,14 +12,25 @@ from django.conf import settings
 
 
 def server_error(request):
+    log = logging.getLogger(__name__)
+
     if request.method == 'POST':
         form = ErrorForm(request.POST)
-        message = request.POST.get('reason')
+        message = request.POST.get('reason', 'Ingen forklaring oppgitt.')
         try: 
+            log.error('%s triggered a 500 server error and provided the following description: %s' % (
+                request.user,
+                message
+            ))
             send_mail('500error user-report', message,
                 settings.DEFAULT_FROM_EMAIL, [settings.EMAIL_DOTKOM])
+            log.debug('Finished sending error email to %s' % settings.EMAIL_DOTKOM)
+
+            messages.success(request, 'Feilmeldingen din ble sendt til %s' % settings.EMAIL_DOTKOM)
+
             return redirect('home')
         except SMTPException:
+            messages.error(request, 'Det oppstod en uventet feil under sending av feilmeldingen')
             return redirect('home')
 
     return render(request, '500.html', {'error_form': ErrorForm})
