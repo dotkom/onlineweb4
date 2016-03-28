4a4ad58865ca29721afe154ba9b85f9417aec38f
sklirg <hlsolbjorg@gmail.com>
2016-03-28 23:56:56 +0900

    Tests all public + safe resourcecenter URLs

--- apps/resourcecenter/tests.py
+++ apps/resourcecenter/tests.py
@@ -4,8 +4,11 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from unittest import skip
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from rest_framework import status
 
 
 class SimpleTest(TestCase):
@@ -14,3 +17,19 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class ResourceCenterURLTestCase(TestCase):
+    def test_resourcecenter_index(self):
+        url = reverse('resourcecenter_index')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_resourcecenter_gameservers(self):
+        url = reverse('resourcecenter_gameservers')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
