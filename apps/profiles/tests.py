7bbd3036ee94321cf6f6aa8383b3f199ef67ffb9
sklirg <hlsolbjorg@gmail.com>
2016-03-28 23:52:41 +0900

    Tests all public + safe profile URLs

--- apps/profiles/tests.py
+++ apps/profiles/tests.py
@@ -4,13 +4,21 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django_dynamic_fixture import G
+from rest_framework import status
+
+from apps.authentication.models import OnlineUser as User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ProfilesURLTestCase(TestCase):
+    def test_user_search(self):
+        user = G(User)
+        url = reverse('profiles_user_search')
+
+        self.client.force_login(user)
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
