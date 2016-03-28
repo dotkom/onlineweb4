02d44b6f1df092440d7c1a6b27dc74be9a9c49fd
sklirg <hlsolbjorg@gmail.com>
2016-03-28 22:14:04 +0900

    Tests all public + safe companyprofile URLs

--- apps/companyprofile/tests.py
+++ apps/companyprofile/tests.py
@@ -2,8 +2,10 @@
 
 import logging
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django_dynamic_fixture import G
+from rest_framework import status
 
 from apps.companyprofile.models import Company
 
@@ -17,3 +19,14 @@ class CompanyTests(TestCase):
     def testCompanyUniqodeIsCorrect(self):
         self.logger.debug("Company __str__() should return correct name")
         self.assertEqual(self.company.__str__(), "testname")
+
+
+class CompanyProfileURLTestCase(TestCase):
+    def test_company_profile_detail(self):
+        company = G(Company)
+
+        url = reverse('company_details', args=(company.id,))
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
