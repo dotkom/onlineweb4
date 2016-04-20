d2f9b39dbb053f6c219f0f9d4f92e9649e1592cb
Torrib <torrib90@gmail.com>
2016-04-20 21:20:25 +0200

    Inventory category implementation (#1520)
    * Added item category to the dashboard
    
    * Don't render table if empty.
    
    * replaced harcoded urls with django reverse urls

--- apps/inventory/dashboard/forms.py
+++ apps/inventory/dashboard/forms.py
@@ -3,17 +3,24 @@ from django import forms
 
 from apps.dashboard.widgets import widget_generator
 from apps.gallery.widgets import SingleImageInput
-from apps.inventory.models import Batch, Item
+from apps.inventory.models import Batch, Item, ItemCategory
 
 
 class ItemForm(forms.ModelForm):
 
     class Meta(object):
         model = Item
-        fields = ('name', 'description', 'available', 'price', 'image')
+        fields = ('name', 'description', 'category', 'available', 'price', 'image')
 
         # Widget generator accepts a form widget, and a list of tuples between field name and an attribute dict
         widgets = widget_generator(SingleImageInput, [('image', {'id': 'responsive-image-id'})])
+
+
+class CategoryForm(forms.ModelForm):
+
+    class Meta(object):
+        model = ItemCategory
+        fields = ('name', )
 
 
 class BatchForm(forms.ModelForm):
