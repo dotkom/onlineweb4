cd112be72d71e6d98dfebc27259d8fee712df987
sklirg <hlsolbjorg@gmail.com>
2016-04-20 22:03:02 +0200

    Add DeleteView. Also workaround bug with DashboardPermissionsMixin.

--- apps/dashboard/chunks/dashboard/views.py
+++ apps/dashboard/chunks/dashboard/views.py
@@ -1,6 +1,6 @@
 from chunks.models import Chunk
 from django.core.urlresolvers import reverse, reverse_lazy
-from django.views.generic import CreateView, ListView, UpdateView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from apps.dashboard.tools import DashboardPermissionMixin
 
@@ -25,5 +25,17 @@ class ChunkCreate(DashboardPermissionMixin, CreateView):
     fields = ('key', 'description', 'content')
     permission_required = 'chunks.add_chunk'
 
+    def get_object(self, queryset=None):
+        # Overridden to work around https://github.com/django-guardian/django-guardian/issues/146,
+        # an issue with regards to guardian's PermissionMixin working with CreateView -
+        # where it requires an object even in a CreateView.
+        return None
+
     def get_success_url(self):
         return reverse('chunk-dashboard:update', args=(self.object.id,))
+
+
+class ChunkDelete(DashboardPermissionMixin, DeleteView):
+    model = Chunk
+    permission_required = ('chunks.delete_chunk',)
+    success_url = reverse_lazy('chunk-dashboard:list')
