e63a70d138a17a011d7dde684a18879839aac2ed
Iver Egge <iverasp@gmail.com>
2016-04-29 18:58:05 +0200

    map group names in ow4 to ldap groups.
    call update_group_memberships after updating a user. this method removes the user from groups it is no longer a member of and adds the user to groups it should be a member of, if needed.

--- apps/ldap/ldap.py
+++ apps/ldap/ldap.py
@@ -1,9 +1,21 @@
 # -*- coding: utf8 -*-
 
-from apps.ldap.ldap_models import LdapUser
+from apps.ldap.ldap_models import LdapUser, LdapGroup
 
 MIN_UID = 2000
 
+GROUP_MAP = {
+    'arrKom': 'arrkom',
+    'banKom': 'bankom',
+    'bedKom': 'bedkom',
+    'dotKom': 'dotkom',
+    'fagKom': 'fagkom',
+    'Hovedstyret': 'hovedstyret',
+    'jubKom': 'jubkom',
+    'Komiteer': 'komiteer',
+    'proKom': 'prokom',
+    'triKom': 'trikom',
+}
 
 def upsert_user_ldap(user, pwd):
     """
@@ -56,8 +68,10 @@ def update(user, pwd):
         ldap_user.last_name = user.last_name
         ldap_user.set_password(pwd)
         ldap_user.save()
+        update_group_memberships(user)
         return True;
     except Exception:
+        print("exception")
         return False
 
 
@@ -73,6 +87,23 @@ def insert(user, pwd):
     # TODO: everything
     return False
 
+def update_group_memberships(user):
+    ldap_groups = []
+    for group in user.groups.all():
+        if group.name in GROUP_MAP: ldap_groups.append(GROUP_MAP[group.name])
+
+    # remove user from groups the user is no longer a member of
+    for ldap_group in LdapGroup.objects.all():
+        if not ldap_group.name in ldap_groups and user.ntnu_username in ldap_group.usernames:
+            ldap_group.usernames.remove(user.ntnu_username)
+            ldap_group.save()
+
+    # add user to new groups
+    for ldap_group in ldap_groups:
+        group = LdapGroup.objects.filter(name=ldap_group).first()
+        if not user.ntnu_username in group.usernames:
+            group.usernames.append(user.ntnu_username)
+            group.save()
 
 def format_field(input):
     if input:
