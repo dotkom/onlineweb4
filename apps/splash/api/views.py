5d7f352ea4d4df638411dc6c64747b406b3b9a08
sklirg <hlsolbjorg@gmail.com>
2016-08-14 08:43:31 +0200

    Make pagination work better

--- apps/splash/api/views.py
+++ apps/splash/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from apps.splash.api.serializers import SplashEventSerializer
@@ -5,10 +6,14 @@ from apps.splash.filters import SplashEventFilter
 from apps.splash.models import SplashEvent
 
 
+class ThirtyItemsPaginator(PageNumberPagination):
+    page_size = 30
+
+
 class SplashEventViewSet(ReadOnlyModelViewSet):
     queryset = SplashEvent.objects.all()
     serializer_class = SplashEventSerializer
-    page_size = 30
+    pagination_class = ThirtyItemsPaginator
     filter_class = SplashEventFilter
     filter_fields = ('start_time', 'end_time')
     ordering_fields = ('id', 'start_time', 'end_time')
