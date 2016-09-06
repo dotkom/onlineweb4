58fc4724531045e605d6bad7eb8c7b482fd540b6
Iver Egge <iverasp@gmail.com>
2016-09-06 15:41:26 +0200

    dont remember what this is

--- apps/ldap/ldap.py
+++ apps/ldap/ldap.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 
+from logging import getLogger
 from apps.ldap.ldap_models import LdapUser, LdapGroup
 
 MIN_UID = 2000
@@ -15,41 +16,43 @@ GROUP_MAP = {
     'Komiteer': 'komiteer',
     'proKom': 'prokom',
     'triKom': 'trikom',
+    'Redaksjonen': 'offline',
 }
+
+GROUP_PRIORITY = [
+    'offline',
+    'prokom',
+]
+
 
 def upsert_user_ldap(user, pwd):
     """
     Update or Insert a user to LDAP
 
-    :param user:
-    :return:
+    :param user: OnlineUser
+    :param pwd: LDAP password
+    :return: boolean successfull
     """
-
-    print("upserting...")
-    # Check if user exists
-    if exists(user):
-        # User exists, update
-        print("user exists")
-        return update(user, pwd)
-    # New user, insert
-    else:
-        return insert(user, pwd)
-
-
-def exists(user):
-    """
-    Check if user exists in LDAP
-
-    :param username: OnlineUser
-    :param conn: Ldap connection
-    :return: boolean exists
-    """
+    print("upsert in progress...")
     if not user.ntnu_username:
+        print("no ntnu")
         return False
 
-    # Get the user from ldap
-    ldap_user = LdapUser.objects.filter(username=user.ntnu_username).first()
-    return ldap_user is not None
+    try:
+        print("trying hard")
+        ldap_user = LdapUser.objects.filter(username=user.ntnu_username).first()
+        if ldap_user is not None:
+            print("ldap user is not none")
+            getLogger(__name__).debug('User exists, updating...')
+            return update(user, pwd)
+        else:
+            getLogger(__name__).debug('User does not exist, inserting...')
+            print("inserting user")
+            return insert(user, pwd)
+    except Exception as exception:
+        log.warning('Exception %s' % exception)
+        print("ex %s" % exception)
+        return False
 
 
 def update(user, pwd):
@@ -62,23 +65,22 @@ def update(user, pwd):
     """
     try:
         # Update user
-        print("looking for user ...")
         ldap_user = LdapUser.objects.filter(username=user.ntnu_username).first()
-        print("ldap user " + str(ldap_user))
+        getLogger(__name__).debug('Found LDAP user %s' % ldap_user)
         ldap_user.username = user.ntnu_username
         ldap_user.email = user.ntnu_username + "@stud.ntnu.no"
         ldap_user.phone = user.phone_number
         ldap_user.first_name = user.first_name
         ldap_user.last_name = user.last_name
         ldap_user.set_password(pwd)
-        print("saving user")
+        getLogger(__name__).debug('Saving LDAP user %s' % ldap_user.username)
         ldap_user.save()
         update_group_memberships(user)
-        return True;
+        getLogger(__name__).info('User %s successfully updated to LDAP' % ldap_user.username)
+        return True
     except Exception as exception:
-        print(exception)
+        log.warning('Exception ' + str(exception))
         return False
-
 
 
 def insert(user, pwd):
@@ -86,20 +88,47 @@ def insert(user, pwd):
     Insert a new user in LDAP
 
     :param user: OnlineUser
+    :param pwd: LDAP password
     :return: boolean inserted
     """
 
-    # TODO: everything
-    return False
+    try:
+        getLogger(__name__).debug('Inserting LDAP user %s' % user.ntnu_username)
+        print("inserting user")
+        ldap_user = LdapUser()
+        print("loL")
+        ldap_user.first_name = user.first_name
+        ldap_user.last_name = user.last_name
+        ldap_user.full_name = user.get_full_name()
+        ldap_user.email = user.ntnu_username + "@stud.ntnu.no"
+        ldap_user.login_shell = '/bin/bash'
+        ldap_user.username = user.ntnu_username
+        ldap_user.uid = find_next_uid()
+        ldap_user.group = get_primary_group(user)
+        home_directory = generate_home_path(ldap_user.username)
+        getLogger(__name__).debug('Saving LDAP user %s' % ldap_user.username)
+        print("saving user")
+        ldap_user.set_password(pwd)
+        ldap_user.save()
+        update_group_memberships(user)
+        getLogger(__name__).info('User %s successfully inserted to LDAP' % ldap_user.username)
+        return True
+    except Exception as exception:
+        getLogger(__name__).warning('Exception %s' % exception)
+        return False
+
 
 def update_group_memberships(user):
+    getLogger(__name__).debug('Updating group membership of %s' % user)
     ldap_groups = []
     for group in user.groups.all():
-        if group.name in GROUP_MAP: ldap_groups.append(GROUP_MAP[group.name])
+        if group.name in GROUP_MAP:
+            ldap_groups.append(GROUP_MAP[group.name])
 
     # remove user from groups the user is no longer a member of
     for ldap_group in LdapGroup.objects.all():
         if not ldap_group.name in ldap_groups and user.ntnu_username in ldap_group.usernames:
+            getLogger(__name__).debug('Removing %s from group %s ' % user, ldap_group.name)
             ldap_group.usernames.remove(user.ntnu_username)
             ldap_group.save()
 
@@ -107,8 +136,10 @@ def update_group_memberships(user):
     for ldap_group in ldap_groups:
         group = LdapGroup.objects.filter(name=ldap_group).first()
         if not user.ntnu_username in group.usernames:
+            getLogger(__name__).debug('Adding %s to group %s' % user, ldap_group.name)
             group.usernames.append(user.ntnu_username)
             group.save()
+
 
 def format_field(input):
     if input:
@@ -124,5 +155,25 @@ def find_next_uid():
     # In that case: TODO: find unused UID if a user has been deleted
     if LdapUser.objects.exists():
         next_uid = LdapUser.objects.latest('uid').uid + 1
-        if next_uid > MIN_UID: return next_uid
-    return MIN_UID
+        print(str(LdapUser.objects.latest('uid')))
+        if next_uid > MIN_UID:
+            getLogger(__name__).debug('Setting UID to %s' % next_uid)
+            return next_uid
+    getLogger(__name__).debug('Setting UID to %s' % MIN_UID)
+    return str(MIN_UID)
+
+
+def generate_home_path(username):
+    return '/home/' + username[:2] + '/' + username
+
+
+def get_primary_group(user):
+    print("getting pri group")
+    for group in user.groups.all():
+        if group.name in GROUP_MAP:
+            for i in range(0, len(GROUP_PRIORITY)):
+                if GROUP_PRIORITY[i] == GROUP_MAP[group.name]:
+                    return GROUP_MAP[group.name]
+    for group in user.groups.all():
+        if group.name in GROUP_MAP:
+            return GROUP_MAP[group.name]
