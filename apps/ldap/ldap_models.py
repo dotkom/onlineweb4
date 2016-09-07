11bcab718e78d7a826e8ae00258e5bcd5c6bdd7d
OptimusCrime <thomasgautv@hotmail.com>
2016-09-07 21:13:43 +0200

    Fix sort

--- apps/ldap/ldap_models.py
+++ apps/ldap/ldap_models.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from django.conf import settings
-
 import ldapdb.models
+from django.conf import settings
 from ldapdb.models.fields import CharField, ImageField, IntegerField, ListField
 from passlib.hash import ldap_sha512_crypt
 
