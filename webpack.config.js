1ce9cdebfda58b205d58ea106d7372db86c1c3ff
Christian Duvholt <cxhristian@gmail.com>
2016-09-28 22:31:03 +0200

    Hello World frontpage events react

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    frontpageEvents: './assets/js/frontpage/events/index'
+  },
+  output: {
+    path: path.resolve('./assets/webpack_bundles/'),
+    filename: '[name]-[hash].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style', 'css!less'),
+      }
+    ]
+  },
+  lessLoader: {
+    includePath: [path.resolve(__dirname, './styles')]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new ExtractTextPlugin('styles.css'),
+    new BundleTracker({filename: './webpack-stats.json'})
+  ]
+};
