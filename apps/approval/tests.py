83494b039127cad5fd614b7a844d19a41c693909
Dora Oline <doraoline@gmail.com>
2016-09-28 22:07:22 +0200

    Added test, not working

--- apps/approval/tests.py
+++ apps/approval/tests.py
@@ -63,3 +63,18 @@ class ApprovalTest(TestCase):
         self.assertEqual(self.approval.is_fos_application(), False)
         self.approval.new_expiry_date = None
         self.assertEqual(self.approval.is_fos_application(), False)
+
+class EmailTest(TestCase):
+    def test_send_email(self):
+        # Send message.
+        mail.send_mail(
+            'Subject here', 'Here is the message.',
+            'from@example.com', ['to@example.com'],
+            fail_silently=False,
+        )
+
+        # Test that one message has been sent.
+        self.assertEqual(len(mail.outbox), 1)
+
+        # Verify that the subject of the first message is correct.
+        self.assertEqual(mail.outbox[0].subject, 'Subject here')
