00935ea386ec39bcdd6a796fd5468956483d883e
Christian Duvholt <cxhristian@gmail.com>
2016-10-05 20:35:43 +0200

    React event image support

--- assets/js/frontpage/events/components/EventImage.js
+++ assets/js/frontpage/events/components/EventImage.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const EventImage = ({ event_start, images }) => (
+const EventImage = ({ event_start, id, images, slug }) => (
   <div className="col-sm-4 col-md-2" id="eventimage">
     <div id="event-carousel" className="carousel slide">
       <div className="carousel-inner">
-        <div className="item active">
-          <img />
-        </div>
+          { images.length > 0
+            ? (
+              <div className="item active">
+                <a href={ `events/${id}/${slug}` }>
+                  <img src={ `https://online.ntnu.no/${images[0]}` } width="100%" alt=""/>
+                </a>
+              </div>
+            )
+            : null
+          }
       </div>
     </div>
     <span className="hero-date">{ moment(event_start).lang('nb').format('DD. MMMM') }</span>
