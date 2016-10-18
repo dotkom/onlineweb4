73907ad57fc81e43c96bbec2ff4c014cdc398fff
Christian Duvholt <cxhristian@gmail.com>
2016-10-18 16:56:10 +0200

    Jesus. Duplicate code

--- apps/api/rfid/events.py
+++ apps/api/rfid/events.py
@@ -96,7 +96,7 @@ class EventResource(ModelResource):
         bundle.data['slug'] = slugify(unidecode(bundle.data['title']))
 
         # If image is set
-        if bundle.data['image']:
+        if 'image' in bundle.data and bundle.data['image']:
             # Parse to FileObject used by Filebrowser
             temp_image = FileObject(bundle.data['image'])
 
