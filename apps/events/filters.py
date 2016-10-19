42b7c63f0e4c3e42d202b84852bb7e7f171363d9
Sardred <andelar96@gmail.com>
2016-10-19 20:58:33 +0200

    Added filter for attendance event

--- apps/events/filters.py
+++ apps/events/filters.py
@@ -8,7 +8,8 @@ class EventDateFilter(django_filters.FilterSet):
     event_start__lte = django_filters.DateTimeFilter(name='event_start', lookup_type='lte')
     event_end__gte = django_filters.DateTimeFilter(name='event_end', lookup_type='gte')
     event_end__lte = django_filters.DateTimeFilter(name='event_end', lookup_type='lte')
+    attendance_event__isnull = django_filters.BooleanFilter(name='attendance_event', lookup_type='isnull')
 
     class Meta:
         model = Event
-        fields = ('event_start', 'event_end',)
+        fields = ('event_start', 'event_end')
