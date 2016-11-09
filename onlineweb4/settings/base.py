4356fb133598db43cb11a8d0191f3ca0dadce7ea
Christian Duvholt <cxhristian@gmail.com>
2016-11-09 23:50:37 +0100

    Only add dev bundle if in debug mode

--- onlineweb4/settings/base.py
+++ onlineweb4/settings/base.py
@@ -16,6 +16,10 @@ NOSE_ARGS = ['--with-coverage', '--cover-package=apps', '--cover-html-dir=covera
 
 DEBUG = False
 
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
+
 ADMINS = (
     ('dotKom', 'dotkom@online.ntnu.no'),
 )
