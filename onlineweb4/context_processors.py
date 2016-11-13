6877ef96046f4cf91851b597aef22100a69270c3
Christian Duvholt <cxhristian@gmail.com>
2016-11-13 23:53:19 +0100

    Tabs to spaces

--- onlineweb4/context_processors.py
+++ onlineweb4/context_processors.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 
 def analytics(request):
-	context_extras = {}
-	if hasattr(settings, 'GOOGLE_ANALYTICS_KEY'):
-		context_extras['GOOGLE_ANALYTICS_KEY'] = settings.GOOGLE_ANALYTICS_KEY
-	return context_extras
+    context_extras = {}
+    if hasattr(settings, 'GOOGLE_ANALYTICS_KEY'):
+        context_extras['GOOGLE_ANALYTICS_KEY'] = settings.GOOGLE_ANALYTICS_KEY
+    return context_extras
