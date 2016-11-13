9d382bb032ad12b7137a7f627b5e4235c6f4e8a2
Christian Duvholt <cxhristian@gmail.com>
2016-11-13 20:10:02 +0100

    Remove unused proptype files

--- assets/js/frontpage/events/proptypes/CompanyPropTypes.js
+++
@@ -1,8 +0,0 @@
-import { PropTypes } from 'react';
-import ImagePropTypes from './ImagePropTypes';
-
-export default {
-  name: PropTypes.string,
-  site: PropTypes.string,
-  image: PropTypes.shape(ImagePropTypes),
-};
