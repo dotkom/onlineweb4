8695016ca79c7050f597081ffc41861786c5bab6
Torjus Iveland <torjusti@gmail.com>
2016-11-14 13:07:16 +0100

    Simplify Tag component

--- assets/js/careeropportunity/components/Tag.js
+++ assets/js/careeropportunity/components/Tag.js
@@ -1,25 +1,11 @@
 import classNames from 'classnames';
 
-class Tag extends React.Component {
-  constructor() {
-    super();
+const Tag = ({ title, selected, handleChange }) => {
+  let classes = classNames({
+    'selected': selected,
+  });
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(key) {
-    this.props.handleChange(key);
-  }
-
-  render() {
-    let classes = classNames({
-      'selected': this.props.selected
-    });
-
-    return (
-      <li className={classes} onClick={this.handleClick.bind(this, this.props.title)}>{this.props.title}</li>
-    );
-  }
-}
+  return <li className={classes} onClick={handleChange.bind(this, title)}>{title}</li>;
+};
 
 export default Tag;
