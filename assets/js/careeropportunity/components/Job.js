35c9af1dd132a6771c5cc14f44c70ad736ea8863
Torjus Iveland <torjusti@gmail.com>
2016-11-14 08:31:23 +0100

    Map results from server to camelCase

--- assets/js/careeropportunity/components/Job.js
+++ assets/js/careeropportunity/components/Job.js
@@ -2,59 +2,47 @@ import moment from 'moment';
 
 moment.locale('nb');
 
-class Job extends React.Component {
-  constructor() {
-    super();
+const Job = ({ locations, deadline, companyImage, companyName, jobTitle, ingress, jobName }) => {
+  if (locations.length >= 2) {
+    locations = `${locations.slice(0, -1).join(', ')} and ${locations[locations.length - 1]}`;
+  } else if (locations.length === 0) {
+    locations = 'Ikke spesifisert';
   }
 
-  render() {
-    let data = this.props.jobData;
+  return (
+    <article className="row">
+      <div className="col-xs-12 col-md-4">
+        <a href="/careeropportunity/4/">
+          <picture>
+            <source srcset={companyImage.lg} media="(max-width: 992px)" />
+            <img src={companyImage.md} alt="Firmalogo" />
+          </picture>
+        </a>
+      </div>
 
-    let locations = data.location.map((location) => location.name).reverse();
+      <div className="col-xs-12 col-md-8">
+        <h1>
+          <a href="/careeropportunity/4/">{companyName} - {jobTitle}</a>
+        </h1>
 
-    if (locations.length >= 2) {
-      locations = `${locations.slice(0, -1).join(', ')} and ${locations[locations.length - 1]}`;
-    } else if (locations.length === 0) {
-      locations = 'Ikke spesifisert';
-    }
+        <div className="ingress">{ingress}</div>
 
-    let deadline = data.deadline ? moment(data.deadline).format('Do MMMM YYYY, HH:mm') : 'Ikke spesifisert';
+        <div className="meta">
+          <div className="col-md-4">
+            <p>Type: {jobName}</p>
+          </div>
 
-    return (
-      <article className="row">
-        <div className="col-xs-12 col-md-4">
-          <a href="/careeropportunity/4/">
-            <picture>
-              <source srcset={data.company.image.lg} media="(max-width: 992px)" />
-              <img src={data.company.image.md} alt="Firmalogo" />
-            </picture>
-          </a>
-        </div>
+          <div className="col-md-4">
+            <p>Sted: {locations}</p>
+          </div>
 
-        <div className="col-xs-12 col-md-8">
-          <h1>
-            <a href="/careeropportunity/4/">{data.company.name} - {data.title}</a>
-          </h1>
-
-          <div className="ingress">{data.ingress}</div>
-
-          <div className="meta">
-            <div className="col-md-4">
-              <p>Type: {data.employment.name}</p>
-            </div>
-
-            <div className="col-md-4">
-              <p>Sted: {locations}</p>
-            </div>
-
-            <div className="col-md-4">
-              <p>Frist: {deadline}</p>
-            </div>
+          <div className="col-md-4">
+            <p>Frist: {deadline}</p>
           </div>
         </div>
-      </article>
-    );
-  }
-}
+      </div>
+    </article>
+  );
+};
 
 export default Job;
