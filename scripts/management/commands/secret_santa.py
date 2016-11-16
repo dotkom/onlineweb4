e9fe68d3b6a5a26e949ae70ea1ffea48938ac9bc
MrHanSan <hanssandbu@gmail.com>
2016-11-16 22:05:23 +0100

    Added text files for mail, added comments, fixed imports, cleaned up the code

--- scripts/management/commands/secret_santa.py
+++ scripts/management/commands/secret_santa.py
@@ -4,8 +4,11 @@ import random
 
 from django.core.management.base import BaseCommand
 from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
 
 from apps.events.models import AttendanceEvent
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -18,23 +21,27 @@ class Command(BaseCommand):
         users = event.attendees_qs
         random_users = Command.generate_random_list(users)
         to_from_dict = {}
-        #to_from_dict_user = {}
         for count, user in enumerate(users):
-            to_from_dict['Par %s' % (count+1)] = {'To': user.user.first_name + ', ' + user.user.last_name, 
-                'from': users[random_users[count]].user.first_name + ', ' + users[random_users[count]].user.last_name}
+            to_from_dict['Par %s' % (count+1)] = {'To': user.user.get_full_name(), 'From': users[random_users[count]].user.get_full_name()}
             
             Command.send_mail(user, users[random_users[count]])
 
         Command.mail_to_committee(to_from_dict)
 
 
+    # Function for generating a list with random numbers corresponding to
+    # the indexes of the list of users attending the event.
     def generate_random_list(users):
-        to_from = random.sample(xrange(0, len(users)), len(users))
+        to_from = random.sample(range(0, len(users)), len(users))
         while(not Command.check_random_list(to_from)):
-            to_from = random.sample(xrange(0, len(users)), len(users))
+            to_from = random.sample(range(0, len(users)), len(users))
         return to_from
 
 
+    # Function for checking that the random list of indexes is valid.
+    # Checks the index of the original list of users against the randomly
+    # generated number. If index and random number is the same, then someone
+    # got themselves as secret Santa.
     def check_random_list(random):
         for count, user in enumerate(random):
             if (count == random[count]):
@@ -43,26 +50,23 @@ class Command(BaseCommand):
 
 
     def send_mail(user_to, user_from):
-        committee_mail = "trikom@online.ntnu.no"
         subject = "Secret santa"
 
-        message = "Hei."
-        message += "\nDu har blitt tildelt " + user_to.user.first_name + ' ' + user_to.user.last_name + " som du skal være secret santa for"
-        message += "\n\nDersom du har spørsmål kan du sende mail til " + committee_mail
-        message += "\n\nMvh\nLinjeforeningen Online"
+        content = render_to_string('secret_santa/attendee_mail.txt', {
+            'user_to': user_to.user.get_full_name(),
+            'mail_committee': settings.EMAIL_TRIKOM,
+        })
 
         receiver = user_from.user.email
 
-        EmailMessage(subject, str(message), committee_mail, [receiver]).send()
+        EmailMessage(subject, content, settings.EMAIL_TRIKOM, [receiver]).send()
 
 
     def mail_to_committee(to_from_dict):
-        committee_mail = "trikom@online.ntnu.no"
         subject = "Secret santa til-fra liste"
 
-        message = "Secret santa til og fra liste:"
+        content = render_to_string('secret_santa/committee_mail.txt', {
+            'to_from_dict': to_from_dict,
+        })
 
-        for key, value in to_from_dict.items():
-            message += '\n' + str(key) + str(value)
-
-        EmailMessage(subject, str(message), committee_mail, [committee_mail]).send()
+        EmailMessage(subject, content, settings.EMAIL_TRIKOM, [settings.EMAIL_TRIKOM]).send()
