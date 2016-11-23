41939834b22059429b0138681709323ac81b0557
Sardred <andelar96@gmail.com>
2016-11-23 20:02:15 +0100

    Changed a status code for Regme attend API

--- apps/events/views.py
+++ apps/events/views.py
@@ -365,7 +365,7 @@ class AttendViewSet(views.APIView):
                 return Response({'message': (attendee.user.get_full_name() +
                                              ' er på venteliste. Registrer dem som møtt opp allikevel?'),
                                  'attend_status': 30},
-                                status=status.HTTP_200_OK)
+                                status=status.HTTP_403_FORBIDDEN)
             attendee.attended = True
             attendee.save()
         except Attendee.DoesNotExist:
