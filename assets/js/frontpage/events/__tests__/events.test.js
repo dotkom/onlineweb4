79258662e71f6e616edc97a7bc1cbade274e3b3d
nornes <andrehn@stud.ntnu.no>
2016-11-24 08:21:42 +0100

    start writing tests

---
+++ assets/js/frontpage/events/__tests__/events.test.js
@@ -0,0 +1,14 @@
+import { mount } from 'enzyme';
+import SmallEvent from '../components/SmallEvent';
+
+
+describe('<SmallEvent />', () => {
+  const testUrl = 'thisIsUrl';
+  const testDate = '2013-02-08';
+  const wrapper = mount(<SmallEvent eventUrl={testUrl} startDate={testDate} title="event" />);
+
+  it('renders a small event with a href to the the correct url', () => {
+    expect(wrapper.find({ href: testUrl })).toBeDefined();
+  });
+});
+
