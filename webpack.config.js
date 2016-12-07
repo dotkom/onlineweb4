38f437d96750eba9fee27ec2a11eacbdd3f1ecef
Christian Duvholt <cxhristian@gmail.com>
2016-12-07 15:27:01 +0100

    Use url-loader instead of file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
-        loader: 'file-loader',
+        loader: 'url-loader?limit=10000',
       },
     ],
   },
