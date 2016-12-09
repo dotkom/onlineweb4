97a29e4a9b5ea3009a8278b9c577f85746605091
Christian Duvholt <cxhristian@gmail.com>
2016-12-09 13:57:36 +0100

    eslint imported js

--- assets/js/events/archive/filters.js
+++ assets/js/events/archive/filters.js
@@ -1,62 +1,62 @@
-var Filters = function () {
-
+const Filters = function Filters() {
   this.query = '';
   this.future = true;
   this.myevents = false;
 };
 
-Filters.prototype.search = function () {
-    var url = Urls.search_events() + '?query=';
-    var query = this.query;
-    var filters = '';
-    filters   += '&future=' + this.future;
-    filters     += '&myevents=' + this.myevents;
+Filters.prototype.search = function search() {
+  const url = `${Urls.search_events()}?query=`;
+  const query = this.query;
+  let filters = '';
+  filters += `&future=${this.future}`;
+  filters += `&myevents=${this.myevents}`;
 
-    var self = this;
-    $.get(url + query + filters, function (result) {
-      $('#events').empty();
-      $('#events').append(result);
-      if(!self.future) {
-        // Calculate menu offset
-        var offset = $('nav.subnavbar').position().top+$('nav.subnavbar').outerHeight(true);
-        // Scroll to closest event
-        var articles = $('#events article');
-        var today = new Date();
-        articles.each(function(i, event) {
-          var event_date = new Date($(event).data('date'));
-          if(event_date < today) {
-            // Scroll animation
-            $('html, body').animate({scrollTop: $(event).offset().top - offset}, 250);
-            return false;
-          }
-        });
-      }
-    });
-  };
+  const self = this;
+  $.get(url + query + filters, (result) => {
+    $('#events').empty();
+    $('#events').append(result);
+    if (!self.future) {
+      // Calculate menu offset
+      const offset = $('nav.subnavbar').position().top + $('nav.subnavbar').outerHeight(true);
+      // Scroll to closest event
+      const articles = $('#events article');
+      const today = new Date();
+      articles.each((i, event) => {
+        const eventDate = new Date($(event).data('date'));
+        if (eventDate < today) {
+          // Scroll animation
+          $('html, body').animate({ scrollTop: $(event).offset().top - offset }, 250);
+          return false;
+        }
+        return true;
+      });
+    }
+  });
+};
 
-$(function () {
-
-  var filters = new Filters();
+$(() => {
+  const filters = new Filters();
   filters.search();
 
 
-  $('#search').keyup(function () {
+  $('#search').keyup(function search() {
     filters.query = $(this).val();
     filters.search();
   });
 
-  // This is a slight hack to avoid a difficult bug in jQuery where some browsers tend to preserve checkbox states
+  // This is a slight hack to avoid a difficult bug in jQuery
+  // where some browsers tend to preserve checkbox states
   // without them being domready.
-  $(document).ready(function () {
+  $(document).ready(() => {
     $('#future').prop('checked', true);
   });
 
-  $('#future').on('click', function () {
+  $('#future').on('click', () => {
     filters.future = !filters.future;
     filters.search();
   });
 
-  $('#myevents').on('click', function () {
+  $('#myevents').on('click', () => {
     filters.myevents = !filters.myevents;
     filters.search();
   });
