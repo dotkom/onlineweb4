425c5958bb3534a7b67cbbfd6d551e3122ea5ae8
Christian Duvholt <cxhristian@gmail.com>
2016-12-23 23:55:55 +0100

    Move rest of less files to core entry

--- assets/js/core/index.js
+++ assets/js/core/index.js
@@ -1,8 +1,8 @@
-import 'less/core.less';
 import 'js/common/less/gallery.less';
 import 'es6-promise/auto';
 import 'whatwg-fetch';
 import moment from 'moment';
+import './less/core.less';
 
 
 moment.locale('nb');
