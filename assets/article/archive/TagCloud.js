f017a72fd26f929b486e7975e8afe086f66ec73c
Christian Duvholt <cxhristian@gmail.com>
2016-12-24 02:39:03 +0100

    eslint article archive and cleanup a bit

--- assets/article/archive/TagCloud.js
+++ assets/article/archive/TagCloud.js
@@ -1,20 +1,18 @@
+import $ from 'jquery';
 
-$(document).ready(function () {
-    $('div.tagcloud span').each(function () {
-        var max = $('div.tagcloud').data('max_frequency');
-        console.log(max);
-        frequency = $(this).data('frequency');
-        rel = ((frequency / (max/3)) + 1);
-        $(this).css("font-size", rel + 'em');
-        if(rel < 2) {
-            rel = 400;
-        } else if(rel < 2.5) {
-            rel = 600;
-        } else if(rel < 3.5) {
-            rel = 700;
-        } else {
-            rel = 800;
-        }
-        $(this).css("font-weight", rel);
-    });
+$('div.tagcloud span').each(function tagCloud() {
+  const max = $('div.tagcloud').data('max_frequency');
+  const frequency = $(this).data('frequency');
+  let rel = ((frequency / (max / 3)) + 1);
+  $(this).css('font-size', `${rel}em`);
+  if (rel < 2) {
+    rel = 400;
+  } else if (rel < 2.5) {
+    rel = 600;
+  } else if (rel < 3.5) {
+    rel = 700;
+  } else {
+    rel = 800;
+  }
+  $(this).css('font-weight', rel);
 });
