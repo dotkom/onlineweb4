bc5f30850131966db6cf5c2e570fdc67eee36c89
Christian Duvholt <cxhristian@gmail.com>
2016-12-27 17:59:34 +0100

    eslint article details

--- assets/article/details/articleRelated.js
+++ assets/article/details/articleRelated.js
@@ -1,20 +1,32 @@
+import $ from 'jquery';
+
+const addRelatedArticles = (data) => {
+  let output = '';
+  const articles = data.results;
+  // Set length of loop to 6 if num articles is more than 6.
+  const len = (articles.length > 6) ? 6 : articles.length;
+
+  for (let i = 0; i < len; i += 1) {
+    output += `
+      <div class="row-fluid">
+        <div class="span12">
+          <a href="/article/${articles[i].id}">
+            <img src="${articles[i].image.thumb}" alt="${articles[i].heading}">
+          </a>
+          <br />
+          <h4>${articles[i].heading}</h4>
+        </div>
+      </div>`;
+  }
+
+  $('#article-detaill-latest').html(output);
+};
+
 export default () => {
-    $.ajax({
-        'url': '/api/v1/articles/?format=json&featured=False',
-        'method': 'GET',
-        'data': {},
-        success: function(data) {
-            var output = '';
-            var articles = data.results;
-            var len = (articles.length > 6) ? 6 : articles.length; // Set length of loop to 6 if num articles is more than 6.
-
-            // The loop
-            for (var i = 0; i < len; i++) {
-                output += '<div class="row-fluid"><div class="span12"><a href="/article/'+articles[i].id+'"><img src="'+articles[i].image.thumb+'" alt="'+articles[i].heading+'"></a><br /><h4>'+articles[i].heading+'</h4></div></div>';
-            }
-
-            // Appending
-            $('#article-detaill-latest').html(output);
-        }
-    });
+  $.ajax({
+    url: '/api/v1/articles/?format=json&featured=False',
+    method: 'GET',
+    data: {},
+    addRelatedArticles,
+  });
 };
