08f605f03c1cafb927ba2dcbbba77a0fcbea7d10
Torjus Iveland <torjusti@gmail.com>
2017-03-25 16:09:21 +0100

    Remove unused js folder

--- assets/js/careeropportunity/propTypes/companyImage.js
+++
@@ -1,7 +0,0 @@
-import { PropTypes } from 'react';
-
-export default PropTypes.shape({
-  id: PropTypes.number,
-  md: PropTypes.string,
-  lg: PropTypes.string,
-});
--- assets/js/careeropportunity/propTypes/tags.js
+++
@@ -1,8 +0,0 @@
-import { PropTypes } from 'react';
-
-export default PropTypes.objectOf(PropTypes.shape({
-  id: PropTypes.number,
-  name: PropTypes.string,
-  display: PropTypes.bool,
-  deadline: PropTypes.number,
-}));
