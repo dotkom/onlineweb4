05b272cebcbdc8e86328d8fc4c0f54a0d1c206c8
martinlunde <martlun@stud.ntnu.no>
2017-04-20 13:06:43 +0200

    added models instead of hardcoded html. Hobbygroups can now be added through the admin panel

---
+++ apps/hobbygroups/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from apps.hobbygroups.models import Hobby
+
+
+class HobbygroupAdmin(admin.ModelAdmin):
+    model = Hobby
+
+admin.site.register(Hobby)
--- apps/hobbygroups/views.py
+++ apps/hobbygroups/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from apps.hobbygroups.models import Hobby
 
 
 # Index page
 def index(request):
-    return render_to_response('hobbygroups/index.html', context_instance=RequestContext(request))
+    hobbygroups = Hobby.objects.all()
+    context = {
+        'hobbygroups': hobbygroups,
+    }
+    return render_to_response('hobbygroups/index.html', context, context_instance=RequestContext(request))
