349d8cdedbd10a1e009347197f323f05cf504ceb
Aslak Hollund <aslakhol@gmail.com>
2017-05-03 23:11:46 +0200

    added labels to form

--- apps/contact/forms.py
+++ apps/contact/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    contact_name = forms.CharField(required=True)
-    contact_email = forms.EmailField(required=True)
-    content = forms.CharField(required=True, widget=forms.Textarea)
+    contact_name = forms.CharField(required=True, label="Ditt navn:")
+    contact_email = forms.EmailField(required=True, label="Din epostadresse:")
+    content = forms.CharField(required=True, widget=forms.Textarea, label="Din melding:")
