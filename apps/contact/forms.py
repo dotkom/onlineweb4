4b8f1d82f8b69ebb0edcfd73713e9b54ac9d1758
martinlunde <martlun@stud.ntnu.no>
2017-05-03 23:42:23 +0200

    changed description and added some css styling

--- apps/contact/forms.py
+++ apps/contact/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
     komiteer = ((1, "Hovedstyret"), (2, "Drifts- og utviklingskomiteen"))
-    contact_name = forms.CharField(required=True, label="Ditt navn:", widget=forms.TextInput({"placeholder":"Navn"}))
-    contact_email = forms.EmailField(required=True, label="Din epostadresse:", widget=forms.TextInput({"placeholder":"Epostadresse"}))
+    contact_name = forms.CharField(required=True, label="Ditt navn:", widget=forms.TextInput({"placeholder": "Navn"}))
+    contact_email = forms.EmailField(required=True, label="Din epostadresse:", widget=forms.TextInput({"placeholder": "Epostadresse"}))
     contact_receiver = forms.ChoiceField(required=True, label="Hvem ønsker du å kontakte?", choices=komiteer)
-    content = forms.CharField(required=True, widget=forms.Textarea({"placeholder":"Din melding"}), label="Melding:")
+    content = forms.CharField(required=True, widget=forms.Textarea({"placeholder": "Din melding"}), label="Melding:")
