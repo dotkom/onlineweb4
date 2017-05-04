119201d77106ebd5570d198c7740f102de723106
Aslak Hollund <aslakhol@gmail.com>
2017-05-04 03:23:17 +0200

    setup captcha

--- apps/contact/forms.py
+++ apps/contact/forms.py
@@ -1,3 +1,4 @@
+from captcha.fields import ReCaptchaField
 from django import forms
 
 
@@ -7,3 +8,5 @@ class ContactForm(forms.Form):
     contact_email = forms.EmailField(required=True, widget=forms.TextInput({"placeholder": "Epostadresse"}))
     contact_receiver = forms.ChoiceField(required=True, label="Hvem ønsker du å kontakte?", choices=komiteer)
     content = forms.CharField(required=True, widget=forms.Textarea({"placeholder": "Din melding"}))
+    captcha = ReCaptchaField(error_messages={'required': ('Du klarte ikke captchaen! Er du en bot?'),
+                                             'invalid': ('Du klarte ikke captchaen! Er du en bot?')})
