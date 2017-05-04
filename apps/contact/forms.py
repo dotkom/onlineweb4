7e08212db11fce400a8244f2b186d72558faaa6e
Aslak Hollund <aslakhol@gmail.com>
2017-05-04 20:06:13 +0200

    Unrequired email and name in contact form and updated text to reflect this.

--- apps/contact/forms.py
+++ apps/contact/forms.py
@@ -6,8 +6,8 @@ class ContactForm(forms.Form):
     mail_choices = (("hovedstyret@online.ntnu.no", "Hovedstyret"),
                     ("dotkom@online.ntnu.no", "Drifts- og utviklingskomiteen")
                     )
-    contact_name = forms.CharField(required=True, widget=forms.TextInput({"placeholder": "Navn"}))
-    contact_email = forms.EmailField(required=True, widget=forms.TextInput({"placeholder": "Epostadresse"}))
+    contact_name = forms.CharField(widget=forms.TextInput({"placeholder": "Navn"}))
+    contact_email = forms.EmailField(widget=forms.TextInput({"placeholder": "Epostadresse"}))
     contact_receiver = forms.ChoiceField(required=True, label="Hvem ønsker du å kontakte?", choices=mail_choices)
     content = forms.CharField(required=True, widget=forms.Textarea({"placeholder": "Din melding"}))
     captcha = ReCaptchaField(error_messages={'required': ('Du klarte ikke captchaen! Er du en bot?'),
