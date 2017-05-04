7068e2c3555da3cf1d37bcc1963958a2e6bac344
Aslak Hollund <aslakhol@gmail.com>
2017-05-04 21:56:21 +0200

    Now looping over form elements

--- apps/contact/forms.py
+++ apps/contact/forms.py
@@ -6,9 +6,9 @@ class ContactForm(forms.Form):
     mail_choices = (("hovedstyret@online.ntnu.no", "Hovedstyret"),
                     ("dotkom@online.ntnu.no", "Drifts- og utviklingskomiteen")
                     )
+    contact_receiver = forms.ChoiceField(required=True, label="Hvem ønsker du å kontakte?", choices=mail_choices)
     contact_name = forms.CharField(widget=forms.TextInput({"placeholder": "Navn"}))
     contact_email = forms.EmailField(widget=forms.TextInput({"placeholder": "Epostadresse"}))
-    contact_receiver = forms.ChoiceField(required=True, label="Hvem ønsker du å kontakte?", choices=mail_choices)
     content = forms.CharField(required=True, widget=forms.Textarea({"placeholder": "Din melding"}))
     captcha = ReCaptchaField(error_messages={'required': ('Du klarte ikke captchaen! Er du en bot?'),
                                              'invalid': ('Du klarte ikke captchaen! Er du en bot?')})
