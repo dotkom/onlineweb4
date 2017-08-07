0533ba321072931b586a57b6f95ce11ee9ceed33
martinlunde <martlun@stud.ntnu.no>
2017-08-07 09:38:26 +0200

    Made it possible to arrange hobbygroups by priority

--- apps/hobbygroups/models.py
+++ apps/hobbygroups/models.py
@@ -8,6 +8,7 @@ class Hobby(models.Model):
     description = models.TextField(max_length=250)
     image = models.ImageField(upload_to=settings.IMAGES_PATH)
     read_more_link = models.URLField(blank=True)
+    hobby_priority = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.title
+        return (self.title + " [Priority: " + str(self.hobby_priority) + "]")
