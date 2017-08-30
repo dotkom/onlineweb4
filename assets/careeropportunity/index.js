b84e9cb6b23153637ed82bfd3f9309ebd8e8982f
Westermoen <jakob@westermoen.com>
2017-08-30 22:44:53 +0200

    temporary

--- assets/careeropportunity/index.js
+++ assets/careeropportunity/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import moment from 'moment';
 import FilterableJobList from './containers/FilterableJobList';
+//import DetailView from './containers/DetailView';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 moment.locale('nb');
+
+const routes = (
+  <Route history={browserHistory}>
+    <Route path='/careeropportunity' component={FilterableJobList} />
+    <Route path='/careeropportunity/*' component={FilterableJobList} />
+  </Route>
+);
 
 ReactDom.render(
   <FilterableJobList />,
