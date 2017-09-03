1d626d913cc3d37380dcb2e86d0e58caf5477e96
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-09-03 03:34:29 +0200

    Fix a runserver crash

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+import threading; threading.stack_size(4*80*1024)
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onlineweb4.settings")
 
