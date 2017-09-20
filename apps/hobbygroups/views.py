65f60f69a30f431f83847e8d4fae0f00f4d21d1c
martinlunde <martlun@stud.ntnu.no>
2017-09-20 19:56:30 +0200

    Added api v1 for hobbygroups

--- apps/hobbygroups/views.py
+++ apps/hobbygroups/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from apps.hobbygroups.models import Hobby
 
+from rest_framework import viewsets
+from apps.hobbygroups.serializers import HobbySerializer
+
 
 # Index page
 def index(request):
@@ -10,3 +13,8 @@ def index(request):
         'hobbygroups': hobbygroups,
     }
     return render(request, 'hobbygroups/index.html', context)
+
+
+class HobbyViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Hobby.objects.all()
+    serializer_class = HobbySerializer
