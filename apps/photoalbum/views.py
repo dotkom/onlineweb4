dd81321ed42b6da5fe7662e2cbdff6b832c2f36f
Dora Oline Eriksrud <doraoline@gmail.com>
2017-09-20 21:06:50 +0200

    Album is not saved when clicked the button

--- apps/photoalbum/views.py
+++ apps/photoalbum/views.py
@@ -21,12 +21,16 @@ def create_album(request):
     form = AlbumForm(request.POST)
     print("In create")
 
-    if request.method == "post":
+    print(request.method)
+
+    if request.method == "POST":
         print("Method is post")
         form = AlbumForm(request.POST, request.FILES)
+        print("Request if method is post: ", request)
         if form.is_valid():
             print("Album form is valid")
+            form.save()
 
-            return render(request, 'photoalbum/index.html', {'form': form})
+            return render(request, 'photoalbum/index.html')
 
     return render(request, 'photoalbum/create.html', {'form': form})
