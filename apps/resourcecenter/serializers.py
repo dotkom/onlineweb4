9f92caa7075e89f7dc6c574868ee4f166272f8d0
martinlunde <martlun@stud.ntnu.no>
2017-09-20 22:08:14 +0200

    Added resourcecenter to api

---
+++ apps/resourcecenter/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from apps.resourcecenter.models import Resource
+
+
+class ResourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Resource
+        fields = ('id', 'title', 'description')
--- apps/resourcecenter/views.py
+++ apps/resourcecenter/views.py
@@ -3,6 +3,8 @@
 from django.shortcuts import render
 from apps.resourcecenter.models import Resource
 
+from apps.resourcecenter.serializers import ResourceSerializer
+from rest_framework import viewsets
 
 # Index page
 def index(request):
@@ -11,3 +13,8 @@ def index(request):
         'resources': resources,
     }
     return render(request, 'resourcecenter/index.html', context)
+
+
+class HobbyViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
