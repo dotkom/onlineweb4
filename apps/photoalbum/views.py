626b8c6d93f4d0282d0ed33a5298186816207480
Dora Oline Eriksrud <doraoline@gmail.com>
2017-09-27 21:29:51 +0200

    Added links to album

--- apps/photoalbum/views.py
+++ apps/photoalbum/views.py
@@ -3,20 +3,21 @@ from django.shortcuts import render
 from apps.photoalbum.models import Album, Photo
 from apps.photoalbum.forms import AlbumForm
 
+from django.shortcuts import render, HttpResponseRedirect
 
-def index(request):
-
-    albums = Album.objects.all()
-    print("Albums: ", albums)
-
-    return render(request, 'photoalbum/index.html', {'albums': albums})
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.utils.decorators import method_decorator
 
 
-def album(request):
+class AlbumsListView(ListView):
+    model = Album
+    template_name = 'photoalbum/index.html'
 
-    photos = Photo.all()
-
-    return render(request, 'photoalbum/{% album.title %}')
+    def get_context_data(self, **kwargs):
+        context = super(AlbumsListView, self).get_context_data(**kwargs)
+        context['albums'] = Album.objects.all()
+        return context
 
 
 def create_album(request):
@@ -42,3 +43,16 @@ def create_album(request):
             return render(request, 'photoalbum/index.html', {'albums': albums})
 
     return render(request, 'photoalbum/create.html', {'form': form})
+
+
+class AlbumDetailView(DetailView):
+    model = Album
+    template_name = "photoalbum/album.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(AlbumDetailView, self).get_context_data(**kwargs)
+        context['album'] = Album.objects.get(pk=self.kwargs['pk'])
+        return context
+
+
+
