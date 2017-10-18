3ae84c641d12ef3a2353a924993173b901aecda0
Torjus Iveland <torjusti@gmail.com>
2017-10-18 19:29:12 +0200

    Add missing url to urls.py (!)

--- apps/careeropportunity/urls.py
+++ apps/careeropportunity/urls.py
@@ -7,7 +7,7 @@ from apps.careeropportunity import views
 
 urlpatterns = [
     url(r'^$', views.index, name='careeropportunity_index'),
-    url(r'^.*$', views.index, name='careeropportunity_index'),
+    url(r'^(?P<opportunity_id>\d+)/$', views.details, name='careeropportunity_details'),
 ]
 
 # API v1
