e21a322aefa3d8bc91ef00212f24bfe97ee53449
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-12-02 17:20:24 +0100

    Remove failing raven default value

--- onlineweb4/settings/raven.py
+++ onlineweb4/settings/raven.py
@@ -4,7 +4,7 @@ import raven
 from decouple import config
 
 RAVEN_CONFIG = {
-    'dsn': config('OW4_RAVEN_DSN', default='https://user:pass@sentry.io/project'),
+    'dsn': config('OW4_RAVEN_DSN', default=''),  # Format: https://user:pass@sentry.io/project
     'environment': config('OW4_ENVIRONMENT', default='DEVELOP'),
     # Use git to determine release
     'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
