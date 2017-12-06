777b9434a715acb2b93835e11fb142714363e3ef
Christian Duvholt <christian@duvholt.net>
2017-12-06 20:35:23 +0100

    Add tests related to payment on detail view for events

--- apps/events/tests/detail_view_tests.py
+++ apps/events/tests/detail_view_tests.py
@@ -1,12 +1,14 @@
-from .utils import generate_event, add_to_trikom
-from ..models import GroupRestriction, TYPE_CHOICES
-from apps.authentication.models import OnlineUser
 from django.contrib.auth.models import Group
-from django.test import TestCase
-
 from django.core.urlresolvers import reverse
-from rest_framework import status
+from django.test import TestCase
 from django_dynamic_fixture import G
+from rest_framework import status
+
+from apps.authentication.models import OnlineUser
+
+from ..models import TYPE_CHOICES, GroupRestriction
+from .utils import (add_payment_delay, add_to_trikom, attend_user_to_event, generate_event,
+                    generate_payment, pay_for_event)
 
 
 class EventsURLTestCase(TestCase):
@@ -22,11 +24,11 @@ class EventsURLTestCase(TestCase):
         self.client.force_login(self.user)
 
         self.event = generate_event(TYPE_CHOICES[0][0])
+        self.event_url = reverse(
+            'events_details', args=(self.event.id, self.event.slug))
 
     def test_ok(self):
-        url = reverse('events_details', args=(self.event.id, self.event.slug))
-
-        response = self.client.get(url)
+        response = self.client.get(self.event_url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -42,9 +44,8 @@ class EventsURLTestCase(TestCase):
         add_to_trikom(self.user)
         trikom = Group.objects.get(name__iexact='trikom')
         G(GroupRestriction, event=self.event, groups=[trikom])
-        url = reverse('events_details', args=(self.event.id, self.event.slug))
 
-        response = self.client.get(url)
+        response = self.client.get(self.event_url)
         messages = list(response.context['messages'])
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -54,10 +55,79 @@ class EventsURLTestCase(TestCase):
         add_to_trikom(self.user)
         arrkom = Group.objects.get(name__iexact='arrkom')
         G(GroupRestriction, event=self.event, groups=[arrkom])
-        url = reverse('events_details', args=(self.event.id, self.event.slug))
 
-        response = self.client.get(url)
+        response = self.client.get(self.event_url)
         messages = [str(message) for message in response.context['messages']]
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn("Du har ikke tilgang til dette arrangementet.", messages)
+
+    def test_payment_logged_out(self):
+        payment = generate_payment(self.event)
+
+        self.client.logout()
+        response = self.client.get(self.event_url)
+        context = response.context
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(context['payment'], payment)
+        self.assertEqual(context['user_paid'], False)
+        self.assertEqual(context['payment_delay'], None)
+        self.assertEqual(context['payment_relation_id'], None)
+
+    def test_payment_not_attended(self):
+        payment = generate_payment(self.event)
+
+        response = self.client.get(self.event_url)
+        context = response.context
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(context['payment'], payment)
+        self.assertEqual(context['user_attending'], False)
+        self.assertEqual(context['user_paid'], False)
+        self.assertEqual(context['payment_delay'], None)
+        self.assertEqual(context['payment_relation_id'], None)
+
+    def test_payment_attended(self):
+        payment = generate_payment(self.event)
+        attend_user_to_event(self.event, self.user)
+
+        response = self.client.get(self.event_url)
+        context = response.context
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(context['payment'], payment)
+        self.assertEqual(context['user_attending'], True)
+        self.assertEqual(context['user_paid'], False)
+        self.assertEqual(context['payment_delay'], None)
+        self.assertEqual(context['payment_relation_id'], None)
+
+    def test_payment_paid(self):
+        payment = generate_payment(self.event)
+        attend_user_to_event(self.event, self.user)
+        payment_relation = pay_for_event(self.event, self.user)
+
+        response = self.client.get(self.event_url)
+        context = response.context
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(context['payment'], payment)
+        self.assertEqual(context['user_attending'], True)
+        self.assertEqual(context['user_paid'], True)
+        self.assertEqual(context['payment_delay'], None)
+        self.assertEqual(context['payment_relation_id'], payment_relation.id)
+
+    def test_payment_attended_with_delay(self):
+        payment = generate_payment(self.event)
+        payment_delay = add_payment_delay(payment, self.user)
+        attend_user_to_event(self.event, self.user)
+
+        response = self.client.get(self.event_url)
+        context = response.context
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(context['payment'], payment)
+        self.assertEqual(context['user_attending'], True)
+        self.assertEqual(context['user_paid'], False)
+        self.assertEqual(context['payment_delay'], payment_delay)
+        self.assertEqual(context['payment_relation_id'], None)
