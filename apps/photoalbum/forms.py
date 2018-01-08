657ac66fab534103692cd186f8080ec0b1d27cc1
Dora Oline Eriksrud <doraoline@gmail.com>
2018-01-08 11:11:28 +0100

    Improved form

--- apps/photoalbum/forms.py
+++ apps/photoalbum/forms.py
@@ -2,9 +2,11 @@
 
 from django import forms
 
+from django.utils.translation import ugettext as _
+
 from apps.photoalbum.models import Album
 
 class AlbumForm(forms.Form):
 
-	title = forms.TextInput(attrs={'placeholder': 'Tittel'})
-	photos = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))    
+	title = forms.CharField(widget=forms.TextInput(), label=_("Tittel"))
+	photos = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label=_("Bilder"))    
