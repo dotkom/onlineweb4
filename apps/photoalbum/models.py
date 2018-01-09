bf93073d231c5a779784c9211973d5c31193467e
Dora Oline Eriksrud <doraoline@gmail.com>
2018-01-09 14:39:31 +0100

    Will try with photologue

--- apps/photoalbum/models.py
+++ apps/photoalbum/models.py
@@ -4,13 +4,19 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext as _
 
+from imagekit.models import ProcessedImageField
+
 IMAGE_FOLDER = "images/photo_album"
 
 class Album(models.Model):
-    title = models.CharField(_("tittel"), blank=False, null=False, max_length=50)
+    title = models.CharField(_("Tittel"), blank=False, null=False, max_length=50)
     photos = models.ForeignKey("Photo", null=True, blank=True)
 
+    def __unicode__(self):
+    	return self.title
 
 class Photo(models.Model):
     # Path should depend on album?
-    photo = models.ImageField(upload_to="uploaded_media/images/photo_album/")
+    #photo = models.ImageField(upload_to="uploaded_media/images/photo_album/")
+	photo = ProcessedImageField(upload_to="uploaded_media/images/photo_album/")
+	album = models.ForeignKey("Album")
