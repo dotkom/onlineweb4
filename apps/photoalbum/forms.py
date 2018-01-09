34c8a981c826b97ef94fa5af4d91682ef3f69709
Dora Oline Eriksrud <doraoline@gmail.com>
2018-01-09 14:39:31 +0100

    Will try with photologue

--- apps/photoalbum/forms.py
+++ apps/photoalbum/forms.py
@@ -4,9 +4,28 @@ from django import forms
 
 from django.utils.translation import ugettext as _
 
-from apps.photoalbum.models import Album
+from apps.photoalbum.models import Album, Photo
 
-class AlbumForm(forms.Form):
+class AlbumForm(forms.ModelForm):
 
+	class Meta:
+		model = Album
+		exclude = []
+
+
+	""""
 	title = forms.CharField(widget=forms.TextInput(), label=_("Tittel"))
-	photos = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label=_("Bilder"))    
+	photos = forms.FileField(widget=forms.ClearableFileInput(), label=_("Bilder"))    
+
+	def upload_photos(photos):
+		photos = UploadImageHandler(photos)
+		print("Photos: ", photos)
+
+		for photo in photos:
+			Photo.objects.create(photo)
+			print("Created photo")
+
+	def save(request):
+		photos = upload_photos(request.FILES.getlist('photos'))
+		#Album = Alcum.objects.create()
+	""""
