b4596259357e6d4374764d2d1bc565acc3d545f4
Dora Oline Eriksrud <doraoline@gmail.com>
2018-01-09 20:58:40 +0100

    Getting the correct images when showing album

--- apps/photoalbum/models.py
+++ apps/photoalbum/models.py
@@ -8,7 +8,6 @@ IMAGE_FOLDER = "uploaded_media/images/photo_album"
 
 class Album(models.Model):
     title = models.CharField(_("Tittel"), blank=False, null=False, max_length=50)
-    photos = models.ForeignKey("Photo", null=True, blank=True)
 
     def __unicode__(self):
     	return self.title
@@ -16,4 +15,5 @@ class Album(models.Model):
 class Photo(models.Model):
     # Path should depend on album?
     photo = models.ImageField(upload_to=IMAGE_FOLDER)
+    album = models.ForeignKey(Album)
 	
