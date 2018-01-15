82f6dcf7feadd1f8f629daef39993b3026e74b19
Dora Oline Eriksrud <doraoline@gmail.com>
2018-01-15 14:20:09 +0100

    Can change name and add photos to album

--- apps/photoalbum/urls.py
+++ apps/photoalbum/urls.py
@@ -7,9 +7,9 @@ from apps.photoalbum import views
 urlpatterns = [
     url(r'^$', views.AlbumsListView.as_view(), name='albums_list'),
     url(r'^create$', views.create_album, name="create_album"),
-    url(r'^delete(?P<pk>\d+)$', views.delete_album, name="delete_album"),
+    url(r'^edit/(?P<pk>\d+)/$', views.edit_album, name="edit_album"), 
+    url(r'^delete/(?P<pk>\d+)$', views.delete_album, name="delete_album"),
     url(r'^(?P<pk>\w+)/$', views.AlbumDetailView.as_view(), name="album_detail"),
     # Should have album-name before pk
     url(r'^(?P<album_pk>\w+)/(?P<pk>\w+)/$', views.PhotoDetailView.as_view(), name="photo_detail"),
-
 ]
