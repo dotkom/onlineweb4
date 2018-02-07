0dc73191e699deef42deb960952576388cf95e3e
Dora Oline Eriksrud <doraoline@gmail.com>
2018-02-07 19:50:09 +0100

    Breaking up in smalle tables. Added tags and user tags

--- apps/photoalbum/models.py
+++ apps/photoalbum/models.py
@@ -4,16 +4,48 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext as _
 
+from apps.authentication.models import OnlineUser
+
 IMAGE_FOLDER = "images/photo_album"
 
 class Album(models.Model):
-    title = models.CharField(_("Tittel"), blank=False, null=False, max_length=50)
+	title = models.CharField(_("Tittel"), blank=False, null=False, max_length=50)
+	#photos = models.ManyToManyField("Photo")
+	tags = models.ManyToManyField("AlbumTag")
+	
+	def __str__(self):
+		return self.title
 
-    def __unicode__(self):
-    	return self.title
+	def get_photos(self):
+		return AlbumToPhoto.objects.filter(album=self).values("photo")
+
 
 class Photo(models.Model):
-    # Path should depend on album?
-    photo = models.ImageField(upload_to=IMAGE_FOLDER)
-    album = models.ForeignKey(Album)
-	
+	# Path should depend on album?
+	photo = models.ImageField(upload_to=IMAGE_FOLDER)
+	#album = models.ForeignKey("Album")
+	#tagged_users = models.ManyToManyField(OnlineUser, blank=True)
+
+	def get_album(self):
+		return AlbumToPhoto.objects.get(photo=self).album
+
+	def get_tagged_users(self):
+		return PhotoWithUser.objects.filter(photo=self).values("user")
+
+		
+
+class AlbumTag(models.Model):
+	name = models.CharField(_("Navn"), blank=False, null=False, max_length=20)
+
+	def __str__(self):
+		return self.name
+
+
+class AlbumToPhoto(models.Model):
+	photo = models.ForeignKey(Photo)
+	album = models.ForeignKey(Album)
+
+
+class PhotoWithUser(models.Model):
+	photo = models.ForeignKey(Photo)
+	user = models.ForeignKey(OnlineUser)
--- apps/photoalbum/utils.py
+++ apps/photoalbum/utils.py
@@ -3,30 +3,34 @@
 
 from django.contrib.auth import get_user_model
 
+from apps.photoalbum.models import Photo, AlbumToPhoto
+
 def upload_photos(photos, album):
 	photos_list = []
 
 	for photo in photos:
 		print(photos)
-		p = Photo(photo=photo, album=album)
+		p = Photo(photo=photo)
 		p.save()
+		albumToPhoto = AlbumToPhoto(album=album, photo=photo)
+		albumToPhoto.save()
 		photos_list.append(p)
 
 	return photos_list
 
 
 def report_photo(description, photo, user):
-  print("Description: ", description)
-  try:
-    user_name = user.get_full_name()
-  except:
-    user_name = "anonym"
+	print("Description: ", description)
+	try:
+		user_name = user.get_full_name()
+	except:
+		user_name = "anonym"
 
-  msg = user_name + " rapporterte bildet " +  \
-    str(photo.pk) + " i album " + photo.album.title + \
-  	" med begrunnelse " + description 
+	msg = user_name + " rapporterte bildet " +  \
+		str(photo.pk) + " i album " + photo.album.title + \
+		" med begrunnelse " + description 
 
-  print("Warning prokom: " + msg)
+	print("Warning prokom: " + msg)
 	# Send email to prokom
 
 
