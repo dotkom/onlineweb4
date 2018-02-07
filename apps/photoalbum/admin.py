9050315e111d610894195270397b6ad21b70db1d
Dora Oline Eriksrud <doraoline@gmail.com>
2018-02-07 19:50:09 +0100

    Breaking up in smalle tables. Added tags and user tags

--- apps/photoalbum/admin.py
+++ apps/photoalbum/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
 
 # Register your models here.
-from apps.photoalbum.models import Album, Photo
+from apps.photoalbum.models import Album, Photo, AlbumTag
 
+class AlbumAdmin(VersionAdmin):
+	model = Album
+	#list_display = ['title', 'photos', 'tags']
+	fieldsets = (
+		#(None, {'fields': ('title', 'photos', 'tags')})
+	)
 
-
-admin.site.register(Album)
+admin.site.register(Album, AlbumAdmin)
 admin.site.register(Photo)
+admin.site.register(AlbumTag)
--- apps/photoalbum/forms.py
+++ apps/photoalbum/forms.py
@@ -6,9 +6,10 @@ from django.utils.translation import ugettext as _
 from apps.photoalbum.models import Album, Photo
 
 class AlbumForm(forms.Form):
-	print("Album form")
 	title = forms.CharField(widget=forms.TextInput(), label=_("Tittel"))
 	photos = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label=_("Bilder"))    
+	tags = forms.CharField(widget=forms.TextInput(), label=("Tags"))
+
 
 class AlbumForm2(forms.ModelForm):
 	print("AlbumForm2")
@@ -16,7 +17,6 @@ class AlbumForm2(forms.ModelForm):
 
 	class Meta(object):
 		model = Album
-		print("Meta object: ", object)
 		fields = ['title', 'photos_to_upload']
 
 
@@ -24,6 +24,7 @@ class AlbumNameForm(forms.ModelForm):
 	class Meta(object):
 		model = Album
 		fields = ['title']
+
 
 class UploadPhotosForm(forms.ModelForm):
 	photos = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label=_("Bilder"), required=False)    
