d33e01cef27edde2aa4627738006476c10fc7ac6
Dora Oline Eriksrud <doraoline@gmail.com>
2018-02-14 18:06:18 +0100

    Can filter on exact album name

--- apps/photoalbum/models.py
+++ apps/photoalbum/models.py
@@ -10,7 +10,6 @@ IMAGE_FOLDER = "images/photo_album"
 
 class Album(models.Model):
 	title = models.CharField(_("Tittel"), blank=False, null=False, max_length=50)
-	#photos = models.ManyToManyField("Photo")
 	tags = models.ManyToManyField("AlbumTag")
 	
 	def __str__(self):
@@ -39,9 +38,7 @@ class Album(models.Model):
 class Photo(models.Model):
 	# Path should depend on album?
 	photo = models.ImageField(upload_to=IMAGE_FOLDER)
-	#album = models.ForeignKey("Album")
-	#tagged_users = models.ManyToManyField(OnlineUser, blank=True)
-
+	
 	def get_album(self):
 		return AlbumToPhoto.objects.get(photo=self).album
 
