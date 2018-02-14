cf4bf971e057fe09f85a21f5c97c90b2ca2a9ca1
Westermoen <jakob@westermoen.com>
2018-02-14 21:46:54 +0100

    fixed linting errors

--- assets/careeropportunity/containers/DetailView.js
+++ assets/careeropportunity/containers/DetailView.js
@@ -5,19 +5,17 @@ import jobPropTypes from '../propTypes/job';
 class DetailView extends React.Component {
   constructor(props) {
     super(props);
+    this.id = parseInt(props.match.params.id, 10);
+    this.job = props.jobs.find(j => j.id === this.id);
   }
-
-  const id = parseInt(props.match.params.id, 10);
-
-  const job = props.jobs.find(j => j.id === id);
 
   componentDidMount(){
       window.scrollTo(0, 0);
   }
 
   render() {
-    return job ? (
-      <InfoBox {...job} />
+    return this.job ? (
+      <InfoBox {...this.job} />
     ) : (
       <div>Denne karrieremuligheten eksisterer ikke.</div>
     );
