bd6e3270de06b30fd4a92da680dec3eee9ae0d22
Dora Oline Eriksrud <doraoline@gmail.com>
2018-04-05 19:09:28 +0200

    Small design fixes

--- apps/photoalbum/urls.py
+++ apps/photoalbum/urls.py
@@ -6,6 +6,6 @@ from apps.photoalbum import views
 
 urlpatterns = [
     url(r'^$', views.AlbumsListView.as_view(), name='albums_list'),
-    url(r'^(?P<pk>\d+)/(?P<album_slug>[a-zA-Z0-9_-]+)/$', views.AlbumDetailView.as_view(), name="album_detail"),
+    url(r'^(?P<pk>\d+)/(?P<slug>[a-zA-Z0-9_-]+)/$', views.AlbumDetailView.as_view(), name="album_detail"),
     url(r'^(?P<album_pk>\d+)/(?P<album_slug>[a-zA-Z0-9_-]+)/(?P<pk>\w+)/$', views.PhotoDetailView.as_view(), name="photo_detail"),
 ]
