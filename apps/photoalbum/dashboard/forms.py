bd6b9831c1b17201bd989eeffd79bc90a2fb9e42
Dora Oline Eriksrud <doraoline@gmail.com>
2018-04-05 11:41:39 +0200

    AlbumForm is modelform again

--- apps/photoalbum/dashboard/forms.py
+++ apps/photoalbum/dashboard/forms.py
@@ -2,12 +2,40 @@
 from django import forms
 from django.utils.translation import ugettext as _
 
+from taggit.forms import TagWidget
+from apps.photoalbum.widgets import MultipleImagesInput
 from apps.photoalbum.models import Album, Photo
 
-class AlbumForm(forms.Form):
-	title = forms.CharField(widget=forms.TextInput(), label=_("Tittel"))
-	tags = forms.CharField(widget=forms.TextInput(), label=_("Tags"))
-	photos = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label=_("Bilder"))    
+from apps.dashboard.widgets import multiple_widget_generator
+
+class AlbumForm(forms.ModelForm):
+	class Meta(object):
+
+		model = Album
+		fields = [
+			'title',
+			'tags',
+			'photos'
+		]
+
+		photos_fields = [('photos', {'id': 'responsive-image-id'})]
+		widgetlist = [
+			#(MultipleImagesInput, 'photos')
+		]
+
+		print("Before widget stuff")
+		# Multiple widget generator merges results from regular widget_generator into a single widget dict
+		widgets = multiple_widget_generator(widgetlist)
+		print("Before widgets update")
+		widgets.update({
+			'tags': TagWidget(attrs={'placeholder': 'Eksempel: åre, online, kjelleren'}),
+			'photos': MultipleImagesInput(attrs={'multiple': True, 'name': 'Bilder'})
+			})
+
+		labels = {
+					'tags': 'Tags'
+		}
+
 
 
 class AlbumNameForm(forms.ModelForm):
@@ -32,3 +60,34 @@ class UploadPhotosForm(forms.ModelForm):
 
 class TagUsersForm(forms.Form):
 	users = forms.CharField(widget=forms.TextInput(), label=_("Brukere"))
+
+"""
+class AlbumForm(forms.ModelForm):
+	class Meta(object):
+
+		model = Album
+		fields = [
+
+		]
+
+	title = forms.CharField(widget=forms.TextInput(), label=_("Tittel"))
+	tags = forms.CharField(widget=forms.TextInput(), label=_("Tags"))
+	photos = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label=_("Bilder"))    
+
+	widgetlist = [
+		#(MultipleImagesInput, 'photos')
+	]
+
+	print("Before widget stuff")
+	# Multiple widget generator merges results from regular widget_generator into a single widget dict
+	widgets = multiple_widget_generator(widgetlist)
+	print("Before widgets update")
+	widgets.update({
+		'tags': TagWidget(attrs={'placeholder': 'Eksempel: åre, online, kjelleren'}),
+		'photos': MultipleImagesInput(attrs={'multiple': True, 'name': 'Bilder'})
+		})
+
+	#labels = {
+	#			'tags': 'Tags'
+	#}
+	"""
---
+++ apps/photoalbum/widgets.py
@@ -0,0 +1,73 @@
+# -*- coding: utf8 -*-
+
+from django.conf import settings
+from django.core.urlresolvers import reverse_lazy
+from django.forms import HiddenInput, TextInput
+from django.forms.utils import flatatt, force_text, format_html
+
+from apps.gallery.models import ResponsiveImage
+from apps.gallery.widgets import SingleImageInput
+
+
+WIDGET_STRING = """<br /><input{} />\r\n
+<div id="multiple-image-field-thumbnail">{}</div>
+<a href="#" class="btn btn-primary" id="add-responsive-images">\r\n
+<i class="fa fa-plus fa-lg"></i> Velg</a>\r\n
+<a href="{}" class="btn btn-primary" target="_blank">\r\n
+<i class="fa fa-image fa-lg"></i> Last opp</a>\r\n
+<a href="#" class="btn btn-danger" id="dashboard-gallery-remove-image">\r\n
+<i class="fa fa-times fa-lg"></i> Fjern bilder</a><br>\r\n
+<div id="image-selection-wrapper">\r\n
+<h2 id="image-selection-title">Velg bilder</h2>\r\n
+<div class="row">\r\n
+<div class="col-md-12">\r\n
+<div class="input-group">\r\n
+<input type="text" id="image-gallery-search" class="form-control" placeholder="Skriv inn søkeord...">\r\n
+<span class="input-group-btn">\r\n
+<a class="btn btn-primary" id="image-gallery-search-button" type="button">Søk!</a>\r\n
+</span>\r\n
+</div>\r\n
+</div>\r\n
+</div>\r\n
+<hr />\r\n
+<div class="row" id="image-gallery-search-results"></div>\r\n
+</div>\r\n"""
+
+
+
+class MultipleImagesInput(HiddenInput):
+  def __init__(self, attrs=None):
+        super(MultipleImagesInput, self).__init__(attrs)
+        self.input_type = 'hidden'
+
+  def render(self, name, value, attrs={'multiple': True}):
+      """
+      Renders this field widget as HTML
+      :param name: Field input name
+      :param value: Field input value
+      :param attrs: Field input attributes
+      :return: An HTML string representing this widget
+      """
+
+      if value is None:
+          value = ''
+
+      img_thumb = 'Det er ikke valgt noen bilder.'
+
+      attrs = self.build_attrs(self.attrs, attrs)
+      final_attrs = self.build_attrs(attrs, {'type': self.input_type, 'name': name})
+      if value != '':
+          # Only add the value attribute if the value is non-empty
+          final_attrs['value'] = force_text(self._format_value(value))
+          img = ResponsiveImage.objects.get(pk=value)
+          img_thumb = format_html(
+              '<img src="{}" alt title="{}"/>',
+              settings.MEDIA_URL + str(img.thumbnail),
+              str(img.name),
+              encoding='utf-8'
+          )
+
+      upload_url = reverse_lazy('gallery_dashboard:upload')
+
+      return format_html(WIDGET_STRING, flatatt(final_attrs), img_thumb, upload_url)  
+
