160e731987cd60f35f6ee79af25da3c2e8b7f8ce
MartinLunde <martinlunde96@gmail.com>
2018-04-11 20:00:13 +0200

    Awkward last linting errors

--- assets/profiles/dependent_fields.js
+++ assets/profiles/dependent_fields.js
@@ -3,10 +3,10 @@ import $ from 'jquery';
 export default () => {
   $('#div_id_documentation').hide();
   $('#id_field_of_study').on('change', () => {
-    if ($('#id_field_of_study').val() === '40'){
+    if ($('#id_field_of_study').val() === '40') {
       $('#div_id_documentation').show();
     } else {
       $('#div_id_documentation').hide();
     }
   });
-}
+};
