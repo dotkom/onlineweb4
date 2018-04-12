e172cf8ee99aa4e6853af95964d20289e08c809d
Håkon Solbjørg <hlsolbjorg@gmail.com>
2018-04-12 18:17:10 +0200

    Python regex uses strings and not '/' to signal an expression…

--- apps/authentication/validators.py
+++ apps/authentication/validators.py
@@ -4,5 +4,5 @@ from django.core.exceptions import ValidationError
 
 
 def validate_rfid(rfid):
-    if not re.match('/^\d{8,10}$/', rfid):
+    if not re.match('^\d{8,10}$', rfid):
         raise ValidationError('Dette er ikke en gyldig RFID.')
