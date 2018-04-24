82cc2a5a8537df8883f79846b809b17ce2b97002
Håkon Solbjørg <hlsolbjorg@gmail.com>
2018-04-24 15:15:18 +0200

    Add more django admin styling for Shop models.

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -5,12 +5,23 @@ from apps.shop.models import Order, OrderLine
 
 class OrderAdmin(admin.ModelAdmin):
     model = Order
-    list_display = ('__str__', 'price', 'quantity')
+    list_display = ('__str__', 'price', 'quantity', 'order_line')
+
+
+class OrderInline(admin.TabularInline):
+    model = Order
+    extra = 0
+    readonly_fields = ['__str__']
+    fields = ['__str__', 'price', 'quantity']
 
 
 class OrderLineAdmin(admin.ModelAdmin):
     model = OrderLine
-    list_display = ('__str__', 'user', 'datetime')
+    list_display = ('__str__', 'user', 'datetime', 'paid')
+    list_filter = ['paid']
+    search_fields = ['user__first_name', 'user__last_name', 'user__username', 'user__ntnu_username']
+
+    inlines = [OrderInline]
 
 
 admin.site.register(Order, OrderAdmin)
