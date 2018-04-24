5ae6a4475c5c569745a975d6982dab28f09ff551
MartinLunde <martinlunde96@gmail.com>
2018-04-24 13:50:40 +0200

    Show/Hide descriptive text in addition to document form field

--- assets/profiles/dependent_fields.js
+++ assets/profiles/dependent_fields.js
@@ -1,12 +1,12 @@
 import $ from 'jquery';
 
 export default () => {
-  $('#div_id_documentation').hide();
+  $('#field-of-study-application-documentation').hide();
   $('#id_field_of_study').on('change', () => {
     if ($('#id_field_of_study').val() === '40') {
-      $('#div_id_documentation').show();
+      $('#field-of-study-application-documentation').show();
     } else {
-      $('#div_id_documentation').hide();
+      $('#field-of-study-application-documentation').hide();
     }
   });
 };
