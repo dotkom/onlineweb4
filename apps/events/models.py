cf98687ca0b8c646179606affe06225573ab970a
KevinMidboe <kevin.midboe@gmail.com>
2018-04-25 22:37:38 +0200

    Indentation fixx

--- apps/events/models.py
+++ apps/events/models.py
@@ -516,9 +516,9 @@ class AttendanceEvent(models.Model):
     @property
     def visible_attending_attendees(self):
         """ List with all attendees whom want to be displayed as attending else return a anonymous name """
-        dyr = ['piggsvin', 'kjøttmais', 'flaggermus', 'elg', 'villsvin', 'rådyr', 'moskus', 'narhval', 
-            'spekkhogger', 'gaupe', 'ekorn', 'kanin', 'lemen', 'neshorn', 'ørn', 'gullfisk', 'kodiakbjørn',
-            'hacker', 'stol', 'bever', 'datamaskin', 'piano', 'strykejern', 'samurai', 'laks', 'server']
+        dyr = ['piggsvin', 'kjøttmais', 'flaggermus', 'elg', 'villsvin', 'rådyr', 'moskus', 'narhval',
+               'spekkhogger', 'gaupe', 'ekorn', 'kanin', 'lemen', 'neshorn', 'ørn', 'gullfisk', 'kodiakbjørn',
+               'hacker', 'stol', 'bever', 'datamaskin', 'piano', 'strykejern', 'samurai', 'laks', 'server']
 
         visible_attendees = []
         for attendee in self.attending_attendees_qs:
