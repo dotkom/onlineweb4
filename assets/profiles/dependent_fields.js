6a503c46328ce03af9fe491f186fce6dbcf57cec
MartinLunde <martinlunde96@gmail.com>
2018-05-28 17:06:00 +0200

    Enable documentation for postgraduate studies

--- assets/profiles/dependent_fields.js
+++ assets/profiles/dependent_fields.js
@@ -1,12 +1,23 @@
 import $ from 'jquery';
 
 export default () => {
-  $('#field-of-study-application-documentation').hide();
+  let master_doc = $('#field-of-study-application-master-documentation');
+  let social_doc = $('#field-of-study-application-documentation');
+  social_doc.hide();
+  master_doc.hide();
+
   $('#id_field_of_study').on('change', () => {
-    if ($('#id_field_of_study').val() === '40') {
-      $('#field-of-study-application-documentation').show();
-    } else {
-      $('#field-of-study-application-documentation').hide();
+      let field_of_study = parseInt($('#id_field_of_study').val());
+
+    if (field_of_study >= 10 && field_of_study <= 30){
+      social_doc.hide();
+      master_doc.show();
+    } else if (field_of_study === 40){
+      social_doc.show();
+      master_doc.hide();
+    } else{
+      social_doc.hide();
+      master_doc.hide();
     }
   });
 };
