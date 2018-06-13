62344abcf56e3bffc47411ca91bad5defe8d1308
MartinLunde <martinlunde96@gmail.com>
2018-06-13 20:50:45 +0200

    small general fixes and cleaning

--- assets/profiles/dependent_fields.js
+++ assets/profiles/dependent_fields.js
@@ -3,21 +3,16 @@ import $ from 'jquery';
 export default () => {
   let master_doc = $('#field-of-study-application-master-documentation');
   let social_doc = $('#field-of-study-application-documentation');
-  social_doc.hide();
-  master_doc.hide();
 
   $('#id_field_of_study').on('change', () => {
-      let field_of_study = parseInt($('#id_field_of_study').val());
+    let field_of_study = parseInt($('#id_field_of_study').val());
+    social_doc.hide();
+    master_doc.hide();
 
-    if (field_of_study >= 10 && field_of_study <= 30){
-      social_doc.hide();
+    if (field_of_study >= 10 && field_of_study <= 30) {
       master_doc.show();
-    } else if (field_of_study === 40){
+    } else if (field_of_study === 40) {
       social_doc.show();
-      master_doc.hide();
-    } else{
-      social_doc.hide();
-      master_doc.hide();
     }
   });
 };
