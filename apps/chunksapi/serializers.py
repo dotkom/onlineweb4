3558577136fbc3452e753845d7d925669789b3f0
Aborysa <aborysa@gmail.com>
2018-07-25 19:32:42 +0200

    Fixed import sorting

--- apps/chunksapi/serializers.py
+++ apps/chunksapi/serializers.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from chunks.models import Chunk
-
 from rest_framework import serializers
 
 
--- apps/chunksapi/views.py
+++ apps/chunksapi/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from apps.chunksapi.serializers import ChunkSerializer
-
 from chunks.models import Chunk
 from rest_framework import mixins, viewsets
 from rest_framework.views import APIView
+
+from apps.chunksapi.serializers import ChunkSerializer
 
 
 class ChunkViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.ListModelMixin, APIView):
