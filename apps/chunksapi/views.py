01bb52aa01cad32b95fd757fed0c7c7ba405a42a
Aborysa <aborysa@gmail.com>
2018-07-29 18:30:39 +0200

    Removed pagination and added filter for key

--- apps/chunksapi/views.py
+++ apps/chunksapi/views.py
@@ -11,3 +11,5 @@ class ChunkViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.Li
     queryset = Chunk.objects.all()
     model = Chunk
     serializer_class = ChunkSerializer
+    filter_fields = ('key', )
+    pagination_class = None
