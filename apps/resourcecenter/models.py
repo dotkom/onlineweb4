47b34f776ee196a085040afa427348c07a704e18
oleast <oleast@stud.ntnu.no>
2018-08-12 18:24:43 +0200

    Fix resourcecenter to use ResponsiveImage

--- apps/resourcecenter/models.py
+++ apps/resourcecenter/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-from apps.resourcecenter import settings
-
+from apps.gallery.models import ResponsiveImage
+from django.db.models import SET_NULL
 
 class Resource(models.Model):
     title = models.CharField(max_length=35)
     description = models.TextField()
-    image = models.ImageField(upload_to=settings.IMAGES_PATH)
+    image = models.ForeignKey(ResponsiveImage, related_name='resources', blank=True, null=True, on_delete=SET_NULL)
     priority = models.IntegerField(default=0)
 
     def __str__(self):
-        return "{title} (Priority: {priority})".format(title=self.title,
-                                                       priority=self.priority)
+        return "{title} (Priority: {priority})".format(title=self.title, priority=self.priority)
--- apps/resourcecenter/serializers.py
+++ apps/resourcecenter/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from apps.resourcecenter.models import Resource
-
+from apps.gallery.serializers import ResponsiveImageSerializer
 
 class ResourceSerializer(serializers.ModelSerializer):
+
+    image = ResponsiveImageSerializer()
+
     class Meta:
         model = Resource
         fields = ('id', 'title', 'description', 'image')
