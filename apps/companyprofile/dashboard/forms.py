1e15362b4830a5ef53f5c959f3dd1f2b46926f9f
r33an1 <r33an1@me.com>
2018-09-26 23:16:29 +0200

    Adds validation to homepage field in company forms

--- apps/companyprofile/dashboard/forms.py
+++ apps/companyprofile/dashboard/forms.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.forms import ModelForm
+from django.forms.fields import URLField
 
 from apps.companyprofile.models import Company
 from apps.dashboard.widgets import widget_generator
@@ -7,11 +8,14 @@ from apps.gallery.widgets import SingleImageInput
 
 
 class CompanyForm(ModelForm):
+    site = URLField(max_length=100)
 
     class Meta(object):
         model = Company
         fields = ('name', 'short_description', 'long_description', 'image', 'site', 'email_address', 'phone_number',)
+
         exclude = ['old_image']
+
 
         # Widget generator accepts a form widget, and a list of tuples between field name and an attribute dict
         widgets = widget_generator(SingleImageInput, [('image', {'id': 'responsive-image-id'})])
