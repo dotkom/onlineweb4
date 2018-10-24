75c2e1019c15d14a5adf72270a2516f231676786
Jarle Trollebø <aborysa@gmail.com>
2018-10-24 18:48:19 +0200

    Removed whitespaces from line 7

--- utils/pagination.py
+++ utils/pagination.py
@@ -4,7 +4,7 @@ from rest_framework.pagination import PageNumberPagination as RFPageNumberPagina
 class PageNumberPagination(RFPageNumberPagination):
     page_size = 10
     max_page_size = 80
-    
+
     def get_page_size(self, request):
         if ('page_size' in request.query_params) and request.query_params['page_size'].isnumeric():
             return min(self.max_page_size, int(request.query_params['page_size']))
