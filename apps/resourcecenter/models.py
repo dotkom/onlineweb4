e3c34476ea13262d694bb5163c08bc8bc25f1560
Ole Anders Stokker <oleast@stud.ntnu.no>
2018-11-01 21:30:08 +0100

    Revert "Add image field to Hobby and Resource API endpoints"

--- apps/resourcecenter/models.py
+++ apps/resourcecenter/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.db.models import SET_NULL
 
-from apps.gallery.models import ResponsiveImage
+from apps.resourcecenter import settings
 
 
 class Resource(models.Model):
     title = models.CharField(max_length=35)
     description = models.TextField()
-    image = models.ForeignKey(ResponsiveImage, related_name='resources', blank=True, null=True, on_delete=SET_NULL)
+    image = models.ImageField(upload_to=settings.IMAGES_PATH)
     priority = models.IntegerField(default=0)
 
     def __str__(self):
-        return "{title} (Priority: {priority})".format(title=self.title, priority=self.priority)
+        return "{title} (Priority: {priority})".format(title=self.title,
+                                                       priority=self.priority)
