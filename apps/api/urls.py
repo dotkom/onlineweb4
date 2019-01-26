da325a728db2e393ef1f50a77fa466cd8817e8fe
oleast <oleast@stud.ntnu.no>
2019-01-26 18:47:36 +0100

    Disable api v0

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -7,8 +7,8 @@ from apps.shop import views as shop_views
 from apps.sso import views as sso_views
 
 urlpatterns = [
-    url(r'^',       include('apps.api.v0.urls')),
-    url(r'^',       include('apps.api.rfid.urls')),
+    #url(r'^',       include('apps.api.v0.urls')),
+    #url(r'^',       include('apps.api.rfid.urls')),
     url(r'^v1/rfid/$', shop_views.SetRFIDView.as_view(), name='set_rfid'),
     url(r'^v1/auth/$', sso_views.TokenView.as_view(), name='oauth2_provider_token'),
     url(r'^v1/attend/$', event_views.AttendViewSet.as_view(), name='event_attend'),
