4298da1b94a51e734086907ef56c3778c7fa5f1b
kharann <anhkhav@email.com>
2019-02-13 23:31:06 +0100

    fixing linting issues

--- assets/contact/index.js
+++ assets/contact/index.js
@@ -1,15 +1,13 @@
 import './less/contact.less';
 
-document.getElementById("is_anon").addEventListener("click", () => {
+document.getElementById('is_anon').addEventListener('click', () => {
+  const checkbox = document.getElementById('id_contact_checkbox');
+  const contactName = document.getElementById('id_contact_name');
+  const contactMail = document.getElementById('id_contact_email');
 
-    const checkbox = document.getElementById("id_contact_checkbox");
-    const contact_name = document.getElementById("id_contact_name");
-    const contact_mail = document.getElementById("id_contact_email");
-
-    if (checkbox.checked) {
-        contact_mail.disabled = contact_name.disabled= true;
-    }
-    else {
-        contact_mail.disabled = contact_name.disabled = false;
-    }
-})
+  if (checkbox.checked) {
+    contactMail.disabled = contactName.disabled = true;
+  } else {
+    contactMail.disabled = contactName.disabled = false;
+  }
+});
