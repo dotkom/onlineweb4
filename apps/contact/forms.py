f49ce7888659a13d60d0866e67a3191bd56b9b43
kharann <anhkhav@gmail.com>
2019-02-19 16:57:00 +0100

    Adding validation method for when a user wants to be anon/not be anon

--- apps/contact/forms.py
+++ apps/contact/forms.py
@@ -8,8 +8,18 @@ class ContactForm(forms.Form):
                     )
     contact_receiver = forms.ChoiceField(required=True, label="Hvem ønsker du å kontakte?", choices=mail_choices)
     contact_checkbox = forms.BooleanField(required=False)
-    contact_name = forms.CharField(required=True, widget=forms.TextInput({"placeholder": "Navn"}))
-    contact_email = forms.EmailField(required=True, widget=forms.TextInput({"placeholder": "Epostadresse"}))
+    contact_name = forms.CharField(required=False, widget=forms.TextInput({"placeholder": "Navn"}))
+    contact_email = forms.EmailField(required=False, widget=forms.TextInput({"placeholder": "Epostadresse"}))
     content = forms.CharField(required=True, widget=forms.Textarea({"placeholder": "Din melding"}))
     captcha = ReCaptchaField(error_messages={'required': ('Du klarte ikke captchaen! Er du en bot?'),
                                              'invalid': ('Du klarte ikke captchaen! Er du en bot?')})
+
+    def clean(self):
+        name = self.cleaned_data['contact_name']
+        is_anon = self.cleaned_data['contact_checkbox']
+        email = self.cleaned_data['contact_email']
+
+        if not (name and email) and not is_anon:
+            raise forms.ValidationError(
+                "Du må skrive ditt navn og mail hvis du ikke skal være anonym!"
+            )
