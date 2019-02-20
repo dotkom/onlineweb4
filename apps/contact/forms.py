99db35b21872bb2c2b07a0ad1957c94a932205f2
Anhkha <anhkhav@gmail.com>
2019-02-20 20:47:09 +0100

    checkbox for being anon for contact form (#2200)

--- apps/contact/forms.py
+++ apps/contact/forms.py
@@ -7,8 +7,21 @@ class ContactForm(forms.Form):
                     ("dotkom@online.ntnu.no", "Drifts- og utviklingskomiteen")
                     )
     contact_receiver = forms.ChoiceField(required=True, label="Hvem ønsker du å kontakte?", choices=mail_choices)
-    contact_name = forms.CharField(required=False, widget=forms.TextInput({"placeholder": "Navn"}))
-    contact_email = forms.EmailField(required=False, widget=forms.TextInput({"placeholder": "Epostadresse"}))
+    contact_checkbox = forms.BooleanField(required=False)
+    contact_name = forms.CharField(required=False, widget=forms.TextInput({"placeholder": "Navn",
+                                                                           "required": True}))
+    contact_email = forms.EmailField(required=False, widget=forms.EmailInput({"placeholder": "Epostadresse",
+                                                                             "required": True}))
     content = forms.CharField(required=True, widget=forms.Textarea({"placeholder": "Din melding"}))
     captcha = ReCaptchaField(error_messages={'required': ('Du klarte ikke captchaen! Er du en bot?'),
                                              'invalid': ('Du klarte ikke captchaen! Er du en bot?')})
+
+    def clean(self):
+        name = self.cleaned_data.get('contact_name')
+        is_anon = self.cleaned_data.get('contact_checkbox')
+        email = self.cleaned_data.get('contact_email')
+
+        if not (name and email) and not is_anon:
+            error_msg = "Must be filled"
+            self.add_error('contact_name', error_msg)
+            self.add_error('contact_email', error_msg)
--- assets/contact/index.js
+++ assets/contact/index.js
@@ -1,1 +1,13 @@
 import './less/contact.less';
+
+const checkbox = document.getElementById('id_contact_checkbox');
+const contactName = document.getElementById('id_contact_name');
+const contactMail = document.getElementById('id_contact_email');
+
+checkbox.addEventListener('change', () => {
+  contactMail.disabled = checkbox.checked;
+  contactMail.value = '';
+
+  contactName.disabled = checkbox.checked;
+  contactName.value = '';
+});
