71985e5bae895daa3cfc302fab9d5db47912183d
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-02-26 14:04:12 +0100

    docs: Add some docs for the non-standard resolve method.

--- apps/shop/graphql_schema.py
+++ apps/shop/graphql_schema.py
@@ -14,6 +14,9 @@ class OrderNode(DjangoObjectType):
         interfaces = (relay.Node,)
 
     def resolve_content_object(self, info):
+        # Not really sure what to do in this case.
+        # We expect the object to be an ItemNode, but what should we do if it isn't?
+        # Return None? Error out? (Errors marks the field as null and sends the raised exception)
         if self.content_object.__class__.__name__ != Item.__name__:
             raise ValueError("Content object on Order has invalid type for this query {} vs {}."
                              .format(self.content_object.__class__.__name__, Item.__name__))
