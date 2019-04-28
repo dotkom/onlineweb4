e3c74637c5d68cfbddacac5b3b2a43705282f5a5
oleast <oleast@stud.ntnu.no>
2019-04-28 22:20:08 +0200

    Change Resource image to ResponsiveImage, add option to add image via API

--- apps/resourcecenter/models.py
+++ apps/resourcecenter/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.db.models import SET_NULL
 
-from apps.resourcecenter import settings
+from apps.gallery.models import ResponsiveImage
 
 
 class Resource(models.Model):
     title = models.CharField(max_length=35)
     description = models.TextField()
-    image = models.ImageField(upload_to=settings.IMAGES_PATH)
+    image = models.ForeignKey(ResponsiveImage, related_name='resources', blank=True, null=True, on_delete=SET_NULL)
     priority = models.IntegerField(default=0)
 
     def __str__(self):
