dc007e79a22435622329bb3e33926300570ab9da
oleast <oleast@stud.ntnu.no>
2019-04-28 22:22:42 +0200

    Add easy model resitriction for API based on permissions, restict resource endpoint

--- apps/resourcecenter/views.py
+++ apps/resourcecenter/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from onlineweb4.permissions import ModelPermission
 from rest_framework import viewsets
 
 from apps.resourcecenter.models import Resource
@@ -16,6 +17,13 @@ def index(request):
     return render(request, 'resourcecenter/index.html', context)
 
 
-class HobbyViewSet(viewsets.ReadOnlyModelViewSet):
+class ResourcePermission(ModelPermission):
+    create_permissions = ['resourcecenter.create_resource']
+    update_permissions = ['resourcecenter.update_resource']
+    delete_permissions = ['resourcecenter.delete_resource']
+
+
+class ResourceViewSet(viewsets.ModelViewSet):
     queryset = Resource.objects.all()
     serializer_class = ResourceSerializer
+    permission_classes = (ResourcePermission,)
