cac415a36a3b2e85e0be79ae90ee3de56fa46330
oleast <oleast@stud.ntnu.no>
2019-04-28 22:17:56 +0200

    Change hobbygroup image to ResponsiveImage

--- apps/hobbygroups/serializers.py
+++ apps/hobbygroups/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
+from apps.gallery.serializers import ResponsiveImageSerializer
 from apps.hobbygroups.models import Hobby
 
 
 class HobbySerializer(serializers.ModelSerializer):
+
+    image = ResponsiveImageSerializer()
+
     class Meta:
         model = Hobby
-        fields = ('id', 'title', 'description', 'read_more_link')
+        fields = ('id', 'title', 'description', 'read_more_link', 'image')
