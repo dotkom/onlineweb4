d1e67ab630d7c7277536af1cdf4061007018ffb9
oleast <oleast@stud.ntnu.no>
2019-05-04 14:48:46 +0200

    Create api endpoints and proptypes for resources

--- assets/dashboard/resources/api/image.js
+++ assets/dashboard/resources/api/image.js
@@ -1,7 +1,7 @@
 import toQueryString from '../../../common/utils/queryParams';
 
 const getResponsiveImages = async (params = {}) => {
-  const response = await fetch(`/api/v1/images${toQueryString(params)}`);
+  const response = await fetch(`/api/v1/images/${toQueryString(params)}`);
   const json = await response.json();
   return json.results || [];
 };
