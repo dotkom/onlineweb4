211bfae95fefdb1e74346c9e2ae3acffd469d178
oleast <oleast@stud.ntnu.no>
2019-05-04 14:48:46 +0200

    Create api endpoints and proptypes for resources

---
+++ assets/dashboard/resources/propTypes/resource.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+import ImagePropTypes from '../../../common/proptypes/ImagePropTypes';
+
+const ResourcePropTypes = PropTypes.shape({
+  image: ImagePropTypes,
+  id: PropTypes.number,
+  title: PropTypes.string,
+  priority: PropTypes.number,
+  description: PropTypes.string,
+});
+
+export default ResourcePropTypes;
