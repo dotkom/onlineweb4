15c8b32d3ee753bcac8291d1c5e60b7015da943e
oleast <oleast@stud.ntnu.no>
2019-05-04 14:02:25 +0200

    Add React routes to resource dashboard, hand off routes from Django to React

--- apps/resourcecenter/dashboard/urls.py
+++ apps/resourcecenter/dashboard/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from apps.resourcecenter.dashboard import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='resources_dashboard_index')
+    url(r'^.*$', views.index, name='resources_dashboard_index')
 ]
