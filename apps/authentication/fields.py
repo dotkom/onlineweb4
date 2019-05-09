4c34797fa8102e255368d19328bb3fb9ecf6fa0d
oleast <oleast@stud.ntnu.no>
2019-05-09 20:20:04 +0200

    Split UserSerializer into different serializers for reading, updating, and creating

---
+++ apps/authentication/fields.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from apps.authentication.models import Email
+
+
+class OnlineUserEmailValidator(object):
+
+    def validate_email(self, email):
+        all_emails = [email_obj.email for email_obj in Email.objects.all()]
+        if email in all_emails:
+            raise serializers.ValidationError('Det eksisterer allerede en bruker med denne e-postadressen')
+        return email
+
+    def __call__(self, value):
+        return self.validate_email(value)
+
+
+class OnlineUserEmailField(serializers.EmailField):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        validator = OnlineUserEmailValidator()
+        self.validators.append(validator)
--- apps/authentication/serializers.py
+++ apps/authentication/serializers.py
@@ -1,8 +1,13 @@
+import logging
+
+from onlineweb4.fields.recaptcha import RecaptchaField
 from rest_framework import serializers
 
+from apps.authentication.fields import OnlineUserEmailField
 from apps.authentication.models import Email
 from apps.authentication.models import OnlineUser as User
 from apps.authentication.models import Position, SpecialPosition
+from apps.authentication.utils import send_register_verification_email
 
 
 class UserNameSerializer(serializers.ModelSerializer):
@@ -11,17 +16,66 @@ class UserNameSerializer(serializers.ModelSerializer):
         fields = ('first_name', 'last_name', 'email', 'username',)
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserReadOnlySerializer(serializers.ModelSerializer):
     rfid = serializers.HiddenField(default='')
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'rfid', 'password', 'email', 'username',)
+        fields = ('id', 'first_name', 'last_name', 'rfid', 'username',)
+        read_only = True
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    recaptcha = RecaptchaField()
+    email = OnlineUserEmailField(required=True)
+
+    def create(self, validated_data):
+        """
+        The recaptcha field is not part of the model, but will still need to be validated.
+        All serializer fields will be put into 'Model#create', and 'recaptcha' is removed for the serializer not
+        to try and create it on the model-
+        """
+        validated_data.pop('recaptcha')
+
+        """ Create user with serializer method """
+        created_user = super().create(validated_data)
+
+        """ Set default values and related objects to user """
+        created_user.is_active = False
+        email = Email.objects.create(
+            user=created_user,
+            email=validated_data.get('email').lower(),
+            primary=True,
+        )
+        created_user.save()
+
+        send_register_verification_email(
+            user=created_user,
+            email_obj=email,
+            request=self.context.get('request', None)
+        )
+
+        return created_user
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'password', 'email', 'username', 'recaptcha'
+        )
         extra_kwargs = {
-            'email': {'write_only': True},
-            'password': {'write_only': True},
-            'username': {'write_only': True},
+            'password': {'write_only': True}
         }
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    email = OnlineUserEmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'rfid', 'password', 'email', 'username', 'infomail', 'jobmail', 'nickname',
+            'website', 'github', 'linkedin', 'gender', 'bio', 'ntnu_username', 'password', 'phone',
+        )
 
 
 class PositionSerializer(serializers.ModelSerializer):
