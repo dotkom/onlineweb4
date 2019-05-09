a146ca32084b5eb5163f3aee8b8bdcea236e1355
oleast <oleast@stud.ntnu.no>
2019-05-09 20:21:57 +0200

    Update user api view to new serializers. Create verifiaction email util for signup

--- apps/authentication/api/views.py
+++ apps/authentication/api/views.py
@@ -1,8 +1,10 @@
+from guardian.shortcuts import get_objects_for_user
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import AllowAny
 
 from apps.authentication.models import OnlineUser as User
-from apps.authentication.serializers import UserSerializer
+from apps.authentication.serializers import (UserCreateSerializer, UserReadOnlySerializer,
+                                             UserUpdateSerializer)
 
 
 class UserViewSet(mixins.CreateModelMixin,
@@ -14,9 +16,23 @@ class UserViewSet(mixins.CreateModelMixin,
     """
 
     queryset = User.objects.all()
-    serializer_class = UserSerializer
     permission_classes = (AllowAny,)
     filterset_fields = ('first_name', 'last_name', 'rfid',)
 
-    def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_authenticated:
+            return User.objects.none()
+        if user.is_superuser or user.is_staff:
+            return User.objects.all()
+        return get_objects_for_user(user, 'authentication.view_online_user')
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserCreateSerializer
+        if self.action in ['update', 'partial_update']:
+            return UserUpdateSerializer
+        if self.action in ['list', 'retrieve']:
+            return UserReadOnlySerializer
+
+        return super().get_serializer_class()
