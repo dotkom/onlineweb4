d7468094a18cf693b68e3c92fdf42069c82796ec
oleast <oleast@stud.ntnu.no>
2019-06-29 11:24:11 +0200

    Remove unused import...

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -2,7 +2,6 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import Group
 from django.utils.translation import ugettext as _
 from reversion.admin import VersionAdmin
 
