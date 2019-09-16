d30806ed6a828e460e2b1cb70b9592fe9ec61e25
oleast <oleast@stud.ntnu.no>
2019-09-16 11:53:27 +0200

    Purge old payment api endpoints

--- apps/payment/urls.py
+++ apps/payment/urls.py
@@ -1,19 +1,8 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
-
 from apps.api.utils import SharedAPIRootRouter
 from apps.payment import views
 
-urlpatterns = [
-    url(r'^$', views.payment, name='payment'),
-    url(r'^payment_info/$', views.payment_info, name='payment_info'),
-    url(r'^refund/(?P<payment_relation_id>\d+)$', views.payment_refund, name='payment_refund'),
-    url(r'^saldo_info/$', views.saldo_info, name='saldo_info'),
-    url(r'^saldo/$', views.saldo, name='saldo'),
-    url(r'^webshop_info/$', views.webshop_info, name='webshop_info'),
-    url(r'^webshop_pay/$', views.webshop_pay, name='webshop_pay'),
-    url(r'^refund/(?P<payment_relation_id>\d+)$', views.payment_refund, name='payment_refund'),
-]
+urlpatterns = []
 
 # API v1
 router = SharedAPIRootRouter()
