807fcdaada782f7ae92a4c039353d31de726256d
MrHanSan <hans.sandbu@protonmail.com>
2019-09-25 19:58:54 +0200

    stared work on anonymous user objects

--- apps/lillebror/views.py
+++ apps/lillebror/views.py
@@ -1,10 +1,16 @@
 import logging
 
 from django.shortcuts import render
-from apps.authentication.models import OnlineUser as User
+from apps.authentication.models import OnlineUser
+from django.contrib.auth.models import User
 
 
 def all_user_data(request):
     logger = logging.getLogger(__name__)
-
-    return(render(request, None))
+    # user = User.objects.create_user(username='annonymousBird1234',
+    #                                 email='annon@ema.il',
+    #                                 password='thisisapassword')
+    for field in User._meta.get_fields():
+        logger.warning(field)
+        # user.field.type = get_internal_type()
+    return(render(request, "lillebror/index.html"))
