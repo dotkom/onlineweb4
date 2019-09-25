15899acb229e6194de61912c737e8fa4aca54c82
oleast <oleast@stud.ntnu.no>
2019-09-25 22:36:19 +0200

    Change docstrings to regular comments in payment signals

--- apps/payment/signals.py
+++ apps/payment/signals.py
@@ -8,7 +8,7 @@ from .models import PaymentRelation, PaymentTransaction
 @receiver(signal=pre_save, sender=PaymentRelation)
 def handle_payment_relation_status_change(sender, instance: PaymentRelation, **kwargs):
     if instance.status == status.SUCCEEDED:
-        """ Handle the completed payment. Remove delays, suspensions and marks """
+        # Handle the completed payment. Remove delays, suspensions and marks
         instance.payment.handle_payment(instance.user)
         instance.status = status.DONE
     elif instance.status == status.REFUNDED:
@@ -19,16 +19,16 @@ def handle_payment_relation_status_change(sender, instance: PaymentRelation, **k
 
 @receiver(signal=pre_save, sender=PaymentTransaction)
 def handle_payment_transaction_status_change(sender, instance: PaymentTransaction, **kwargs):
-    """ When a payment succeeds, ot should be stored to the DB """
+    # When a payment succeeds, ot should be stored to the DB
     if instance.status == status.SUCCEEDED:
         instance.user.change_saldo(instance.amount)
 
-        """ Pass the transaction to the next step, which is DONE """
+        # Pass the transaction to the next step, which is DONE
         instance.status = status.DONE
 
-    """ Handle when a transaction is being refunded by Stripe """
+    # Handle when a transaction is being refunded by Stripe
     if instance.status == status.REFUNDED:
         instance.user.change_saldo(-instance.amount)
 
-        """ Pass transaction to the next strip, which is REMOVED """
+        # Pass transaction to the next strip, which is REMOVED
         instance.status = status.REMOVED
