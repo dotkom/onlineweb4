5b93546b6c86ebd95de0436554b9bb3bb48bd474
oleast <oleast@stud.ntnu.no>
2019-09-28 12:20:13 +0200

    Update fiken attachment PDF with vat

--- apps/payment/pdf_generator.py
+++ apps/payment/pdf_generator.py
@@ -1,10 +1,16 @@
+import logging
 import uuid
 from tempfile import gettempdir
 
+from django.conf import settings
 from pdfdocument.utils import PDFDocument
 from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import Paragraph, TableStyle
+from reportlab.platypus import Image, Paragraph, TableStyle
+
+logger = logging.getLogger(__name__)
+
+ONLINE_LOGO_IMAGE = f'{settings.PROJECT_ROOT_DIRECTORY}/files/static/img/online_logo.png'
 
 
 class FikenSalePDF:
@@ -27,6 +33,25 @@ class FikenSalePDF:
             ))
 
     @property
+    def vat_table_data(self):
+        order_lines_by_vat = {}
+        for order_line in self.sale.order_lines.all():
+            if order_line.vat_percentage not in order_lines_by_vat:
+                order_lines_by_vat[order_line.vat_percentage] = []
+            order_lines_by_vat[order_line.vat_percentage].append(order_line)
+
+        vat_totals = []
+        for vat_percentage, order_lines in order_lines_by_vat.items():
+            total_for_vat = sum([line.price for line in order_lines])
+            vat_totals.append([
+                None,
+                f'{vat_percentage} %',
+                f'{total_for_vat / 100} kr',
+            ])
+
+        return vat_totals
+
+    @property
     def column_widths(self):
         return 245, 80, 145
 
@@ -35,8 +60,11 @@ class FikenSalePDF:
         pdf = PDFDocument(path)
         pdf.init_report()
 
+        online_logo = Image(filename=ONLINE_LOGO_IMAGE)
+        pdf.append(online_logo)
+
         pdf.p('Kvittering for kjøp på online.ntnu.no', style=create_paragraph_style(font_size=18))
-        pdf.spacer(10)
+        pdf.spacer(height=16)
         pdf.p(self.sale.created_date.strftime('%d. %B %Y'), create_paragraph_style(font_size=9))
         pdf.spacer(height=25)
 
@@ -49,6 +77,12 @@ class FikenSalePDF:
         pdf.spacer(height=20)
         pdf.table(self.table_data, self.column_widths, style=get_table_style())
 
+        pdf.spacer(height=20)
+        pdf.p('Moms', style=create_paragraph_style(font_size=14))
+        pdf.spacer(height=10)
+        vat_table_type = get_vat_table_style(line_color=colors.dimgrey)
+        pdf.table(self.vat_table_data, self.column_widths, style=vat_table_type)
+
         pdf.spacer(height=25)
         pdf.p('Total', style=create_paragraph_style(font_size=14))
         pdf.spacer(height=10)
@@ -60,11 +94,22 @@ class FikenSalePDF:
         return pdf_file
 
 
-# Table style for framed table with grids
-def get_table_style(full_spans=None):
+def get_vat_table_style(full_spans=None, line_color=colors.grey):
     style = [
-        ('GRID', (0, 0), (-1, -1), 0.5, colors.grey),
-        ('BOX', (0, 0), (-1, -1), 1, colors.black),
+        ('LINEBELOW', (0, 0), (-1, -1), 0.5, line_color),
+    ]
+    if full_spans:
+        for line in full_spans:
+            style.append(('SPAN', (0, line), (-1, line)))
+
+    return TableStyle(style)
+
+
+# Table style for framed table with grids
+def get_table_style(full_spans=None, border_color=colors.black, grid_color=colors.grey):
+    style = [
+        ('GRID', (0, 0), (-1, -1), 0.5, grid_color),
+        ('BOX', (0, 0), (-1, -1), 1, border_color),
     ]
     if full_spans:
         for line in full_spans:
