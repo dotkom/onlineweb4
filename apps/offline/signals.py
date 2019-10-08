52ad194c325affa73fac3aeb08b8274d93890351
oleast <oleast@stud.ntnu.no>
2019-10-08 19:25:09 +0200

    Refactor offline thumbnail generation to a celery task creating a responsive-image

--- apps/offline/signals.py
+++ apps/offline/signals.py
@@ -3,17 +3,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from apps.offline.models import Issue
-from apps.offline.tasks import create_thumbnail
+from apps.offline.tasks import create_thumbnail_task
 
 
 @receiver(post_save, sender=Issue)
-def trigger_create_thumbnail(sender, instance, created, **kwargs):
+def trigger_create_thumbnail(sender, instance: Issue, created=False, **kwargs):
     """
-    :param sender: The model that triggered this hook
-    :param instance: The model instance triggering this hook
-    :param created: True if the instance was created, False if the instance was updated
-
     Calls the create thumbnail task if an issue saved (either created or updated).
     """
-
-    create_thumbnail(instance)
+    if not instance.image:
+        create_thumbnail_task.delay(issue_id=instance.id)
