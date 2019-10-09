d35898e889564447de934b41751755c951dbb8bd
oleast <oleast@stud.ntnu.no>
2019-10-09 10:44:14 +0200

    Update offline tests for responsive thumbnails

--- apps/offline/tests.py
+++ apps/offline/tests.py
@@ -1,7 +1,5 @@
 import logging
 from datetime import datetime
-from os import remove
-from subprocess import CalledProcessError, check_call
 
 from django.test import TestCase
 from django.urls import reverse
@@ -21,24 +19,10 @@ class OfflineTest(TestCase):
 
     def setUp(self):
         self.logger = logging.getLogger(__name__)
-        self.issue = G(Issue, issue=IMAGE_FOLDER + '/offline-test-pdf.pdf')
+        self.issue: Issue = G(Issue, issue=IMAGE_FOLDER + '/offline-test-pdf.pdf', image=None)
 
-    def _runImagemagick(self):
-        try:
-            check_call(['which', 'convert'])
-            return True
-        except (OSError, CalledProcessError):
-            self.logger.error('Missing dependency imagemagick.')
-            return False
-
-    def testImagemagickExists(self):
-        self.assertTrue(self._runImagemagick())
-
-    def testThumbnailExists(self):
-        self.assertTrue(self.issue.thumbnail_exists)
-
-    def tearDown(self):
-        remove(self.issue.thumbnail)
+    def test_thumbnail_exists(self):
+        self.assertTrue(self.issue.image)
 
 
 class OfflineURLTestCase(TestCase):
