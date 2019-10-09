8caee4f3719d53e4e60ed8117a57dbd18f2cf41c
oleast <oleast@stud.ntnu.no>
2019-10-09 21:13:44 +0200

    Create API and admin for mark rules

--- apps/marks/views.py
+++ apps/marks/views.py
@@ -1,7 +1,9 @@
 from rest_framework import mixins, permissions, viewsets
 
-from apps.marks.models import MarkUser, Suspension
-from apps.marks.serializers import MarkUserSerializer, SuspensionSerializer
+from apps.marks.models import MarkRuleSet, MarkUser, RuleAcceptance, Suspension
+from apps.marks.serializers import (MarkRuleSetReadOnlySerializer, MarkUserSerializer,
+                                    RuleAcceptanceCreateSerializer,
+                                    RuleAcceptanceReadOnlySerializer, SuspensionSerializer)
 
 
 class MarksViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -20,3 +22,28 @@ class SuspensionViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
 
     def get_queryset(self):
         return Suspension.objects.filter(user=self.request.user)
+
+
+class MarkRuleSetViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = MarkRuleSetReadOnlySerializer
+    permission_classes = (permissions.AllowAny,)
+    queryset = MarkRuleSet.objects.all()
+
+
+class RuleAccpetanceViewSet(viewsets.GenericViewSet,
+                            mixins.CreateModelMixin,
+                            mixins.ListModelMixin,
+                            mixins.RetrieveModelMixin):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = RuleAcceptance.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action in ['retrieve', 'list']:
+            return RuleAcceptanceReadOnlySerializer
+        if self.action == 'create':
+            return RuleAcceptanceCreateSerializer
+
+        return super().get_serializer_class()
