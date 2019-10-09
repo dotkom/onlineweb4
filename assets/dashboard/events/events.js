9d39db877cd436e11f6a62a60fb3e762f223f132
Tobias Kongsvik <tokongs98@gmail.com>
2019-10-09 21:14:53 +0200

    Fix table that is generated when adding or removing attendees from event

--- assets/dashboard/events/events.js
+++ assets/dashboard/events/events.js
@@ -1,6 +1,6 @@
-import jQuery from "jquery";
-import { plainUserTypeahead } from "common/typeahead";
-import { ajax, showStatusMessage, toggleChecked } from "common/utils";
+import jQuery from 'jquery';
+import { plainUserTypeahead } from 'common/typeahead';
+import { ajax, showStatusMessage, toggleChecked } from 'common/utils';
 
 /*
   The event module provides dynamic functions to event objects
@@ -9,221 +9,223 @@ import { ajax, showStatusMessage, toggleChecked } from "common/utils";
 
 const Event = (function PrivateEvent($) {
     // Get the currently correct endpoint for posting user changes to
-    const posOfLastSlash = window.location.href
+  const posOfLastSlash = window.location.href
         .toString()
         .slice(0, window.location.href.toString().length)
-        .lastIndexOf("/");
-    const attendanceEndpoint = `${window.location.href
+        .lastIndexOf('/');
+  const attendanceEndpoint = `${window.location.href
         .toString()
         .slice(0, posOfLastSlash)}/attendees/`;
 
     // Javascript to enable link to tab
-    const url = document.location.toString();
-    if (url.match("#")) {
-        $(`.nav-tabs a[href="#${url.split("#")[1]}"]`).tab("show");
-    }
+  const url = document.location.toString();
+  if (url.match('#')) {
+    $(`.nav-tabs a[href="#${url.split('#')[1]}"]`).tab('show');
+  }
 
     // Change hash for page-reload
-    $(".nav-tabs a").on("shown.bs.tab", e => {
-        if (history.pushState) {
-            history.pushState(null, "", e.target.hash);
-        } else {
-            window.location.hash = e.target.hash; // Polyfill for old browsers
-        }
-    });
+  $('.nav-tabs a').on('shown.bs.tab', (e) => {
+    if (history.pushState) {
+      history.pushState(null, '', e.target.hash);
+    } else {
+      window.location.hash = e.target.hash; // Polyfill for old browsers
+    }
+  });
 
-    const attendeeRow = (attendee, isPaymentEvent) => {
-        let row = "<tr>";
-        row += `<td>${attendee.number}</td>`;
-        row += `<td><a href="${attendee.link}">${attendee.first_name}</td>`;
-        row += `<td><a href="${attendee.link}">${attendee.last_name}</td>`;
-        row += `<td>${attendee.year_of_study}</td>`;
+  const attendeeRow = (attendee, isPaymentEvent) => {
+    let row = '<tr>';
+    row += `<td>${attendee.number}</td>`;
+    row += `<td><a href="${attendee.link}">${attendee.first_name}</td>`;
+    row += `<td><a href="${attendee.link}">${attendee.last_name}</td>`;
+    row += `<td>${attendee.year_of_study}</td>`;
 
         // Paid cell
-        if (isPaymentEvent) {
-            row += `<td><a href="#" data-id="${attendee.id}" class="toggle-attendee paid">`;
-            if (attendee.paid) {
-                row += '<i class="fa fa-lg fa-check-square-o checked"></i>';
-            } else {
-                row += '<i class="fa fa-lg fa-square-o"></i>';
-            }
-        }
-        row += "</a></td>";
+    if (isPaymentEvent) {
+      row += `<td><a href="#" data-id="${attendee.id}" class="toggle-attendee paid">`;
+      if (attendee.paid) {
+        row += '<i class="fa fa-lg fa-check-square-o checked"></i>';
+      } else {
+        row += '<i class="fa fa-lg fa-square-o"></i>';
+      }
+    }
+    row += '</a> Betalt</td>';
         // Attended cell
-        row += `<td><a href="#" data-id="${attendee.id}" class="toggle-attendee attended">`;
-        if (attendee.attended) {
-            row += '<i class="fa fa-lg fa-check-square-o checked"></i>';
-        } else {
-            row += '<i class="fa fa-lg fa-square-o"></i>';
-        }
-        row += "</a></td>";
-        // Extras cell
-        row += `<td>${attendee.extras}</td>`;
-        // Delete cell
- row += `<td><a href="#modal-delete-attendee" data-toggle="modal" data-id="${attendee.id}" data-name="${attendee.first_name} ${attendee.last_name}" class="remove-user">`;
-        row += '<i class="fa fa-times fa-lg pull-right red"></i>';
-        row += "</a></td>";
-        // Close row
-        row += "</tr>";
-        return row;
-    };
+    row += `<td><a href="#" data-id="${attendee.id}" class="toggle-attendee attended">`;
 
-    const buttonBinds = () => {
+    if (attendee.attended) {
+      row += '<i class="fa fa-lg fa-check-square-o checked"></i>';
+    } else {
+      row += '<i class="fa fa-lg fa-square-o"></i>';
+    }
+    row += '</a></td>';
+
+        // Extras cell
+    row += `<td>${attendee.extras}</td>`;
+        // Delete cell
+    row += `<td><a href="#modal-delete-attendee" data-toggle="modal" data-id="${attendee.id}" data-name="${attendee.first_name} ${attendee.last_name}" class="remove-user">`;
+    row += '<i class="fa fa-times fa-lg pull-right red"></i>';
+    row += '</a></td>';
+        // Close row
+    row += '</tr>';
+    return row;
+  };
+
+  const buttonBinds = () => {
         // Bind remove user buttons
-        $(".remove-user").each(function removeUser() {
-            $(this).on("click", function removeUserClick(e) {
-                e.preventDefault();
-                $(".modal-remove-user-name").text($(this).data("name"));
-                $(".confirm-remove-user").data("id", $(this).data("id"));
-            });
-        });
+    $('.remove-user').each(function removeUser() {
+      $(this).on('click', function removeUserClick(e) {
+        e.preventDefault();
+        $('.modal-remove-user-name').text($(this).data('name'));
+        $('.confirm-remove-user').data('id', $(this).data('id'));
+      });
+    });
 
         // Toggle paid and attended
-        $(".toggle-attendee").each(function toggleAttendee() {
-            $(this).on("click", function toggleAttendeeClick() {
-                if ($(this).hasClass("attended")) {
-                    Event.attendee.toggle(this, "attended");
-                }
-                if ($(this).hasClass("paid")) {
-                    Event.attendee.toggle(this, "paid");
-                }
-            });
-        });
+    $('.toggle-attendee').each(function toggleAttendee() {
+      $(this).on('click', function toggleAttendeeClick() {
+        if ($(this).hasClass('attended')) {
+          Event.attendee.toggle(this, 'attended');
+        }
+        if ($(this).hasClass('paid')) {
+          Event.attendee.toggle(this, 'paid');
+        }
+      });
+    });
 
         // Refresh tablesorter
-        $("#attendees-table").trigger("update");
-        $("#waitlist-table").trigger("update");
-    };
+    $('#attendees-table').trigger('update');
+    $('#waitlist-table').trigger('update');
+  };
 
-    const drawTable = (tbody, data, isPaymentEvent) => {
+  const drawTable = (tbody, data, isPaymentEvent) => {
         // Redraw the table with new data
-        let tbodyHtml = "";
-        $.each(data, (i, attendee) => {
-            tbodyHtml += attendeeRow(attendee, isPaymentEvent);
-        });
-        $(`#${tbody}`).html(tbodyHtml);
+    let tbodyHtml = '';
+    $.each(data, (i, attendee) => {
+      tbodyHtml += attendeeRow(attendee, isPaymentEvent);
+    });
+    $(`#${tbody}`).html(tbodyHtml);
 
         // Bind all the buttons
-        buttonBinds();
-    };
+    buttonBinds();
+  };
 
-    return {
+  return {
         // Bind them buttons here
-        init() {
-            $("#upcoming_events_list").tablesorter();
-            $("#attendees-table").tablesorter();
-            $("#waitlist-table").tablesorter();
-            $("#extras-table").tablesorter();
+    init() {
+      $('#upcoming_events_list').tablesorter();
+      $('#attendees-table').tablesorter();
+      $('#waitlist-table').tablesorter();
+      $('#extras-table').tablesorter();
 
             // Bind add users button
-            $("#event_users_button").on("click", e => {
-                e.preventDefault();
-                $("#event_edit_users").slideToggle(200);
-                $("#usersearch").focus();
-            });
+      $('#event_users_button').on('click', (e) => {
+        e.preventDefault();
+        $('#event_edit_users').slideToggle(200);
+        $('#usersearch').focus();
+      });
 
             // Bind toggle paid/attended and remove use button
-            buttonBinds();
+      buttonBinds();
 
             // Bind the modal button only once
-            $(".confirm-remove-user").on("click", function confirmRemoveUser() {
-                Event.attendee.remove($(this).data("id"));
-            });
+      $('.confirm-remove-user').on('click', function confirmRemoveUser() {
+        Event.attendee.remove($(this).data('id'));
+      });
 
             /* Typeahead for user search */
-            plainUserTypeahead($("#usersearch"), (e, datum) => {
-                $(() => {
-                    Event.attendee.add(datum.id);
-                    $("#usersearch").val("");
-                });
-            });
-        },
+      plainUserTypeahead($('#usersearch'), (e, datum) => {
+        $(() => {
+          Event.attendee.add(datum.id);
+          $('#usersearch').val('');
+        });
+      });
+    },
 
         // Attendee module, toggles and adding
-        attendee: {
-            toggle(cell, action) {
-                const data = {
-                    attendee_id: $(cell).data("id"),
-                    action
-                };
-                const success = () => {
+    attendee: {
+      toggle(cell, action) {
+        const data = {
+          attendee_id: $(cell).data('id'),
+          action,
+        };
+        const success = () => {
                     // var line = $('#' + attendee_id > i)
-                    toggleChecked(cell);
-                };
-                const error = (xhr, txt, errorMessage) => {
-                    showStatusMessage(errorMessage, "alert-danger");
-                };
+          toggleChecked(cell);
+        };
+        const error = (xhr, txt, errorMessage) => {
+          showStatusMessage(errorMessage, 'alert-danger');
+        };
 
-                ajax("POST", attendanceEndpoint, data, success, error, null);
-            },
-            add(userId) {
-                const data = {
-                    user_id: userId,
-                    action: "add_attendee"
-                };
-                const success = eventData => {
-                    $("#attendees-count").text(eventData.attendees.length);
-                    $("#waitlist-count").text(eventData.waitlist.length);
+        ajax('POST', attendanceEndpoint, data, success, error, null);
+      },
+      add(userId) {
+        const data = {
+          user_id: userId,
+          action: 'add_attendee',
+        };
+        const success = (eventData) => {
+          $('#attendees-count').text(eventData.attendees.length);
+          $('#waitlist-count').text(eventData.waitlist.length);
                     // If this was the first attendee to be added we need to show the table
-                    if (eventData.attendees.length === 1) {
-                        $("#no-attendees-content").hide();
-                        $("#attendees-content").show();
-                    }
+          if (eventData.attendees.length === 1) {
+            $('#no-attendees-content').hide();
+            $('#attendees-content').show();
+          }
                     // We have to redraw this table in any case
-                    drawTable("attendeelist", eventData.attendees, eventData.is_payment_event);
+          drawTable('attendeelist', eventData.attendees, eventData.is_payment_event);
                     // Waitlist only needs to be considered if it actually has content
-                    if (eventData.waitlist.length > 0) {
+          if (eventData.waitlist.length > 0) {
                         // If this was the first, etc
-                        if (eventData.waitlist.length === 1) {
-                            $("#no-waitlist-content").hide();
-                            $("#waitlist-content").show();
-                        }
-                        drawTable("waitlist", eventData.waitlist, eventData.is_payment_event);
-                    }
-                    showStatusMessage(eventData.message, "alert-success");
-                };
-                const error = xhr => {
-                    showStatusMessage(xhr.responseText, "alert-danger");
-                };
+            if (eventData.waitlist.length === 1) {
+              $('#no-waitlist-content').hide();
+              $('#waitlist-content').show();
+            }
+            drawTable('waitlist', eventData.waitlist, eventData.is_payment_event);
+          }
+          showStatusMessage(eventData.message, 'alert-success');
+        };
+        const error = (xhr) => {
+          showStatusMessage(xhr.responseText, 'alert-danger');
+        };
 
-                ajax("POST", attendanceEndpoint, data, success, error, null);
-            },
-            remove(attendeeId) {
-                const data = {
-                    attendee_id: attendeeId,
-                    action: "remove_attendee"
-                };
-                const success = eventData => {
-                    $("#attendees-count").text(eventData.attendees.length);
-                    $("#waitlist-count").text(eventData.waitlist.length);
+        ajax('POST', attendanceEndpoint, data, success, error, null);
+      },
+      remove(attendeeId) {
+        const data = {
+          attendee_id: attendeeId,
+          action: 'remove_attendee',
+        };
+        const success = (eventData) => {
+          $('#attendees-count').text(eventData.attendees.length);
+          $('#waitlist-count').text(eventData.waitlist.length);
                     // If there are no attendees left, hide the table
-                    if (eventData.attendees.length === 0) {
-                        $("#no-attendees-content").show();
-                        $("#attendees-content").hide();
-                    } else {
+          if (eventData.attendees.length === 0) {
+            $('#no-attendees-content').show();
+            $('#attendees-content').hide();
+          } else {
                         // Only draw table if there are attendees to add to it
-                        drawTable("attendeelist", eventData.attendees);
-                    }
+            drawTable('attendeelist', eventData.attendees, eventData.is_payment_event);
+          }
                     // Hide waitlist if noone is in it.
                     // Hard to do any checks to prevent doing this every time.
-                    if (eventData.waitlist.length === 0) {
-                        $("#no-waitlist-content").show();
-                        $("#waitlist-content").hide();
-                    } else {
-                        drawTable("waitlist", eventData.waitlist);
-                    }
-                    showStatusMessage(eventData.message, "alert-success");
-                };
-                const error = xhr => {
-                    showStatusMessage(xhr.responseText, "alert-danger");
-                };
+          if (eventData.waitlist.length === 0) {
+            $('#no-waitlist-content').show();
+            $('#waitlist-content').hide();
+          } else {
+            drawTable('waitlist', eventData.waitlist, eventData.is_payment_event);
+          }
+          showStatusMessage(eventData.message, 'alert-success');
+        };
+        const error = (xhr) => {
+          showStatusMessage(xhr.responseText, 'alert-danger');
+        };
 
-                ajax("POST", attendanceEndpoint, data, success, error, null);
-            }
-        }
-    };
-})(jQuery);
+        ajax('POST', attendanceEndpoint, data, success, error, null);
+      },
+    },
+  };
+}(jQuery));
 
 jQuery(document).ready(() => {
-    Event.init();
+  Event.init();
 });
