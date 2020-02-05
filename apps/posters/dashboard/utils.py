e0c3e61191ea93e3119e1f5205fa9a8e4c5a085f
Johannes Kvamme <22146437+FluidSense@users.noreply.github.com>
2020-02-05 19:56:52 +0100

    Add admin_group for posters
    Add admin_group (currently "prokom", just avoid having the name in the code) and give them permissions to view individual poster orders.

--- apps/posters/dashboard/utils.py
+++ apps/posters/dashboard/utils.py
@@ -2,7 +2,7 @@ import logging
 
 from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ImproperlyConfigured
 from django.core.mail import EmailMessage
@@ -27,9 +27,13 @@ def _handle_poster_add(request, form, order_type):
 
     poster.save()
     ordered_committee = form.cleaned_data["ordered_committee"]
+    poster_admin_group = get_poster_admin_group()
 
     # Let this user have permissions to show this order
     UserObjectPermission.objects.assign_perm("view_poster_order", request.user, poster)
+    GroupObjectPermission.objects.assign_perm(
+        "view_poster_order", poster_admin_group, poster
+    )
     GroupObjectPermission.objects.assign_perm(
         "view_poster_order", ordered_committee, poster
     )
@@ -90,3 +94,10 @@ def get_poster_admins():
         Q(groups__permissions=change_order_perm) | Q(user_permissions=change_order_perm)
     ).distinct()
     return users
+
+def get_poster_admin_group():
+    content_type = ContentType.objects.get_for_model(Poster)
+    all_permissions = Permission.objects.filter(content_type=content_type)
+    change_order_perm = all_permissions.filter(codename="change_poster").first()
+    admin_group = Group.objects.filter(permissions=change_order_perm).distinct()
+    return admin_group
