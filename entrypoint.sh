cde8866e37a0f35e967775631d62e4225e946a41
Tobias Kongsvik <tokongs98@gmail.com>
2020-04-01 12:12:00 +0200

    Fix config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python manage.py migrate --noinput
-uwsgi --ini uwsgi.ini --wsgi-file ./onlineweb4/wsgi.py
+uwsgi --ini uwsgi.ini --wsgi-file ./onlineweb4/wsgi.py --socket 0.0.0.0:8000
