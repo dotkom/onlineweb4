f204b2299e24da4734ab11afb0489002b943d9c9
Henrik Hørlück Berg <henrik@horluck.no>
2020-04-05 15:22:59 +0200

    Post_init -> post_save with created

--- apps/mailinglists/signals.py
+++ apps/mailinglists/signals.py
@@ -1,14 +1,17 @@
-from django.db.models.signals import post_init
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Mail, Mailinglist
 
 
-@receiver(signal=post_init, sender=Mailinglist)
-def create_organization_email_for_list(sender, instance: Mailinglist, **kwargs):
-    Mail.objects.create(
-        email=instance.email,
-        name=instance.name,
-        description=instance.description,
-        public=instance.public,
-    )
+@receiver(signal=post_save, sender=Mailinglist)
+def create_organization_email_for_list(
+    sender, instance: Mailinglist, created: bool, **kwargs
+):
+    if created:
+        Mail.objects.create(
+            email=instance.email,
+            name=instance.name,
+            description=instance.description,
+            public=instance.public,
+        )
