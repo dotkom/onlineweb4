214e89fe0505303c4bd41fc80ab4f407fa5c9c59
Henrik Hørlück Berg <henrik@horluck.no>
2020-05-03 15:12:39 +0200

    Add support for filtering on domain, and other text fields

--- apps/mailinglists/filters.py
+++ apps/mailinglists/filters.py
@@ -3,6 +3,8 @@ import logging
 from django_filters import filters, filterset
 from watson import search as watson_search
 
+from .models import MailGroup
+
 logger = logging.getLogger(__name__)
 
 
@@ -14,5 +16,8 @@ class WatsonFilter(filters.CharFilter):
 
 
 class MailGroupFilter(filterset.FilterSet):
-    public = filters.BooleanFilter(field_name="public")
     query = WatsonFilter()
+
+    class Meta:
+        model = MailGroup
+        fields = ("domain",)
