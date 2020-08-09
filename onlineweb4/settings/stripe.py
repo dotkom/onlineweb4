245b49e167b490acd7f8d1699b50d21ec191167c
Henrik Hørlück Berg <henrik@horluck.no>
2020-08-09 22:37:48 +0200

    Don't version API since it also breaks tests

--- onlineweb4/settings/stripe.py
+++ onlineweb4/settings/stripe.py
@@ -1,5 +1,4 @@
 from decouple import config
-import stripe
 
 # Online stripe keys.
 # For development replace with https://online.ntnu.no/wiki/komiteer/dotkom/aktuelt/onlineweb4/keys/
@@ -27,5 +26,3 @@ STRIPE_PRIVATE_KEYS = {
         "OW4_DJANGO_STRIPE_PRIVATE_KEY_TRIKOM", default="pk_test_replace_this"
     ),
 }
-
-stripe.api_version = config("OW4_DJANGO_STRIPE_API_VERSION", default="2020-03-02")
