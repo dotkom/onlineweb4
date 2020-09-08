7c0963eb6e5a2eb2fcceb54627568126df537681
Henrik Hørlück Berg <henrik@horluck.no>
2020-09-08 16:29:36 +0200

    Ensure user is authed when viewing api-users
    - Previously `/api/v1/users` was available without authentication,
    giving the usernames, firstname, lastname, and ID of every single user.
    This fixes that.

--- apps/authentication/api/permissions.py
+++ apps/authentication/api/permissions.py
@@ -1,7 +1,7 @@
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 
 
-class IsSelfOrSuperUser(permissions.BasePermission):
+class IsSelfOrSuperUser(IsAuthenticated):
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and (
             request.user.is_superuser or request.user == obj
