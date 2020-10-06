e19b513736c2cc8193eab1ba611dab58bd83881c
Aslak Hollund <aslakhol@gmail.com>
2020-10-06 19:26:32 +0200

    lint the migration file, because that's a totaly reasonable thing to do

--- apps/careeropportunity/migrations/0016_careeropportunity_application_email.py
+++ apps/careeropportunity/migrations/0016_careeropportunity_application_email.py
@@ -6,13 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('careeropportunity', '0015_auto_20200426_1109'),
+        ("careeropportunity", "0015_auto_20200426_1109"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='careeropportunity',
-            name='application_email',
-            field=models.EmailField(blank=True, max_length=254, null=True, verbose_name='søknadsepost'),
+            model_name="careeropportunity",
+            name="application_email",
+            field=models.EmailField(
+                blank=True, max_length=254, null=True, verbose_name="søknadsepost"
+            ),
         ),
     ]
