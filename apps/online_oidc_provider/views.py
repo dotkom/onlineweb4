2cae0bb88e50c06e9f12d7a0094946788f61e9be
FluidSense <22146437+FluidSense@users.noreply.github.com>
2020-10-18 22:35:03 +0200

    Add endpoint for viewing all info about your own clients

--- apps/online_oidc_provider/views.py
+++ apps/online_oidc_provider/views.py
@@ -1,13 +1,15 @@
 from oidc_provider.models import Client, ResponseType, UserConsent
-from rest_framework import mixins, permissions, viewsets
-
+from rest_framework import mixins, permissions, viewsets, status
+from rest_framework.decorators import action
 from apps.common.rest_framework.mixins import MultiSerializerMixin
+from rest_framework.response import Response
 
 from .serializers import (
     ClientCreateAndUpdateSerializer,
     ClientReadOnlySerializer,
     ResponseTypeReadOnlySerializer,
     UserConsentReadOnlySerializer,
+    ClientReadOwnSerializer
 )
 
 
@@ -47,6 +49,32 @@ class ClientViewSet(MultiSerializerMixin, viewsets.ModelViewSet):
             return Client.objects.filter(owner=user)
 
         return super().get_queryset()
+
+    @action(
+        detail=False,
+        methods=["POST"],
+        permission_classes=[permissions.IsAuthenticated],
+        serializer_class=ClientReadOwnSerializer,
+        url_path="get-own"
+    )
+    def get_own(self, request):
+        """
+        This methods uses POST since HTTPS POST encrypts the transmission
+        and this endpoints transmits sensitive data.
+        """
+        """
+        if not request.is_secure():
+            return Response(data={"detail": "This endpoint requires HTTPS"}, status=status.HTTP_400_BAD_REQUEST)
+        """
+        user = self.request.user
+        clients = Client.objects.filter(owner=user).order_by("id")
+        page = self.paginate_queryset(clients)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(clients, many=True)
+        return Response(serializer.data)
 
 
 class ResponseTypeViewSet(viewsets.ReadOnlyModelViewSet):
