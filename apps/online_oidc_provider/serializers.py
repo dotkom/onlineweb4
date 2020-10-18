01474e3190f96ec40bfbd18b1a663a1597405841
FluidSense <22146437+FluidSense@users.noreply.github.com>
2020-10-18 22:33:09 +0200

    Add serializer for showing your own clients

--- apps/online_oidc_provider/serializers.py
+++ apps/online_oidc_provider/serializers.py
@@ -1,7 +1,16 @@
 from oidc_provider.models import Client, ResponseType, UserConsent
 from rest_framework import serializers
-
+from hashlib import sha224
+from uuid import uuid4
+from random import randint
 from apps.authentication.serializers import UserNameSerializer
+
+
+class ResponseTypeReadOnlySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ResponseType
+        read_only = True
+        fields = ("id", "value", "description")
 
 
 class UserConsentReadOnlySerializer(serializers.ModelSerializer):
@@ -9,6 +18,32 @@ class UserConsentReadOnlySerializer(serializers.ModelSerializer):
         model = UserConsent
         read_only = True
         fields = ("id", "date_given", "expires_at", "client", "scope", "has_expired")
+
+
+class ClientReadOwnSerializer(serializers.ModelSerializer):
+    response_types = ResponseTypeReadOnlySerializer(many=True)
+
+    class Meta:
+        model = Client
+        ordering = ["-id"]
+        fields = (
+            "id",
+            "name",
+            "owner",
+            "date_created",
+            "website_url",
+            "terms_url",
+            "contact_email",
+            "logo",
+            "require_consent",
+            "reuse_consent",
+            "scope",
+            "response_types",
+            "client_secret",
+            "client_id",
+            "client_type",
+            "redirect_uris"
+        )
 
 
 class ClientCreateAndUpdateSerializer(serializers.ModelSerializer):
@@ -57,10 +92,3 @@ class ClientReadOnlySerializer(serializers.ModelSerializer):
             "client_id",
             "client_type",
         )
-
-
-class ResponseTypeReadOnlySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ResponseType
-        read_only = True
-        fields = ("id", "value", "description")
