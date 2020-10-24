cb94bbe61fbc3e386ea0a9cba7688d39fd38731c
kharann <anhkhav@gmail.com>
2020-10-24 19:14:18 +0200

    Lint fix

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -13,8 +13,7 @@ class Privacy(models.Model):
     )
     expose_nickname = models.BooleanField(_("vis kallenavn"), default=True)
     expose_email = models.BooleanField(_("vis epost"), default=True)
-    expose_phone_number = models.BooleanField(
-        _("vis telefonnummer"), default=True)
+    expose_phone_number = models.BooleanField(_("vis telefonnummer"), default=True)
     expose_address = models.BooleanField(_("vis addresse"), default=True)
     visible_as_attending_events = models.BooleanField(
         _("vis på påmeldingsarrangement"), default=None, null=True
@@ -23,8 +22,7 @@ class Privacy(models.Model):
         _("greit å ta bilder på arrangement"), default=None, null=True
     )
 
-    user = models.OneToOneField(
-        User, related_name="privacy", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name="privacy", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.get_full_name()
