80f873611536cf8585f4f8f12e3a6a60477eec5d
Aslak Hollund <aslakhol@gmail.com>
2021-03-21 23:09:31 +0100

    add field to make it possible to add an ASAP deadline to career opportunities

--- apps/careeropportunity/forms.py
+++ apps/careeropportunity/forms.py
@@ -53,6 +53,10 @@ class AddCareerOpportunityForm(forms.ModelForm):
         required=False,
         widget=forms.TextInput(attrs={"placeholder": "Velg søknadsfrist"}),
     )
+    deadline_asap = forms.BooleanField(
+        label="Frist er snarest",
+        required=False,
+    )
 
     class Meta:
         model = CareerOpportunity
@@ -67,6 +71,7 @@ class AddCareerOpportunityForm(forms.ModelForm):
             "end",
             "featured",
             "deadline",
+            "deadline_asap",
             "employment",
             "location",
         )
