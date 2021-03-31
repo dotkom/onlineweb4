621b5433312d0f4807952c3f36cb90b0c44cbef7
FluidSense <22146437+FluidSense@users.noreply.github.com>
2021-03-31 19:48:50 +0200

    Set openid scope claims if included

--- apps/sso/settings.py
+++ apps/sso/settings.py
@@ -6,9 +6,14 @@
 # SSO / OAuth2 settings
 OAUTH2_SCOPES = {
     "null": "Ingen datatilgang",
-    "openid": "OpenID Connect Scope",
+    "openid": "OpenID Connect Scope - Autentisering av hvilken bruker du er.",
     "read": "DRF Read",
     "write": "DRF Write",
+    "profile": "Informasjon vedrørende din profil",
+    "address": "Din registrerte adresse.",
+    "email": "Din registrerte e-post.",
+    "phone": "Ditt registrerte telefonnummer.",
+    "online": "Informasjon vedrørende ditt medlemskap i Linjeforeningen Online.",
     "authentication.onlineuser.username.read": "Brukernavn (Lesetilgang)",
     "authentication.onlineuser.first_name.read": "Fornavn (Lesetilgang)",
     "authentication.onlineuser.last_name.read": "Etternavn (Lesetilgang)",
