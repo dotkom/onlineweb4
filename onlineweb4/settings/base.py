6e8d6667e325142f445a76de6115a4cc2815bbb4
FluidSense <johanneskv96@gmail.com>
2021-04-26 22:46:11 +0200

    lint

--- onlineweb4/settings/base.py
+++ onlineweb4/settings/base.py
@@ -65,7 +65,7 @@ SLACK_INVITER = {
 
 # SSO / OAuth2 settings
 
-OIDC_RSA_PRIVATE_KEY = "" # Default case
+OIDC_RSA_PRIVATE_KEY = ""  # Default case
 if not os.path.isfile("oidc.key"):
     print("Failed to locate OIDC RSA Private key file during setup.")
 else:
