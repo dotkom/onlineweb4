c4752f86bf0738e837b14c66c57cae2fe09647b1
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2021-08-29 15:06:28 +0200

    Purge articles

--- assets/frontpage/index.js
+++ assets/frontpage/index.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import ArticlesContainer from './articles/containers/ArticlesContainer';
 import EventsContainer from './events/containers/EventsContainer';
 import './initFrontpage';
 import './less/frontpage.less';
-
-const renderArticles = (Articles) => {
-  ReactDom.render(
-    <AppContainer>
-      <Articles />
-    </AppContainer>,
-    document.getElementById('article-items'),
-  );
-};
-
-renderArticles(ArticlesContainer);
-
-if (module.hot) {
-  module.hot.accept('./articles/containers/ArticlesContainer', () => {
-    renderArticles(ArticlesContainer);
-  });
-}
 
 const renderEvents = (Events) => {
   ReactDom.render(
