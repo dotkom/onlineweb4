7491875e1b38ac99fce9d181c5d5a9a0465aa648
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2022-03-21 00:19:16 +0100

    Fix more deprecation warnings and unclosed file-issue

--- apps/offline/utils.py
+++ apps/offline/utils.py
@@ -11,10 +11,10 @@ def pdf_page_to_png(pdf: FieldFile, page_number=0):
     """
     source_pdf = fitz.open(pdf.path)
     first_page: fitz.Page = source_pdf[page_number]
-    first_page_image = first_page.getPixmap(alpha=False, matrix=fitz.Matrix(4.0, 4.0))
+    first_page_image = first_page.get_pixmap(alpha=False, matrix=fitz.Matrix(4.0, 4.0))
 
     temp_image_path = f"{gettempdir()}/{uuid.uuid1()}.png"
-    first_page_image.writePNG(temp_image_path)
+    first_page_image.save(temp_image_path)
     image_file = open(temp_image_path, "rb")
 
     return image_file
