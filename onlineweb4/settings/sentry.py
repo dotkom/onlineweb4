9e9c8ec0a3f68aa090c40ee3ef1f28d749e7f878
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2022-03-31 01:49:14 +0200

    Fix Sentry config, make it not repeat itself
    - Revert 372e49754609a97aecf602dcc1061fef23818657 to make Sentry
    required again. The current setup was crashing due to a typo in the
    prod-extras (`sentry` instead of `sentry-sdk`), meaning Sentry was never
    installed, but you would'nt notice until you actually start to run.
    - Add `python manage.py check` to CI to ensure this does not happen
    again.
    - Use documented `SENTRY_RELEASE` env-variable instead of a custom one,
    mostly due to it having sane defaults normally, and that also crashed
    upon startup.
    
    Co-authored-by: Henrik Skog <henrikskog@users.noreply.github.com>

--- onlineweb4/settings/sentry.py
+++ onlineweb4/settings/sentry.py
@@ -1,19 +1,17 @@
+import sentry_sdk
 from decouple import config
+from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
+from sentry_sdk.integrations.django import DjangoIntegration
 
 from .django import DEBUG
 
 OW4_SENTRY_DSN = config("OW4_SENTRY_DSN", default="")
 
-if OW4_SENTRY_DSN:
-    import sentry_sdk
-    from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
-    from sentry_sdk.integrations.django import DjangoIntegration
 
-    sentry_sdk.init(
-        dsn=OW4_SENTRY_DSN,
-        environment=config("OW4_ENVIRONMENT", default="DEVELOP"),
-        debug=DEBUG,
-        release=config("OW4_VERSION"),
-        traces_sample_rate=0.2,
-        integrations=[DjangoIntegration(), AwsLambdaIntegration(timeout_warning=True)],
-    )
+sentry_sdk.init(
+    dsn=OW4_SENTRY_DSN,
+    environment=config("OW4_ENVIRONMENT", default="DEVELOP"),
+    debug=DEBUG,
+    traces_sample_rate=0.2,
+    integrations=[DjangoIntegration(), AwsLambdaIntegration(timeout_warning=True)],
+)
