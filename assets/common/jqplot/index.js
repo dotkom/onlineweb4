722e6efb7ec351d63343c9227dda7b20390fa969
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2023-10-19 14:10:19 +0200

    Replace jqplot with echarts
    Easier to replace than fix imports. Is pretty enough, but may be some
    layout-issues.

--- assets/common/jqplot/index.js
+++
@@ -1,6 +0,0 @@
-import './jquery.jqplot.js';
-import './plugins/jqplot.pieRenderer.js';
-import './plugins/jqplot.barRenderer.js';
-import './plugins/jqplot.categoryAxisRenderer.js';
-import './plugins/jqplot.pointLabels.js';
-import './css/jquery.jqplot.css';
