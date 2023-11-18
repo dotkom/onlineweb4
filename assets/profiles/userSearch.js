fdf87b9421d802b63760ad840af17e8639fdb892
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2023-11-18 18:55:24 +0100

    Remove scrollspy-ing
    There is a scrollspy function in initFrontpage.js, it appears to not be
    related? User-search is likely to get removed anyways.

--- assets/profiles/userSearch.js
+++ assets/profiles/userSearch.js
@@ -26,9 +26,5 @@ export default () => {
   $(window).load(() => {
     $('.affix-spinner-wrapper').remove();
     $('.affix').show();
-    $('body').scrollspy({
-      target: '#affix',
-      offset: 90,
-    });
   });
 };
