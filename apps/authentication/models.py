f62d37053d803fc189e53f3ee01b58e74c9efab3
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2024-01-25 16:40:59 +0100

    Add Cognito export and just-in-time pwd-setting
    Co-Authored-By: Henrik Skog <henrikskog01@gmail.com>
    Co-Authored-By: Mats Jun <mats@jun.codes>

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -156,6 +156,37 @@ class OnlineUser(AbstractUser):
         _("NTNU-brukernavn"), max_length=50, blank=True, null=True, unique=True
     )
 
+    cognito_subject = models.UUIDField(
+        _("Cognito User Id"), blank=True, null=True, unique=True
+    )
+
+    def set_password(self, raw_password):
+        super().set_password(raw_password)
+        if (sub := self.cognito_subject) and settings.COGNITO_USER_POOL_ID:
+            import boto3
+
+            client = boto3.client("cognito-idp")
+            client.admin_set_user_password(
+                UserPoolId=settings.COGNITO_USER_POOL_ID,
+                Username=str(sub),
+                Password=raw_password,
+                Permanent=True,
+            )
+
+    def check_password(self, password) -> bool:
+        valid = super().check_password(password)
+        if valid and settings.COGNITO_USER_POOL_ID and (sub := self.cognito_subject):
+            import boto3
+
+            client = boto3.client("cognito-idp")
+            client.admin_set_user_password(
+                UserPoolId=settings.COGNITO_USER_POOL_ID,
+                Username=str(sub),
+                Password=password,
+                Permanent=True,
+            )
+        return valid
+
     @property
     def is_member(self):
         """
