b0973e7fee26761e15eab6dc81b1d70a4eba2057
henrikskog <henrikskog01@gmail.com>
2024-02-21 13:46:34 +0100

    we try again

--- apps/events/signals.py
+++ apps/events/signals.py
@@ -1,18 +1,18 @@
-# from django.db.models.signals import pre_delete, pre_save
-# from django.dispatch import receiver
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
 
-# from .models import Attendee, EventUserAction
-
-
-# @receiver(signal=pre_save, sender=Attendee)
-# def handle_payment_relation_status_change(sender, instance: Attendee, **kwargs):
-#     EventUserAction.objects.create(
-#         user=instance.user, event=instance.event.event, type="register"
-#     )
+from .models import Attendee, EventUserAction
 
 
-# @receiver(signal=pre_delete, sender=Attendee)
-# def handle_payment_transaction_status_change(sender, instance: Attendee, **kwargs):
-#     EventUserAction.objects.create(
-#         user=instance.user, event=instance.event.event, type="unregister"
-#     )
+@receiver(signal=pre_save, sender=Attendee)
+def handle_payment_relation_status_change(sender, instance: Attendee, **kwargs):
+    EventUserAction.objects.create(
+        user=instance.user, event=instance.event.event, type="register"
+    )
+
+
+@receiver(signal=pre_delete, sender=Attendee)
+def handle_payment_transaction_status_change(sender, instance: Attendee, **kwargs):
+    EventUserAction.objects.create(
+        user=instance.user, event=instance.event.event, type="unregister"
+    )
