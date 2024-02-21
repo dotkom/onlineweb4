50b6e827fb3aa5b0983f5beeb0546ba31abda534
henrikskog <henrikskog01@gmail.com>
2024-02-21 14:15:12 +0100

    save events post save

--- apps/events/signals.py
+++ apps/events/signals.py
@@ -1,17 +1,17 @@
-from django.db.models.signals import pre_delete, pre_save
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from .models import Attendee, EventUserAction
 
 
-@receiver(signal=pre_save, sender=Attendee)
+@receiver(signal=post_save, sender=Attendee)
 def handle_payment_relation_status_change(sender, instance: Attendee, **kwargs):
     EventUserAction.objects.create(
         user=instance.user, event=instance.event.event, type="register"
     )
 
 
-@receiver(signal=pre_delete, sender=Attendee)
+@receiver(signal=post_delete, sender=Attendee)
 def handle_payment_transaction_status_change(sender, instance: Attendee, **kwargs):
     EventUserAction.objects.create(
         user=instance.user, event=instance.event.event, type="unregister"
