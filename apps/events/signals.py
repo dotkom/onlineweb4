dc4d0dc26e2ef04c25b16da6553a91d7ae988e97
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2024-02-29 10:36:35 +0100

    Revert EventUserActions again
    They don't have the right access in prod.

--- apps/events/signals.py
+++ apps/events/signals.py
@@ -1,18 +1,18 @@
-from django.db.models.signals import post_delete, post_save
-from django.dispatch import receiver
+# from django.db.models.signals import post_delete, post_save
+# from django.dispatch import receiver
 
-from .models import Attendee, EventUserAction
-
-
-@receiver(signal=post_save, sender=Attendee)
-def handle_payment_relation_status_change(sender, instance: Attendee, **kwargs):
-    EventUserAction.objects.create(
-        user=instance.user, event=instance.event.event, type="register"
-    )
+# from .models import Attendee, EventUserAction
 
 
-@receiver(signal=post_delete, sender=Attendee)
-def handle_payment_transaction_status_change(sender, instance: Attendee, **kwargs):
-    EventUserAction.objects.create(
-        user=instance.user, event=instance.event.event, type="unregister"
-    )
+# @receiver(signal=post_save, sender=Attendee)
+# def handle_payment_relation_status_change(sender, instance: Attendee, **kwargs):
+#     EventUserAction.objects.create(
+#         user=instance.user, event=instance.event.event, type="register"
+#     )
+
+
+# @receiver(signal=post_delete, sender=Attendee)
+# def handle_payment_transaction_status_change(sender, instance: Attendee, **kwargs):
+#     EventUserAction.objects.create(
+#         user=instance.user, event=instance.event.event, type="unregister"
+#     )
