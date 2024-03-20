dotkom/onlineweb4

commit ad797e3881f667fd34f1064b5992078aca0f4ca8

lint fixes

apps/payment/mommy.py
@@ -71,24 +71,6 @@ def send_reminder_mail(payment):
     EmailMessage(subject, content, payment.responsible_mail(), [], receivers).send()
 
 
-def send_deadline_passed_mail(payment):
-    subject = _("Betalingsfrist utgått: ") + payment.description()
-
-    content = render_to_string(
-        "payment/email/reminder_deadline_passed.txt",
-        {
-            "payment_description": payment.description(),
-            "payment_url": settings.BASE_URL
-            + payment.content_object.event.get_absolute_url(),
-            "payment_email": payment.responsible_mail(),
-        },
-    )
-
-    receivers = not_paid_mail_addresses(payment)
-
-    EmailMessage(subject, content, payment.responsible_mail(), [], receivers).send()
-
-
 def send_missed_payment_mail(payment):
     # NOTE
     # This method does nothing. Guess it was left here in cases rules for expired payments
@@ -157,11 +139,6 @@ def set_marks(payment):
         user_entry.user = user
         user_entry.mark = mark
         user_entry.save()
-
-
-def unattend(payment):
-    for user in not_paid(payment):
-        Attendee.objects.get(event=payment.content_object, user=user).delete()
 
 
 def suspend(payment):

.github/workflows/codeql-analysis.yml
@@ -42,7 +42,7 @@ jobs:
         POETRY_VIRTUALENVS_CREATE: false
       run: |
         pip install poetry
-        poetry export -f requirements.txt > requirements.txt --dev -E prod -E docs --without-hashes
+        poetry export -f requirements.txt > requirements.txt --dev -E docs --without-hashes
     # Initializes the CodeQL tools for scanning.
     - name: Initialize CodeQL
       uses: github/codeql-action/init@v1

.github/workflows/main.yml
@@ -42,7 +42,7 @@ jobs:
           POETRY_VIRTUALENVS_CREATE: false
         run: |
           pip install poetry
-          poetry install --no-interaction --no-ansi -E prod
+          poetry install --no-interaction --no-ansi
 
       - name: Cache node modules
         uses: actions/cache@v2.1.6

.github/workflows/publish.yml
@@ -1,40 +1,0 @@
-name: publish
-
-on:
-  push:
-    branches:
-      - 'master'
-  workflow_dispatch:
-
-jobs:
-  publish:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Set up QEMU
-        uses: docker/setup-qemu-action@v1
-
-      - name: Set up Docker Buildx
-        uses: docker/setup-buildx-action@v1
-
-      - name: Login to DockerHub
-        uses: docker/login-action@v1 
-        with:
-          username: ${{ secrets.DOCKERHUB_USERNAME }}
-          password: ${{ secrets.DOCKERHUB_TOKEN }}
-
-      - name: Build and push nginx
-        uses: docker/build-push-action@v2
-        with:
-          context: .
-          target: onlineweb4-nginx
-          push: true
-          tags: dotkomonline/onlineweb4-nginx:latest,dotkomonline/onlineweb4-nginx:${GITHUB_SHA}
-          cache-to: type=local,dest=/tmp/.buildx-cache,mode=max
-
-      - name: Build and push django
-        uses: docker/build-push-action@v2
-        with:
-          context: .
-          push: true
-          tags: dotkomonline/onlineweb4:latest,dotkomonline/onlineweb4:${GITHUB_SHA}
-          cache-from: type=local,src=/tmp/.buildx-cache

apps/gallery/dashboard/views.py
@@ -54,7 +54,7 @@ class GalleryIndex(DashboardPermissionMixin, ListView):
                     )
 
         # Filter out potential ResponsiveImage objects that have orphan file references
-       # context["images"] = [i for i in context["images"] if i.file_status_ok()]
+        # context["images"] = [i for i in context["images"] if i.file_status_ok()]
 
         # Add query filters and some statistics on disk usage
         context["years"] = years

apps/gallery/models.py
@@ -170,7 +170,7 @@ class BaseResponsiveImage(models.Model):
 
     def save(self, *args, **kwargs):
         total = 0
-        total  = self.thumbnail.size
+        total = self.thumbnail.size
         total += self.image_xs.size
         total += self.image_sm.size
         total += self.image_md.size

apps/gallery/util.py
@@ -3,12 +3,10 @@
 import logging
 import math
 import os
-import shutil
 import uuid
 from io import BytesIO
 
 from django.conf import settings as django_settings
-from django.core.files import File
 from django.core.files.storage import default_storage
 from django.core.files.uploadedfile import InMemoryUploadedFile, TemporaryUploadedFile
 from PIL import Image, ImageOps

apps/notifications/tasks.py
@@ -2,7 +2,6 @@ import json
 import logging
 
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import send_mail
 from pywebpush import WebPushException, webpush
 from rest_framework import serializers

commit 0aa846c8deb8857a76e7a511d61f217f1792f7a4

Fix reminder mail using wrong func
Broken since ad797e3881f667fd34f1064b5992078aca0f4ca8

apps/payment/mommy.py
@@ -37,7 +37,7 @@ def payment_reminder():
 
         if deadline_diff <= 0:
             if not_paid(payment):
-                send_deadline_passed_mail(payment)
+                send_deadline_passed_mail_payment(payment)
                 notify_committee(payment)
                 set_marks(payment)
                 suspend(payment)
@@ -119,7 +119,7 @@ def notify_committee(payment):
 
     receivers = [payment.responsible_mail()]
 
-    EmailMessage(subject, content, "online@online.ntnu.no", [], receivers).send()
+    EmailMessage(subject, content, "online@online.ntnu.no", receivers).send()
 
 
 def not_paid(payment):
@@ -223,7 +223,27 @@ def handle_suspensions(payment_delay):
     suspension.save()
 
 
-def send_deadline_passed_mail(payment_delay, unattend_deadline_passed=True):
+def send_deadline_passed_mail_payment(payment: Payment):
+    subject = _("Betalingsfrist utgått: ") + payment.description()
+
+    content = render_to_string(
+        "payment/email/reminder_deadline_passed.txt",
+        {
+            "payment_description": payment.description(),
+            "payment_url": settings.BASE_URL
+            + payment.content_object.event.get_absolute_url(),
+            "payment_email": payment.responsible_mail(),
+        },
+    )
+
+    receivers = not_paid_mail_addresses(payment)
+
+    EmailMessage(subject, content, payment.responsible_mail(), [], receivers).send()
+
+
+def send_deadline_passed_mail(
+    payment_delay: PaymentDelay, unattend_deadline_passed=True
+):
     payment = payment_delay.payment
 
     subject = _("Betalingsfrist utgått: ") + payment.description()

apps/payment/tests/mommy_tests.py
@@ -1,0 +1,49 @@
+from django.core import mail
+from django.test import TestCase
+from django.utils import timezone
+from django_dynamic_fixture import F, G
+
+from apps.authentication.models import OnlineGroup
+from apps.events.tests.utils import attend_user_to_event, generate_event, generate_user
+from apps.payment.models import PaymentTypes
+from apps.payment.mommy import payment_reminder
+from apps.payment.tests.utils import generate_event_payment
+
+
+class PaymentReminderTests(TestCase):
+    def setup_testcase(self):
+        self.committee = G(
+            OnlineGroup, group=F(name="Arrkom"), email="arrkom@online.ntnu.no"
+        )
+        self.user = generate_user(username="test_user", email="oline@online.online")
+
+        self.event = generate_event(organizer=self.committee.group)
+        self.event.event_end = timezone.now() - timezone.timedelta(days=3)
+        self.event.event_start = timezone.now() - timezone.timedelta(days=2)
+        self.event.attendance_event.registration_end = (
+            timezone.now() - timezone.timedelta(days=1)
+        )
+        self.event.attendance_event.unattend_deadline = (
+            timezone.now() - timezone.timedelta(days=1)
+        )
+        self.event.save()
+        self.event.attendance_event.save()
+
+        self.payment = generate_event_payment(
+            self.event,
+            deadline=timezone.now() - timezone.timedelta(days=1),
+            payment_type=PaymentTypes.DEADLINE,
+        )
+        self.attendee = attend_user_to_event(self.event, self.user)
+
+    def test_no_attend_no_mail(self):
+        payment_reminder()
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_attend_mail(self):
+        self.setup_testcase()
+        payment_reminder()
+        self.assertEqual(len(mail.outbox), 2)
+        user_mail, committee_mail = mail.outbox
+        self.assertEqual(user_mail.bcc, [self.user.email])
+        self.assertEqual(committee_mail.to, [self.committee.email])