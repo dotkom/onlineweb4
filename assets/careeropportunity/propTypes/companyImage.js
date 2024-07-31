0a58e99099ed5618da5c1684168dac4d7cdb718a
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2024-07-31 21:46:23 +0200

    add proper redirect to owf for careeropportunity

--- assets/careeropportunity/propTypes/companyImage.js
+++
@@ -1,7 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default PropTypes.shape({
-  id: PropTypes.number,
-  md: PropTypes.string,
-  lg: PropTypes.string,
-});
--- assets/careeropportunity/propTypes/tags.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default PropTypes.objectOf(PropTypes.shape({
-  id: PropTypes.number,
-  name: PropTypes.string,
-  display: PropTypes.bool,
-  deadline: PropTypes.number,
-}));
