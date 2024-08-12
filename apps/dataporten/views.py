6ff2124c9ddfece590c5c6fb6986e5a01310a5c3
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2024-08-12 12:39:20 +0200

    Allow repeat applications
    - User should be able to update their study to master's without manual
      intervention

--- apps/dataporten/views.py
+++ apps/dataporten/views.py
@@ -33,11 +33,6 @@ def study(request):
     user's groups membership, which can be used to verify eligibility for membership of Online.
     """
 
-    # If the user already is a member we can return early. However, if we're in testing, we want to skip the check.
-    if request.user.is_member and not settings.DATAPORTEN.get("STUDY").get("TESTING"):
-        messages.info(request, "Du er allerede registrert som medlem.")
-        return redirect("profiles_active", active_tab="membership")
-
     logger.debug(
         f"{request.user} wants to automatically confirm study programme through Dataporten.",
         extra={"user": request.user},
